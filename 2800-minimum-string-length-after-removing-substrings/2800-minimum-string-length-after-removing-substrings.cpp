2d35cf23f53e2131091f57f68c7a65fa2950d3e8
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-14 00:12:02 +0200

    Update 2800-minimum-string-length-after-removing-substrings.cpp

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,3 +1,35 @@
+//methode  1  O(N^2)    
+//class Solution {
+//public:
+//    int minLength(string s) {
+//        bool foundAB = false, foundCD = false;
+//         unsigned int indx;
+//        while (true)
+//        {
+//            if ( s.find ( "AB") != string ::npos ) 
+//            {
+//                indx = s.find("AB");
+//                foundAB = true;
+//                s.erase(s.begin() + indx);
+//                s.erase(s.begin() + indx);
+//            }
+//            if (s.find("CD") != string::npos)
+//            {
+//                indx = s.find("CD");
+//                foundCD = true;
+//                s.erase(s.begin() + indx);
+//                s.erase(s.begin() + indx );
+//            }
+//            if (!(foundAB || foundCD)) 
+//                break;
+//            foundAB = false;
+//            foundCD = false;
+//        }       
+//        return s.size();
+//    }
+//};
+
+
 //methode 1 [optimized]  O(N)   
 class Solution {
 public:
@@ -16,7 +48,7 @@ public:
                     }
                     else
                         stack.pop_back();       
-                }else 
+                }else //to add B if it is last char in string which has no (opining A) before  
                 stack += s[i];
             }
             else if (s[i] == 'D')
@@ -29,7 +61,7 @@ public:
                         }
                         else
                             stack.pop_back();       
-                    }else 
+                    }else //to add D if it is last char in string which has no (opining C) before  
                         stack += s[i];
                 }
             else
@@ -39,4 +71,4 @@ public:
         }
         return stack.size();
     }
-}; 
+};
