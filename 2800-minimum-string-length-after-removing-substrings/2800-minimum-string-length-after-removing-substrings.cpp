95a25d2302b874729aab757d4fddc205afc7f324
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-14 01:07:23 +0200

    Time: 0 ms (100%), Space: 10.4 MB (75.35%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,3 +1,35 @@
+//methode  1  O(N^2)    
+//class Solution {
+//public:
+//    int minLength(string s) {
+//        bool foundAB = false, foundCD = false;
+//         unsigned int indx;
+//        while (true)
+//        {
+//
+//            if ( s.find ( "AB") != string ::npos ) 
+//            {
+//                indx = s.find("AB");
+//                foundAB = true;
+//                s.erase(s.begin() + indx);
+//                s.erase(s.begin() + indx);
+//            }
+//            if (s.find("CD") != string::npos)
+//            {
+//                indx = s.find("CD");
+//                foundCD = true;
+//                s.erase(s.begin() + indx);
+//                s.erase(s.begin() + indx );
+//            }
+//            if (!(foundAB || foundCD)) 
+//                break;
+//            foundAB = false;
+//            foundCD = false;
+//        }       
+//        return s.size();
+//    }
+//};
+
 //methode 1 [optimized]  O(N)   
 // class Solution {
 // public:
