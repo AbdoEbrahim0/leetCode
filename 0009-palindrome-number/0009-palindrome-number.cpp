5b13e157456688dad12d565b73131f922b986710
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-16 00:07:37 +0200

    Time: 6 ms (19%), Space: 8.8 MB (25.6%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,47 @@
 #include<string>
 using namespace std;
+//methode 1
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if (x < 0) return false;
+//         int copyOfX = x;
+//         string str="";
+//         int digit;
+//         while (x > 0)
+//         {
+//             digit = x % 10;
+//             str += to_string(digit);
+//             x = x / 10;
+//         }
+//         if (str.empty()) 
+//             str = "0";
+//         if (str ==to_string(copyOfX) )
+//             return true;
+//         else
+//             return false;
+
+//     }
+// };
+//methode 2
 class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
+        if (x ==0) return true;   
+
         int copyOfX = x;
-        string str="";
-        int digit;
-        while (x > 0)
+        int how_many_digits = log10(x) +1;
+        
+        unsigned  long long reversedNum=0;
+        
+        while (how_many_digits !=0)
         {
-            digit = x % 10;
-            str += to_string(digit);
+            reversedNum = reversedNum + (x % 10)*pow(10, --how_many_digits);
             x = x / 10;
         }
-        if (str.empty()) 
-            str = "0";
-        if (str ==to_string(copyOfX) )
+        
+        if (reversedNum==copyOfX )
             return true;
         else
             return false;
