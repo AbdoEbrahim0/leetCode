bb06fb9b8eea9215598aed5800f9012dcfb80874
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-16 00:23:07 +0200

    Time: 4 ms (33.91%), Space: 10.9 MB (9.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -24,27 +24,45 @@ using namespace std;
 //     }
 // };
 //methode 2
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if (x < 0) return false;
+//         if (x ==0) return true;   
+
+//         int copyOfX = x;
+//         int how_many_digits = log10(x) +1;
+        
+//         unsigned  long long reversedNum=0;
+        
+//         while (how_many_digits !=0)
+//         {
+//             reversedNum = reversedNum + (x % 10)*pow(10, --how_many_digits);
+//             x = x / 10;
+//         }
+        
+//         if (reversedNum==copyOfX )
+//             return true;
+//         else
+//             return false;
+
+//     }
+// };
+//method 2 [optimized]
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
-        if (x ==0) return true;   
-
-        int copyOfX = x;
-        int how_many_digits = log10(x) +1;
-        
-        unsigned  long long reversedNum=0;
-        
-        while (how_many_digits !=0)
+        if (x < 0) return false; 
+        string str = to_string(x);
+        int digit;
+        int indx = 0;
+        while (x>0)
         {
-            reversedNum = reversedNum + (x % 10)*pow(10, --how_many_digits);
+            digit =(x % 10);
+            if (digit != (str[indx++] - '0'))
+                return false;
             x = x / 10;
         }
-        
-        if (reversedNum==copyOfX )
             return true;
-        else
-            return false;
-
     }
-};
+};  
