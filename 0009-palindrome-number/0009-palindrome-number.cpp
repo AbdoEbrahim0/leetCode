c7cdd6db00c21ac821ae4f941354ee3bbd3ca5cc
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-16 02:43:25 +0200

    Time: 6 ms (19%), Space: 10.8 MB (15.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,5 @@
 #include<string>
 using namespace std;
-//best approach check half reversal
-class Solution {
-public:
-bool isPalindrome(int x) {
-    if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-    int reversed = 0;
-    while (x > reversed) {
-        reversed = reversed * 10 + x % 10;
-        x /= 10;
-    }
-    return (x == reversed || x == reversed / 10 );
-}
-};
 //methode 1
 // class Solution {
 // public:
@@ -33,6 +20,7 @@ bool isPalindrome(int x) {
 //             return true;
 //         else
 //             return false;
+
 //     }
 // };
 //methode 2
@@ -41,81 +29,40 @@ bool isPalindrome(int x) {
 //     bool isPalindrome(int x) {
 //         if (x < 0) return false;
 //         if (x ==0) return true;   
+
 //         int copyOfX = x;
-//         int how_many_digits = log10(x) +1;     
+//         int how_many_digits = log10(x) +1;
+        
 //         unsigned  long long reversedNum=0;
+        
 //         while (how_many_digits !=0)
 //         {
 //             reversedNum = reversedNum + (x % 10)*pow(10, --how_many_digits);
 //             x = x / 10;
 //         }
+        
 //         if (reversedNum==copyOfX )
 //             return true;
 //         else
 //             return false;
+
 //     }
 // };
-//methode 2 math optimized
-//  class Solution {
-//  public:
-//      bool isPalindrome(int x) {
-//          if (x < 0) return false;
-//          if (x ==0) return true;   
-//          int copyOfX = x;
-//          int how_many_digits = log10(x) +1;     
-//          int hundereds = pow(10, --how_many_digits);
-//          unsigned  long long reversedNum=0;
-//          while (hundereds !=0)
-//          {
-//              reversedNum = reversedNum + (x % 10)* hundereds;
-//              hundereds /= 10;
-//              x = x / 10;
-//          }
-//          if (reversedNum==copyOfX )
-//              return true;
-//          else
-//              return false;
-//      }
-//  };
-//methode 2 using first_digit and last digit 
-//  class Solution {
-//  public:
-//      bool isPalindrome(int x) {
-//          if (x < 0) return false;
-//          if (x ==0) return true;   
-//          int how_many_digits = log10(x) +1;
-//          int hundreds= pow(10, how_many_digits -1);
-//          int f_digit = x / hundreds;
-//          int numAfterRemoveF_d=x;
-//          while (true) 
-//          {
-//              //hundreds = pow(10, --how_many_digits);//100
-//              if (x % 10 != (int)(f_digit))
-//                  return false;
-//              numAfterRemoveF_d = (numAfterRemoveF_d - (numAfterRemoveF_d / hundreds) * hundreds);//121 - (121/100) *100 =121-1*100=21
-//              if (hundreds == 1) break;
-//              hundreds /= 10;
-//              f_digit = (numAfterRemoveF_d) / (hundreds);//2
-//              x = x / 10;
-//          }
-//              return true;
-//      }
-//  };
-//method 3 
-// class Solution {
-// public:
-//     bool isPalindrome(int x) {
-//         if (x < 0) return false; 
-//         string str = to_string(x);
-//         int digit;
-//         int indx = 0;
-//         while (x>0)
-//         {
-//             digit =(x % 10);
-//             if (digit != (str[indx++] - '0'))
-//                 return false;
-//             x = x / 10;
-//         }
-//             return true;
-//     }
-// };  
+//method 2 [optimized]
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; 
+        string str = to_string(x);
+        int digit;
+        int indx = 0;
+        while (x>0)
+        {
+            digit =(x % 10);
+            if (digit != (str[indx++] - '0'))
+                return false;
+            x = x / 10;
+        }
+            return true;
+    }
+};  
