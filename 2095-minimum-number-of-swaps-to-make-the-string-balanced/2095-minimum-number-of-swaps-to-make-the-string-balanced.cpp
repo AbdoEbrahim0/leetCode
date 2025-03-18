6a777330be331790d439aaabec20e5b27801b34b
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-18 03:06:59 +0200

    Time: 10 ms (68.65%), Space: 34.2 MB (60.66%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,16 +1,33 @@
+// class Solution {
+// public:
+//     int minSwaps(string s) {  
+// int open = 0;//, problem = 0;
+// for (int i = 0; i < s.size(); i++) {
+//     if (s[i] == '[')
+//         open++;
+//     else {
+//         if (open)
+//             open--;
+//        // else problem++;
+//     }
+// }
+// return ceil((double)open/2);
+//     }
+// };
+
 class Solution {
 public:
-    int minSwaps(string s) {
-       
-int open = 0, problem = 0;
-for (int i = 0; i < s.size(); i++) {
+    int minSwaps(string s) {  
+int open = 0;
+for (int i = 0; i < s.size(); i++)
+ {
     if (s[i] == '[')
         open++;
     else {
         if (open)
             open--;
-       // else problem++;
     }
+        
 }
 return ceil((double)open/2);
     }
