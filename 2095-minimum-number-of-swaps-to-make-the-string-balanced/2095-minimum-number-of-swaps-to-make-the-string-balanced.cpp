6172171cd283f7fb7bcb617bb53680f1d4bdcd82
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-18 03:33:08 +0200

    Time: 19 ms (26.7%), Space: 36.9 MB (20.45%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,33 +1,38 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        //note that it is guranteed to be couple number not individual
+        //][][ 
+        //if (s.empty()) return true;
+        stack<char>stk;
+        int counter = 0;
+        for (auto& it : s) //4/2 =2 //[0],[0+2]  
+        {
+            if (it == '[') stk.push(it);
+            else
+            {
+                if (stk.size())
+                    stk.pop();
+                //else counter--;       
+            }
+        }
+        counter = stk.size();
+        return ceil(counter / 2.0);
+    }
+};
 // class Solution {
 // public:
 //     int minSwaps(string s) {  
-// int open = 0;//, problem = 0;
-// for (int i = 0; i < s.size(); i++) {
-//     if (s[i] == '[')
+// int  open = 0;
+// for (auto & it : s)
+//  {
+//     if (it == '[')
 //         open++;
-//     else {
-//         if (open)
-//             open--;
-//        // else problem++;
-//     }
+//     else 
+//     //if (open) // use this if to pass this test :"][]["
+//         open--;
+        
 // }
 // return ceil((double)open/2);
 //     }
 // };
-
-class Solution {
-public:
-    int minSwaps(string s) {  
-int  open = 0;
-for (auto & it : s)
- {
-    if (it == '[')
-        open++;
-    else 
-    if (open)
-        open--;
-        
-}
-return ceil((double)open/2);
-    }
-};
