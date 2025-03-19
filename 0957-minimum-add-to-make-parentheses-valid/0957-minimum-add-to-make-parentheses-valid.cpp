75aea26d7ae1c2aaa46e9c4aaf165ff2101f1c4d
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-19 23:20:16 +0200

    Time: 0 ms (100%), Space: 8.2 MB (95.16%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,22 +1,37 @@
+//8.31 MB Beats 81.16%
+// class Solution {
+// public:
+//     int minAddToMakeValid(string s) { 
+//                int stk=0; //cntOpenWithClosed
+//        int cnt_closedWithoutOpen = 0;
+//        for (char c : s)
+//        {
+//            if (c == '(')
+//                stk++;
+//            else
+//                if (stk )
+//                {
+//                    stk--;
+//                }
+//                else
+//                {
+//                    cnt_closedWithoutOpen++;
+//                }
+//        }
+//        return stk + cnt_closedWithoutOpen;
+//     }
+// };
+
+//methode  1  using stack optimized for memory & runtime
 class Solution {
 public:
-    int minAddToMakeValid(string s) {
-               int stk=0; //cntOpenWithClosed
+   int minAddToMakeValid(string s) {
+       int stk = 0; //cntOpenWithClosed
        int cnt_closedWithoutOpen = 0;
        for (char c : s)
        {
-           if (c == '(')
-               stk++;
-           else
-               if (stk )
-               {
-                   stk--;
-               }
-               else
-               {
-                   cnt_closedWithoutOpen++;
-               }
+           (c == '(') ? stk++ : (stk)? stk--: cnt_closedWithoutOpen++;
        }
        return stk + cnt_closedWithoutOpen;
-    }
+   }
 };
