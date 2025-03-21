bd1ecc45f54cfab7abc8ac5ad9c5658e6b4fe99c
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-03-21 11:12:27 +0200

    Time: 16 ms (24.21%), Space: 48.5 MB (36.72%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -0,0 +1,51 @@
+//methode 1 naive N^2
+
+// class Solution {
+// public:
+//     int maxWidthRamp(vector<int>& nums) {
+//         int  maxWidth = 0;
+//         int i = 0, j = 0;
+//         for (i = 0; i < nums.size(); i++)
+//         {
+//             for (j = i+1; j < nums.size(); j++)
+//             {
+//                 if (nums[j] >= nums[i]) //we replace this cond:&& j >= i with starting j from (i+1)
+//                 {
+//                     if ((j - i) >= maxWidth)
+//                         maxWidth = j - i;
+//                 }
+//             }
+//         }
+//         return maxWidth;
+//     }
+// };
+//methode 2 O(N) 
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int  maxWidth = 0;
+        unsigned short i;
+        stack<int>stk;
+        stk.push(0);
+        for ( i = 1 ; i < nums.size() ; i++ )
+        {
+            if (nums[i] <= nums[stk.top()])
+                stk.push(i);
+        }
+
+        for (unsigned short j = nums.size() - 1; j >= 0; j--)
+        {
+            
+            while (nums[j] >= nums[stk.top()])
+            {
+                
+                maxWidth = max(maxWidth, j - stk.top());
+                stk.pop();
+                if (stk.empty())
+                    return maxWidth;
+            }
+
+        }
+        return maxWidth;
+    }
+};
