74d4dd2e8df7725a28773d41b4c9de278caee2f6
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-10 02:06:36 +0200

    Time: 7 ms (59.4%), Space: 48.4 MB (58.88%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -19,31 +19,78 @@
 //     }
 // };
 //methode 2 using stack O(N) 
+// class Solution {
+// public:
+//     int maxWidthRamp(vector<int>& nums) {
+//         int  maxWidth = 0;
+//         unsigned short i;
+//         stack<unsigned short>stk;
+//         stk.push(0);
+//         //which i < j 
+//         for ( i = 1 ; i < nums.size() ; i++ )
+//         {
+//             if (nums[i] <= nums[stk.top()])
+//                 stk.push(i);
+//         }
+        
+//         for (unsigned short j = nums.size() - 1; j >= 0; j--)
+//         {
+//             while (nums[j] >= nums[stk.top()])
+//             {
+//                 maxWidth = max(maxWidth, j - stk.top());
+//                 stk.pop();
+//                 if (stk.empty())
+//                     return maxWidth;
+//             }
+//         }
+//         return maxWidth;
+//     }
+// };
+
+///////////////
+
+// class Solution {
+// public:
+//     int maxWidthRamp(vector<int>& nums) {
+//         short my_size = nums.size();
+//         int maxi = 0;
+//         for (int i = 0; i < my_size; i++)
+//         {
+//             for (int j = i + 1; j < my_size; j++)
+//             {
+//                 if (j > i && nums[j] >= nums[i])
+//                     maxi= max(maxi, j - i);
+//             }
+//         }
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        int  maxWidth = 0;
-        unsigned short i;
-        stack<unsigned short>stk;
-        stk.push(0);
-        //which i < j 
-        for ( i = 1 ; i < nums.size() ; i++ )
-        {
-            if (nums[i] <= nums[stk.top()])
-                stk.push(i);
+        int n = nums.size();
+        vector<int> rightMax(n);
+
+        // Fill rightMax array with the maximum values from the right
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], nums[i]);
         }
-        
-        for (unsigned short j = nums.size() - 1; j >= 0; j--)
-        {
-            while (nums[j] >= nums[stk.top()])
-            {
-                maxWidth = max(maxWidth, j - stk.top());
-                stk.pop();
-                if (stk.empty())
-                    return maxWidth;
+
+        int left = 0, right = 0;
+        int maxWidth = 0;
+
+        // Traverse the array using left and right pointers
+        while (right < n) {
+            // Move left pointer forward if current left exceeds rightMax
+            while (left < right && nums[left] > rightMax[right]) {
+                left++;
             }
+            maxWidth = max(maxWidth, right - left);
+            right++;
         }
+
         return maxWidth;
     }
 };
-
