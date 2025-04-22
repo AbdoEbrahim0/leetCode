d7e28162386dd200175c5bc162a6c045af5f93a4
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-22 20:29:19 +0200

    Time: 0 ms (100%), Space: 8 MB (44.25%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int x=0;
+        int power = 0;
+        while (num != 0)
+        {
+            
+            if (num % 2 ==0)
+            {
+                x += pow(2, power);
+            }
+            num = num / 2;
+            power++;
+        }
+        return x;
+    }
+};
