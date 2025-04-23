8a80b619362d1462dfee48ab5bbd4a176fe890e1
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-23 11:43:31 +0200

    Time: 0 ms (100%), Space: 8.4 MB (3.12%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -6,17 +6,14 @@ public:
         
         string s1(maxDigits, '0');
         string s2(maxDigits, '0');
+        int count=0;
         for(int i= maxDigits -1 ; (start != 0) || (goal != 0);i--)
         {
              (start % 2) ? s1[i] = '1': s1[i] = '0';
                 start = start / 2;
              (goal % 2) ? s2[i] = '1' : s2[i] = '0';
                 goal = goal / 2;
-        }
-        int count=0;
-        for (int i = 0; i < s1.size(); i++)
-        {
-            if (s1[i] != s2[i])
+                if (s1[i] != s2[i])
                 count++;
         }
         return count;
