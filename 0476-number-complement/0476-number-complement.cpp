82cf8ec136bbc85558a63d59e982adfb907e92e3
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-23 09:44:44 +0200

    Time: 0 ms (100%), Space: 8 MB (17.29%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,20 +1,43 @@
+// class Solution {
+// public:
+//     int findComplement(int num) {
+//         int x=0;
+//         int power = 0;
+//         while (num != 0)
+//         {
+//             if (num % 2 ==0)
+//             {
+//              //   x += pow(2, power); 
+//              //Or 
+//                 x += 1<< power;
+//             }
+//             num = num / 2;
+//             power++;
+//         }
+//         return x;
+//     }
+// };
+
+//cout << (5 & (1<<2)); 5& 2^2 
+//101
+//100
+//out : 100
+//if even fasle then set the bit to 1
+//if odd true then set the bit to 0 
 class Solution {
 public:
     int findComplement(int num) {
-        int x=0;
-        int power = 0;
-        while (num != 0)
+        if (num == 0) return 1;
+        int howManyDigits;
+            howManyDigits = floor(log2(num)) + 1;; //last added 1 for ceiling;
+    //cout << howManyDigits;//3
+        for (int i = 0; i < howManyDigits; i++)
         {
-            
-            if (num % 2 ==0)
-            {
-             //   x += pow(2, power); 
-             //Or 
-                x += 1<< power;
-            }
-            num = num / 2;
-            power++;
+            if (num & (1<<i))  //if digit is 1 true then set the bit to 0 
+                num &= ~(1 << i);
+            else            //if digit is 0 fasle then set the bit to 1
+                num |= (1 << i);
         }
-        return x;
+        return num;
     }
 };
