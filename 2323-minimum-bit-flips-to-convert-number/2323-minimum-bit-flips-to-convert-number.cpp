0f0a4e01f2e36eaffb6d5f8aea5c3267db85e9bd
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-23 11:48:16 +0200

    Time: 0 ms (100%), Space: 8.1 MB (24.96%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         if (start == 0 && goal == 0)return 0;
-        int maxDigits = (start >= goal) ? ((int)log2(start) ) +1: ((int)log2(goal)) + 1;
-        
-        string s1(maxDigits, '0');
-        string s2(maxDigits, '0');
-        int count=0;
-        for(int i= maxDigits -1 ; (start != 0) || (goal != 0);i--)
+        int maxDigits = (start >= goal) ? ((int)log2(start)) + 1 : ((int)log2(goal)) + 1;
+
+        //string s1(maxDigits, '0');
+        //string s2(maxDigits, '0');
+        int count = 0;
+        char ch1, ch2;
+        for (int i = maxDigits - 1; (start != 0) || (goal != 0); i--)
         {
-             (start % 2) ? s1[i] = '1': s1[i] = '0';
-                start = start / 2;
-             (goal % 2) ? s2[i] = '1' : s2[i] = '0';
-                goal = goal / 2;
-                if (s1[i] != s2[i])
+            (start % 2) ? ch1 = '1' : ch1 = '0';
+            start = start / 2;
+            (goal % 2) ? ch2 = '1' : ch2 = '0';
+            goal = goal / 2;
+            if (ch1 != ch2)
                 count++;
         }
         return count;
