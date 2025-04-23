d92c9b2b6be0760cfebf77dfc4d87df6aacdf97f
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-23 11:59:31 +0200

    Time: 0 ms (100%), Space: 7.9 MB (59.2%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,19 +1,34 @@
+// class Solution {
+// public:
+//     int minBitFlips(int start, int goal) {
+//         if (start==0 && goal==0)return 0;
+//         int maxDigits = (start >= goal) ? ((int)log2(start)) : ((int)log2(goal));
+//         int count = 0;
+//         char ch1, ch2;
+//         for (int i = maxDigits; (start != 0) || (goal != 0); i--)
+//         {
+//             (start % 2) ? ch1 = '1' : ch1 = '0';
+//             start = start / 2;
+//             (goal % 2) ? ch2 = '1' : ch2 = '0';
+//             goal = goal / 2;
+//             if (ch1 != ch2)
+//                 count++;
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        if (start==0 && goal==0)return 0;
-        int maxDigits = (start >= goal) ? ((int)log2(start)) : ((int)log2(goal));
-        int count = 0;
-        char ch1, ch2;
-        for (int i = maxDigits; (start != 0) || (goal != 0); i--)
+        int res = start ^ goal;
+        int counter = 0;
+        while (res!=0)
         {
-            (start % 2) ? ch1 = '1' : ch1 = '0';
-            start = start / 2;
-            (goal % 2) ? ch2 = '1' : ch2 = '0';
-            goal = goal / 2;
-            if (ch1 != ch2)
-                count++;
+            if (res % 2)
+                counter++;
+            res = res / 2;
         }
-        return count;
+        return counter;
     }
 };
