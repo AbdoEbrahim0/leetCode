afb5832bdda3eaea6adef989329f56faca9bec91
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-23 11:50:34 +0200

    Time: 0 ms (100%), Space: 8.2 MB (3.12%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        if (start ==goal)return 0;
+        if (start==0 && goal==0)return 0;
         int maxDigits = (start >= goal) ? ((int)log2(start)) + 1 : ((int)log2(goal)) + 1;
         int count = 0;
         char ch1, ch2;
