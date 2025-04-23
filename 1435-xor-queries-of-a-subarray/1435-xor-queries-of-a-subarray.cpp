93f5e7b6b9e35d40100378050983edf201a39fea
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-23 17:40:49 +0200

    Time: 3 ms (60.9%), Space: 92.5 MB (46.24%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -1,5 +1,3 @@
-//next sol. give me Time Limit Exceed
-// 
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
@@ -16,7 +14,32 @@ public:
             int L = q[0], R = q[1];
             result.push_back(prefixXor[R + 1] ^ prefixXor[L]);
         }
+//1 001
+//6 100
+//out 101 ->7
 
+//0 000
+//14 1110
+//out 1110 ->14
+
+//6   110
+//14 1110 
+//out 1000 ->8
         return result;
     }
 };
+// 
+//1  0001
+//9  1001
+
+//1 001
+//3 011
+//out 010 ->2
+
+//2 010
+//4 100
+//out 110 ->6
+
+//6  110
+//8 1000
+//out 1110//8+4+2 ->14
