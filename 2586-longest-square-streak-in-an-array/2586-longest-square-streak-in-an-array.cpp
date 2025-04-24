21499d880741b56fe76ea6b4bb0293f02199eee4
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-24 23:33:55 +0200

    Time: 285 ms (5.02%), Space: 109 MB (56.67%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -73,27 +73,38 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_set<long long> s(nums.begin(), nums.end());
-        int maxLen = -1;
-
-        for (int num : nums) {
-            long long curr = num;
-            int len = 0;
-
-            // Only start if this is the beginning of a chain
-            if (s.find((long long)sqrt(curr)) != s.end() && sqrt(curr) * sqrt(curr) == curr)
-                continue;
-
-            while (s.find(curr) != s.end()) {
-                len++;
-                curr *= curr;
-                if (curr > 1e10) break;  // prevent overflow
-            }
-
-            if (len >= 2)  // must be at least a square once
-                maxLen = max(maxLen, len);
+        int maxElement= *max_element(nums.begin(), nums.end());
+        unordered_set <int> mySet;
+        for (int &val : nums)
+        {
+            mySet.insert(val);
         }
+        long long temp;
+        int maxi = 0;
+        int cnt = 0;//,power=2;
+        for (int val :mySet)
+        {
+            temp = val;
+            temp = temp * temp;
+            for (int val_j : mySet)
+            {
+                if ((val_j > temp) && (val_j > maxElement) )
+                    break;
+                if (temp > 100000)
+                    break;
+                if (mySet.find(temp ) != mySet.end() )
+                {
+                    temp = temp * temp;
+                    cnt++;
+                //    power++;
+                }
 
-        return maxLen;
+            }
+            maxi = max(maxi, cnt+1);
+            cnt = 0;
+            //power = 2;
+        }
+        if (maxi == 1) return -1;
+        return maxi;
     }
 };
