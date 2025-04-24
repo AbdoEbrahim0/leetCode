2b43c40c16c2b4103054db710a4b664ac77decec
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-25 02:13:22 +0300

    Time: 63 ms (81.21%), Space: 86.5 MB (96.56%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -29,7 +29,7 @@
 //         return maxi;
 //     }
 // };
-//TLE [optimized]
+//TLE [optimized]^2
 
 // class Solution {
 // public:
@@ -69,42 +69,64 @@
 //         return maxi;
 //     }
 // };
+    //TLE [optimized]^3
+// class Solution {
+// public:
+//     int longestSquareStreak(vector<int>& nums) {
+//         int maxElement= *max_element(nums.begin(), nums.end());
+//         unordered_set <int> mySet;
+//         for (int &val : nums)
+//         {
+//             mySet.insert(val);
+//         }
+//         long long temp;
+//         int maxi = 0;
+//         int cnt = 0;//,power=2;
+//         for (int val :mySet)
+//         {
+//             temp = val;
+//             temp = temp * temp;
+//             for (int val_j : mySet)
+//             {
+//                 if ((val_j > temp) && (val_j > maxElement) )
+//                     break;
+//                 if (temp > 100000)
+//                     break;
+//                 if (mySet.find(temp ) != mySet.end() )
+//                 {
+//                     temp = temp * temp;
+//                     cnt++;
+//                 //    power++;
+//                 }
 
+//             }
+//             maxi = max(maxi, cnt+1);
+//             cnt = 0;
+//             //power = 2;
+//         }
+//         if (maxi == 1) return -1;
+//         return maxi;
+//     }
+// };
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        int maxElement= *max_element(nums.begin(), nums.end());
-        unordered_set <int> mySet;
-        for (int &val : nums)
-        {
-            mySet.insert(val);
-        }
-        long long temp;
+        sort(nums.begin(), nums.end());
+        long long temp ;
+        int counter = 0;
         int maxi = 0;
-        int cnt = 0;//,power=2;
-        for (int val :mySet)
+        for (int& val : nums)
         {
-            temp = val;
-            temp = temp * temp;
-            for (int val_j : mySet)
+            temp = 1LL* val * val;
+            counter = 0;
+            while (binary_search(nums.begin(), nums.end(), temp))
             {
-                if ((val_j > temp) && (val_j > maxElement) )
-                    break;
-                if (temp > 100000)
-                    break;
-                if (mySet.find(temp ) != mySet.end() )
-                {
-                    temp = temp * temp;
-                    cnt++;
-                //    power++;
-                }
-
+                counter++;
+                temp = temp * temp;
             }
-            maxi = max(maxi, cnt+1);
-            cnt = 0;
-            //power = 2;
+            maxi = max(maxi, counter +1);
         }
-        if (maxi == 1) return -1;
+        if (maxi <= 1) return -1;
         return maxi;
     }
 };
