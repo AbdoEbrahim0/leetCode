3c2598ce8c7fa7ed9d80e151af2dca244c7bd2f1
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-25 22:48:56 +0300

    Time: 39 ms (16.57%), Space: 88.3 MB (7.27%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,10 +1,42 @@
+// class Solution {
+// public:
+//     bool ok(int mid, vector<int>& candies, long long k)
+//     {
+//         long long cnt=0;
+//         for (int& val : candies)
+//             {cnt += val/mid;}
+//         return cnt >= k;
+//     }
+//     int maximumCandies(vector<int>& candies, long long k) {
+//         int start = 1, end = 1e7;
+//         int cur = 0, mid;
+//         while (start<=end)
+//         {
+//             mid = (start + end) / 2;
+//             if ( ok(mid,candies,k) )
+//             {
+//                 cur = mid;
+//                 start = mid + 1;
+//             }
+//             else
+//                 end = mid -1;
+//         }
+//         return cur;
+//     }
+// };
 class Solution {
 public:
     bool ok(int mid, vector<int>& candies, long long k)
     {
         long long cnt=0;
+        // long   sum=0;
         for (int& val : candies)
-            {cnt += val/mid;}
+            {cnt += val/mid;
+            // sum+=val;
+            
+            }
+            // if(k>sum)
+            // return 0;
         return cnt >= k;
     }
     int maximumCandies(vector<int>& candies, long long k) {
