bc4458857cd93845027185eda68d44b594d84b00
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-04-25 20:04:03 +0300

    Time: 24 ms (61.01%), Space: 88.3 MB (43.47%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool ok(int mid, vector<int>& candies, long long k)
+    {
+        long long cnt=0;
+        for (int& val : candies)
+            cnt += val/mid;
+        return cnt >= k;
+    }
+    int maximumCandies(vector<int>& candies, long long k) {
+        int start = 1, end = *max_element(candies.begin(), candies.end());
+        int cur = 0, mid;
+        while (start<=end)
+        {
+            mid = (start + end) / 2;
+            if ( ok(mid,candies,k) )
+            {
+                cur = mid;
+                start = mid + 1;
+            }
+            else
+                end = mid -1;
+        }
+        return cur;
+    }
+};
