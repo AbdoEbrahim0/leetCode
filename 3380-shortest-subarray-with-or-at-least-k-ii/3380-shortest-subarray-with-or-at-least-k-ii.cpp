fc44a3c64193d40b30e077585d8fdf16f1e97723
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-03 18:52:42 +0300

    Time: 691 ms (6.02%), Space: 89.9 MB (15.48%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -175,9 +175,56 @@
 //     }
 // };
 //////////////////////////////
+// class Solution {
+// public:
+//     void update(int x, vector<int>& vis, int val)
+//     {
+//         for (int i = 0; i < 32; i++)
+//         {
+//             if ((x >> i) & 1)
+//                 vis[i] += val;
+//         }
+//     }
+//     int getVal(vector<int>& vis)
+//     {
+//         int x = 0;
+//         for (int i = 0; i < 32; i++)
+//         {
+//             if(vis[i])
+//             x |= (1 << i);
+//         }
+//         return x;
+//     }
+//      int minimumSubarrayLength(vector<int>& nums, int k) {
+//          int n = nums.size();
+//          int st = 0, ed = 0, cur = INT_MAX;
+//          vector<int> vis(32);
+//          while (true)
+//          {
+//              if (getVal(vis) >= k && st < ed)
+//              {
+//                  cur = min(cur, ed - st);
+//                  update(nums[st], vis, -1);
+//                  st++;
+
+//              }
+//              else
+//              {
+//                  if (ed == n) break;
+//                  update(nums[ed++], vis, 1);
+//              }
+//          }
+//          if (cur == INT_MAX)
+//              return -1;
+
+//          return cur;
+//      }
+//  };
+
 class Solution {
 public:
-    void update(int x, vector<int>& vis, int val)
+    
+    void updat(int x, vector<int>& vis ,int val)
     {
         for (int i = 0; i < 32; i++)
         {
@@ -185,38 +232,55 @@ public:
                 vis[i] += val;
         }
     }
-    int getVal(vector<int>& vis)
+    int getWindowVal(vector<int>& vis)
     {
-        int x = 0;
+        int sumOfOr = 0;
+        
         for (int i = 0; i < 32; i++)
         {
             if(vis[i])
-            x |= (1 << i);
+            sumOfOr = sumOfOr | (1<<i); //2^0  2^1  2^2
         }
-        return x;
+        return sumOfOr;
     }
-     int minimumSubarrayLength(vector<int>& nums, int k) {
-         int n = nums.size();
-         int st = 0, ed = 0, cur = INT_MAX;
-         vector<int> vis(32);
-         while (true)
-         {
-             if (getVal(vis) >= k && st < ed)
-             {
-                 cur = min(cur, ed - st);
-                 update(nums[st], vis, -1);
-                 st++;
+    bool ok(int mid, vector<int>& nums, int k)
+    {
+        vector<int> vis(32);
+        for (int i = 0; i < mid; i++)
+        {
+            updat(nums[i], vis, 1);
+        }
+        //if return true then length of Windows may be less than mid
+        if (getWindowVal(vis) >= k)
+            return true;
+        //if false then continue on the array to find if there is any window Value >=k
+        for (int i = mid; i < nums.size(); i++)
+        {
+            updat(nums[i], vis, 1);//adding  new element 
+            updat(nums[i-mid], vis, -1);// removing previous one 
+            if (getWindowVal(vis) >= k)
+                return true;
+        }
+        return false;
+    }
 
-             }
-             else
-             {
-                 if (ed == n) break;
-                 update(nums[ed++], vis, 1);
-             }
-         }
-         if (cur == INT_MAX)
-             return -1;
-
-         return cur;
-     }
- };
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int start = 1, end = n, cur = -1;
+        int mid;
+        while (start<=end)
+        {
+            mid = (start + end) / 2;
+            if (ok(mid,nums,k))
+            {
+                cur = mid;
+                end =  mid- 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+    return cur;
+    }
+};
