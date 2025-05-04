7cccab891128ae817175a3ed508a7fc447439b40
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-04 23:41:04 +0300

    Time: 80 ms (81.86%), Space: 117.4 MB (98.49%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,37 +1,52 @@
+// class Solution {
+// public:
+//     int maxCount(vector<int>& banned, int n, int maxSum) {
+//         int bannedSize = banned.size();
+//         int counter = 0;
+//         sort(banned.begin(), banned.end());
+//         for (int j = 1; j < n; j++) //n
+//         {
+//                 int start = 0, end = bannedSize-1, mid, cur = -1; // 1 2 3 4 5 6 n
+//                 while (start <= end) // log(baneedSize)
+//                 {
+//                     mid = (start + end) / 2;
+//                     if (j == banned[mid])
+//                     {
+//                         cur = mid;
+//                         break;
+//                     }
+//                     else if (  j >banned[ mid]) {
+//                         start = mid + 1;
+//                     }
+//                     else 
+//                         end = mid - 1;
+//                 }
+//                 if (cur == -1 && (maxSum - j) >= 0)
+//                 {
+//                     counter++;
+//                     maxSum -= j; //maxSum -[2+3 
+//                 }              
+//         }
+//         return counter;
+//     }
+// };
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         int bannedSize = banned.size();
-        
         int counter = 0;
         sort(banned.begin(), banned.end());
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= n; j++) //n
         {
-            //binary search
-                int start = 0, end = bannedSize-1, mid, cur = -1; // 1 2 3 4 5 6 n
-                while (start <= end)
-                {
-                    mid = (start + end) / 2;
-                    //(maxSum -mid)>=0
-                    if (j == banned[mid])
-                    {
-                        cur = mid;
-                        break;
-                    }
-                    else if (  j >banned[ mid]) {
-
-                        start = mid + 1;
-                    }
-                    else 
-                    {
-                        end = mid - 1;
-                    }
-                }
-                if (cur == -1 && (maxSum - j) >= 0)
+            
+            if (!(binary_search(banned.begin(),banned.end() ,j)))
+            {
+                if (maxSum - j >= 0)
                 {
                     counter++;
                     maxSum -= j; //maxSum -[2+3 
                 }
+            }
         }
         return counter;
     }
