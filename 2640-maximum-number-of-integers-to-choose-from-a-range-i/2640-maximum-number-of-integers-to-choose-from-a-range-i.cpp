d4d172c73f708c61c2c0670b3cc049be30bdcb94
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-04 23:52:29 +0300

    Time: 181 ms (51.64%), Space: 177.8 MB (68.39%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -30,16 +30,60 @@
 //         return counter;
 //     }
 // };
+
+// total comp: O(m log m) + O(n log m) /and MEM comp for sorting : log m
+// class Solution {
+// public:
+//     int maxCount(vector<int>& banned, int n, int maxSum) {
+//         int bannedSize = banned.size();
+//         int counter = 0;
+//         sort(banned.begin(), banned.end()); //O(m log m)
+//         for (int j = 1; j <= n; j++) //o(n)
+//         {
+//             if (!(binary_search(banned.begin(),banned.end() ,j))) //O (log m)
+//             {
+//                 if (maxSum - j >= 0)
+//                 {
+//                     counter++;
+//                     maxSum -= j; //maxSum -[2+3 
+//                 }
+//             }
+//         }
+//         return counter;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int maxCount(vector<int>& banned, int n, int maxSum) {
+//         int bannedSize = banned.size();
+//         int counter = 0;
+//         sort(banned.begin(), banned.end()); //O(m log m)
+//         for (int j = 1; j <= n; j++) //o(n)
+//         {
+//             if (!(binary_search(banned.begin(),banned.end() ,j))) //O (log m)
+//             {
+//                 if (maxSum - j >= 0)
+//                 {
+//                     counter++;
+//                     maxSum -= j; //maxSum -[2+3 
+//                 }
+//             }
+//         }
+//         return counter;
+//     }
+// };
+
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         int bannedSize = banned.size();
         int counter = 0;
-        sort(banned.begin(), banned.end());
+        unordered_set<int> un_set(banned.begin(), banned.end());
         for (int j = 1; j <= n; j++) //n
         {
-            
-            if (!(binary_search(banned.begin(),banned.end() ,j)))
+
+            if (! un_set.count(j))
             {
                 if (maxSum - j >= 0)
                 {
