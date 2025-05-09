b0f1926a8ff00f9b13fedec8964f0f513f4ca1f0
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-09 23:57:36 +0300

    Time: 0 ms (100%), Space: 13.6 MB (65.41%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -67,17 +67,18 @@
 //     }
 // };
 
+//[best solution]
 // total Comp: O(N * log n) with caring with differnt complexity for sorting in different languages
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         sort(arr.begin(), arr.end());
 
-        for (int i = 0; i < arr.size(); i++)
+        for (int i = 0; i < arr.size(); i++) //n
         {
-            if (binary_search(arr.begin(), arr.begin() + i, arr[i] * 2))
+            if (binary_search(arr.begin(), arr.begin() + i, arr[i] * 2))//log n
                 return true;
-            if (binary_search(arr.begin()+i+1, arr.end(), arr[i] * 2))
+            if (binary_search(arr.begin()+i+1, arr.end(), arr[i] * 2))//log n
                 return true;
 
         }
