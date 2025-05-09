3625cc581958be3c351f09f9b793d06f0fa2f5e6
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-09 23:35:55 +0300

    Time: 0 ms (100%), Space: 14 MB (20.37%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,37 +1,49 @@
+// class Solution {
+// public:
+//     bool checkIfExist(vector<int>& arr) {
+//         sort(arr.begin(), arr.end());
+//         int i = 0;
+//         for (int j = arr.size() - 1; j >= 0; j--)
+//         {
+//             /*if (arr.end()-1 == arr.begin() + j )
+//                 cout << "TRUE";*/
+//                 int half=(arr[j] / 2); //to pass test case [0,-2,2]
+//                 if(half != arr[j])
+//                 {
+//                 if ((arr[j] %2==0) &&binary_search(arr.begin(), arr.begin() + j ,half ))
+//                 return true;    
+//     //wrong
+//     // if (i < arr.size())
+//     // {
+//     //     if ((arr[j] %2==0) && binary_search(arr.begin() + (i + 1), arr.end(), (arr[i] / 2)))
+//     //         return true;
+//     // }
+//                  }
+//     //wrong
+//     // if (binary_search(arr.begin(), arr.end(), arr[i] * 2)) {
+//     //     return true;
+//     //   }
+//             if (binary_search(arr.begin(), arr.end(), arr[i] * 2)) {
+//                 if (arr[i] != 0 || count(arr.begin(), arr.end(), 0) > 1)
+//                     return true;
+//             }
+//             i++;
+//         }
+//         return false;
+//     }
+// };
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int i = 0;
-        for (int j = arr.size() - 1; j >= 0; j--)
+        unordered_map <int, bool> unMap;
+        for (int i = 0; i < arr.size(); i++)
         {
-            /*if (arr.end()-1 == arr.begin() + j )
-                cout << "TRUE";*/
-                
-                int half=(arr[j] / 2); //to pass test case [0,-2,2]
-                if(half != arr[j])
-                {
-                if ((arr[j] %2==0) &&binary_search(arr.begin(), arr.begin() + j ,half ))
+            if (unMap.count(arr[i] * 2))
                 return true;
-
-                
-            
-            // if (i < arr.size())
-            // {
-            //     if ((arr[j] %2==0) && binary_search(arr.begin() + (i + 1), arr.end(), (arr[i] / 2)))
-            //         return true;
-            // }
-                }
-            // if (binary_search(arr.begin(), arr.end(), arr[i] * 2)) {
-            //     return true;
-            //   }
-            if (binary_search(arr.begin(), arr.end(), arr[i] * 2)) {
-                if (arr[i] != 0 || count(arr.begin(), arr.end(), 0) > 1)
-                    return true;
-            }
-                
-            i++;
+            if (arr[i]%2 ==0 && unMap.count(arr[i]/2))
+                return true;
+            unMap[arr[i]] = true;//to not check 2 times 
         }
-        return false;
+    return false;
     }
 };
