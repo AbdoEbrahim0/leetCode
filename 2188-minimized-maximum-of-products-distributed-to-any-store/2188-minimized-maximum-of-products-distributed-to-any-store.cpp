17f12d46df68420949f157ddd256111789c20757
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-10 14:56:15 +0300

    Time: 1486 ms (5.19%), Space: 106.6 MB (8.15%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,28 +1,56 @@
+//priorty ques and operator overloading
+
+// class Solution {
+//     struct  data
+//     {
+//         int quantity, stores;
+//         bool operator <(const data& other) const {
+//             return 1LL*quantity* other.stores <  1LL*stores* other.quantity;
+//         }
+//     };
+// public:
+//     int minimizedMaximum(int n, vector<int>& quantities) {
+//         priority_queue<data> pr;
+//         int m = quantities.size();
+//         for (int z = 0; z < m; z++)
+//         {
+//             pr.push({ quantities[z], 1 });
+//         }
+//         for (int i = 0; i < n - m; i++)
+//         {
+//             auto cur =pr.top() ;
+//             pr.pop();
+//             pr.push({ cur.quantity,cur.stores + 1 });
+//         }
+//         return ceil(1.0 * pr.top().quantity / pr.top().stores);
+//         //return ceil(pr.top().quantity / pr.top().stores);
+//     }
+// };
+
 class Solution {
-    struct  data
-    {
+    struct data {
         int quantity, stores;
-        bool operator <(const data& other) const {
-            return 1LL*quantity* other.stores <  1LL*stores* other.quantity;
-        }
     };
+
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        priority_queue<data> pr;
+        auto cmp = [](const data& a, const data& b) {
+            return 1LL * a.quantity * b.stores < 1LL * a.stores * b.quantity;
+        };
+        priority_queue<data, vector<data>, decltype(cmp)> pr(cmp);
+
         int m = quantities.size();
-        for (int z = 0; z < m; z++)
-        {
+        for (int z = 0; z < m; z++) {
             pr.push({ quantities[z], 1 });
         }
-        
-        for (int i = 0; i < n - m; i++)
-        {
-            auto cur =pr.top() ;
-            pr.pop();
-            pr.push({ cur.quantity,cur.stores + 1 });
 
+        for (int i = 0; i < n - m; i++) {
+            auto cur = pr.top();
+            pr.pop();
+            pr.push({ cur.quantity, cur.stores + 1 });
         }
-        return ceil(1.0 * pr.top().quantity / pr.top().stores);
-        //return ceil(pr.top().quantity / pr.top().stores);
+
+        auto top = pr.top();
+        return (top.quantity + top.stores - 1) / top.stores; // Ceil division without floating point
     }
 };
