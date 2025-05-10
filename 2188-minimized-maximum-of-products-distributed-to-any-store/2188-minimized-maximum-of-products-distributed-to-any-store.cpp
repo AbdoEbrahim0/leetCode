42aa60fe6a003143719600275b345d0246e6dc27
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-10 17:16:46 +0300

    Time: 67 ms (18.59%), Space: 88.5 MB (17.11%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,5 +1,5 @@
 //priorty ques and operator overloading
-
+//O(m) +O(n-m) *log*O(n-m)
 // class Solution {
 //     struct  data
 //     {
@@ -27,30 +27,89 @@
 //     }
 // };
 
-class Solution {
-    struct data {
-        int quantity, stores;
-    };
+// class Solution {
+//     struct data {
+//         int quantity, stores;
+//     };
 
+// public:
+//     int minimizedMaximum(int n, vector<int>& quantities) {
+//         auto cmp = [](const data& a, const data& b) {
+//             return 1LL * a.quantity * b.stores < 1LL * a.stores * b.quantity;
+//         };
+//         priority_queue<data, vector<data>, decltype(cmp)> pr(cmp);
+
+//         int m = quantities.size();
+//         for (int z = 0; z < m; z++) {
+//             pr.push({ quantities[z], 1 });
+//         }
+//         //When you push/pop in priority_queue, it will use your overloaded operator< for sorting automatically.
+//         for (int i = 0; i < n - m; i++) {
+//             auto cur = pr.top(); 
+//             pr.pop();
+//             pr.push({ cur.quantity, cur.stores + 1 }); //add 1 safley as we always store old res in cur which can add 1 every time and save change inside pr_queue always
+//         }
+//         return ceil(1.0 * pr.top().quantity / pr.top().stores);
+//     }
+// };
+
+//TLE
+// class Solution {
+// public:
+//     int minimizedMaximum(int n, vector<int>& quantities) {
+//         int sumQ = 0;
+//         for (int val : quantities)
+//             sumQ += val;
+//         int sizeQ = quantities.size();
+//         int mid = sumQ / n;
+//         int stores = 0;
+//         while (stores !=n)
+//         {
+//             stores = 0;
+//             for (int i = 0; i < sizeQ; i++)
+//             {
+//                 if (quantities[i] % mid != 0)
+//                     stores = stores + (quantities[i] / mid) + 1;
+//                 else
+//                     stores = stores + (quantities[i] / mid);
+//             }
+//             if (stores == n)
+//                 return mid;
+//             if (stores > n) mid++;
+//             else if (stores < n) mid--;
+//         }
+//         return mid;
+//     }
+// };
+
+class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        auto cmp = [](const data& a, const data& b) {
-            return 1LL * a.quantity * b.stores < 1LL * a.stores * b.quantity;
-        };
-        priority_queue<data, vector<data>, decltype(cmp)> pr(cmp);
+        sort(quantities.begin(), quantities.end());
+        int sizeQ = quantities.size();
+        int st = 1, end = quantities[sizeQ - 1];
+        int mid, cur;
+        int stores = 0;
+                while (st<=end)
+                {
+                    mid = (st + end) / 2;
+                    stores = 0;
+                    for (int i = 0; i < sizeQ; i++)
+                    {
+                    if (quantities[i] % mid != 0)
+                        stores = stores + (quantities[i] / mid) + 1;
+                    else
+                        stores = stores + (quantities[i] / mid);
+                    }
 
-        int m = quantities.size();
-        for (int z = 0; z < m; z++) {
-            pr.push({ quantities[z], 1 });
-        }
+                    if (stores <= n)
+                    {
+                        cur = mid;
+                        end = mid - 1;
+                    }else 
+                        st = mid + 1;
 
-        for (int i = 0; i < n - m; i++) {
-            auto cur = pr.top();
-            pr.pop();
-            pr.push({ cur.quantity, cur.stores + 1 });
-        }
-
-        //auto top = pr.top();
-             return ceil(1.0 * pr.top().quantity / pr.top().stores);
+                }
+                return cur;
     }
 };
