0e9ba230780427207b544d2335bd6c935e84eced
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-16 23:38:30 +0300

    Time: 0 ms (100%), Space: 12.7 MB (100%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,25 +1,46 @@
+// //priorty queue
+// class Solution {
+// public:
+//     long long pickGifts(vector<int>& gifts, int k) {
+//         priority_queue<int ,vector<int>,less<>> pr;
+//         for (int i = 0; i < gifts.size(); i++)
+//         {
+//             pr.push(gifts[i]);
+
+//         }
+//         long long val;
+//         while ((k--) >= 1)
+//         {
+//             val= sqrt(pr.top());
+//             pr.pop();
+//             pr.push(val);
+//         }
+//         val = 0;
+//         while ( ! pr.empty())
+//         {
+//             val += pr.top();
+//             pr.pop();
+//         }
+//         return val;
+//     }
+// };
+
+//heap
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        priority_queue<int ,vector<int>,less<>> pr;
-        for (int i = 0; i < gifts.size(); i++)
+        make_heap(gifts.begin(), gifts.end());
+        while (k-- )
         {
-            pr.push(gifts[i]);
-
+            // pop_heap(gifts.begin(),gifts.end());
+            // gifts.back() = sqrt(gifts.back());
+            
+            pop_heap(gifts.begin(), gifts.end());
+            // Take the square root of the largest element
+            gifts.back() = sqrt(gifts.back());
+            // Push the modified element back into the heap
+            push_heap(gifts.begin(), gifts.end());
         }
-        long long val;
-        while ((k--) >= 1)
-        {
-            val= sqrt(pr.top());
-            pr.pop();
-            pr.push(val);
-        }
-        val = 0;
-        while ( ! pr.empty())
-        {
-            val += pr.top();
-            pr.pop();
-        }
-        return val;
+        return accumulate(gifts.begin(),gifts.end(),0LL);
     }
 };
