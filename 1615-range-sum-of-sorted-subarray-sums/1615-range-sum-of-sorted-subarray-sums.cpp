49a084db7528500cfde002071ad32aa6da02dadb
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-17 13:49:24 +0300

    Time: 291 ms (5.17%), Space: 43.6 MB (5.39%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,36 +1,68 @@
+// class Solution {
+// public:
+//     int rangeSum(vector<int>& nums, int n, int left, int right) {
+
+//         //int sum = 0;
+//         int* arr=new int[n];
+//         arr[0] = nums[0];
+//         //vector<int > combutedSubArray(n * (n + 1) / 2);
+//         vector<int> combutedSubArray;
+//         combutedSubArray.emplace_back(arr[0]);
+//         for (int i = 1; i < n; i++)
+//         {
+//             arr[i] = arr[i - 1] + nums[i];
+//             combutedSubArray.emplace_back(arr[i]);
+//         }
+//         for (int z =n- 1; z >= 0; z--)
+//         {
+//             int j = z - 1;
+//             while (j >= 0)
+//             {
+//                 combutedSubArray.emplace_back(arr[z] - arr[j]);
+//                 j--;
+//             }
+        
+//         }
+//         sort(combutedSubArray.begin(), combutedSubArray.end());
+//         const long long ONE_BILLION = 1e9;
+//         return accumulate(combutedSubArray.begin() + left-1, combutedSubArray.begin() + right, 0LL) % (ONE_BILLION +7);
+        
+//     }
+// };
+
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
 
-        //int sum = 0;
-        int* arr=new int[n];
+
+        int* arr = new int[n];
         arr[0] = nums[0];
-        //vector<int > combutedSubArray(n * (n + 1) / 2);
-        vector<int> combutedSubArray;
-        combutedSubArray.emplace_back(arr[0]);
-        for (int i = 1; i < n; i++)
+        priority_queue<int, vector<int>, greater<>>pr;
+        pr.push(arr[0]);
+        for (int i = 1; i < n; i++)//O(n)
         {
             arr[i] = arr[i - 1] + nums[i];
-            combutedSubArray.emplace_back(arr[i]);
+            pr.push(arr[i]);
         }
-        
-        /*for (int i = 0; i < 4; i++)
-        {
-            cout << arr[i] << endl;
-        }*/
-        for (int z =n- 1; z >= 0; z--)
+
+        for (int z = n - 1; z >= 0; z--)//O(n) *O(n--) O(n^2)
         {
             int j = z - 1;
-            while (j >= 0)
+            while (j >= 0)// O(n--)
             {
-                combutedSubArray.emplace_back(arr[z] - arr[j]);
+                pr.push(arr[z] - arr[j]);
                 j--;
             }
-        
+
         }
-        sort(combutedSubArray.begin(), combutedSubArray.end());
+        vector<int >combutedSubArray;
+        while (!pr.empty())//O(size of subarray)
+        {
+            combutedSubArray.emplace_back(pr.top());
+            pr.pop();
+        }
         const long long ONE_BILLION = 1e9;
-        return accumulate(combutedSubArray.begin() + left-1, combutedSubArray.begin() + right, 0LL) % (ONE_BILLION +7);
-        
+        return accumulate(combutedSubArray.begin() + left - 1, combutedSubArray.begin() + right, 0LL) % (ONE_BILLION + 7);
+
     }
 };
