ed363d94a2aa817cbd6edf32278d95f73e2b47c3
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-17 14:10:59 +0300

    Time: 86 ms (77.16%), Space: 12.9 MB (82.97%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,3 +1,4 @@
+// //O(n^2log n)
 // class Solution {
 // public:
 //     int rangeSum(vector<int>& nums, int n, int left, int right) {
@@ -29,40 +30,76 @@
         
 //     }
 // };
+// //O(n^2 log n^2)
+// class Solution {
+// public:
+//     int rangeSum(vector<int>& nums, int n, int left, int right) {
+
+
+//         int* arr = new int[n];
+//         arr[0] = nums[0];
+//         priority_queue<int, vector<int>, greater<>>pr;
+//         pr.push(arr[0]);
+//         for (int i = 1; i < n; i++)//O(n)
+//         {
+//             arr[i] = arr[i - 1] + nums[i];
+//             pr.push(arr[i]);
+//         }
+
+//         for (int z = n - 1; z >= 0; z--)//O(n) *O(n--) O(n^2)
+//         {
+//             int j = z - 1;
+//             while (j >= 0)// O(n--)
+//             {
+//                 pr.push(arr[z] - arr[j]);
+//                 j--;
+//             }
+
+//         }
+//         vector<int >combutedSubArray;
+//         while (!pr.empty())//O(size of subarray)
+//         {
+//             combutedSubArray.emplace_back(pr.top());
+//             pr.pop();
+//         }
+//         const long long ONE_BILLION = 1e9;
+//         return accumulate(combutedSubArray.begin() + left - 1, combutedSubArray.begin() + right, 0LL) % (ONE_BILLION + 7);
+
+//     }
+// };
 
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
+        const int MOD = 1e9 + 7;
+        using T = tuple<int, int, int>; // (sum, start_index, next_index)
 
+        priority_queue<T, vector<T>, greater<T>> minHeap;
 
-        int* arr = new int[n];
-        arr[0] = nums[0];
-        priority_queue<int, vector<int>, greater<>>pr;
-        pr.push(arr[0]);
-        for (int i = 1; i < n; i++)//O(n)
-        {
-            arr[i] = arr[i - 1] + nums[i];
-            pr.push(arr[i]);
+        // Initialize: each element nums[i] is a subarray starting and ending at i
+        for (int i = 0; i < n; ++i) {
+            minHeap.emplace(nums[i], i, i + 1);
         }
 
-        for (int z = n - 1; z >= 0; z--)//O(n) *O(n--) O(n^2)
-        {
-            int j = z - 1;
-            while (j >= 0)// O(n--)
-            {
-                pr.push(arr[z] - arr[j]);
-                j--;
+        long long result = 0;
+        int count = 0;
+
+        while (count < right) {
+            auto [sum, start, next] = minHeap.top();
+            minHeap.pop();
+            count++;
+
+            if (count >= left) {
+                result = (result + sum) % MOD;
             }
 
+            // If next < n, push the next subarray starting from 'start'
+            if (next < n) {
+                int newSum = sum + nums[next];
+                minHeap.emplace(newSum, start, next + 1);
+            }
         }
-        vector<int >combutedSubArray;
-        while (!pr.empty())//O(size of subarray)
-        {
-            combutedSubArray.emplace_back(pr.top());
-            pr.pop();
-        }
-        const long long ONE_BILLION = 1e9;
-        return accumulate(combutedSubArray.begin() + left - 1, combutedSubArray.begin() + right, 0LL) % (ONE_BILLION + 7);
 
+        return result;
     }
 };
