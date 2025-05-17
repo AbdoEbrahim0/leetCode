39a753975f8824154b0c62fc68d4bdeb94c35119
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-17 14:20:50 +0300

    Time: 87 ms (76.51%), Space: 26.9 MB (74.14%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -2,7 +2,6 @@
 // class Solution {
 // public:
 //     int rangeSum(vector<int>& nums, int n, int left, int right) {
-
 //         //int sum = 0;
 //         int* arr=new int[n];
 //         arr[0] = nums[0];
@@ -22,14 +21,34 @@
 //                 combutedSubArray.emplace_back(arr[z] - arr[j]);
 //                 j--;
 //             }
-        
 //         }
-//         sort(combutedSubArray.begin(), combutedSubArray.end());
+//         sort(combutedSubArray.begin(), combutedSubArray.end()); //O(n^2 log n^2)
 //         const long long ONE_BILLION = 1e9;
 //         return accumulate(combutedSubArray.begin() + left-1, combutedSubArray.begin() + right, 0LL) % (ONE_BILLION +7);
-        
 //     }
 // };
+
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        
+        vector<int>combutedSubArray;
+        for (int i = 0; i < n; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < n; j++)
+            {
+                sum += nums[j];
+                combutedSubArray.emplace_back(sum);
+            }
+        }
+        
+        sort(combutedSubArray.begin(), combutedSubArray.end());
+        const int mod= 1e9+7;
+        return accumulate(combutedSubArray.begin() + left-1, combutedSubArray.begin() + right, 0LL) % mod;
+        
+    }
+};
 // //O(n^2 log n^2)
 // class Solution {
 // public:
@@ -68,38 +87,38 @@
 //     }
 // };
 
-class Solution {
-public:
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
-        const int MOD = 1e9 + 7;
-        using T = tuple<int, int, int>; // (sum, start_index, next_index)
+// class Solution {
+// public:
+//     int rangeSum(vector<int>& nums, int n, int left, int right) {
+//         const int MOD = 1e9 + 7;
+//         using T = tuple<int, int, int>; // (sum, start_index, next_index)
 
-        priority_queue<T, vector<T>, greater<T>> minHeap;
+//         priority_queue<T, vector<T>, greater<T>> minHeap;
 
-        // Initialize: each element nums[i] is a subarray starting and ending at i
-        for (int i = 0; i < n; ++i) {
-            minHeap.emplace(nums[i], i, i + 1);
-        }
+//         // Initialize: each element nums[i] is a subarray starting and ending at i
+//         for (int i = 0; i < n; ++i) {
+//             minHeap.emplace(nums[i], i, i + 1);
+//         }
 
-        long long result = 0;
-        int count = 0;
+//         long long result = 0;
+//         int count = 0;
 
-        while (count < right) {
-            auto [sum, start, next] = minHeap.top();
-            minHeap.pop();
-            count++;
+//         while (count < right) {
+//             auto [sum, start, next] = minHeap.top();
+//             minHeap.pop();
+//             count++;
 
-            if (count >= left) {
-                result = (result + sum) % MOD;
-            }
+//             if (count >= left) {
+//                 result = (result + sum) % MOD;
+//             }
 
-            // If next < n, push the next subarray starting from 'start'
-            if (next < n) {
-                int newSum = sum + nums[next];
-                minHeap.emplace(newSum, start, next + 1);
-            }
-        }
+//             // If next < n, push the next subarray starting from 'start'
+//             if (next < n) {
+//                 int newSum = sum + nums[next];
+//                 minHeap.emplace(newSum, start, next + 1);
+//             }
+//         }
 
-        return result;
-    }
-};
+//         return result;
+//     }
+// };
