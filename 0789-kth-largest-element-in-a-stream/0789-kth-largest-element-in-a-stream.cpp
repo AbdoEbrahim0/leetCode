6b423a00db00d92fb045876e7be0ad992cf6199d
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-18 13:15:53 +0300

    Time: 996 ms (5.13%), Space: 37.1 MB (5.36%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -27,45 +27,74 @@
 
 //without change default multiset
 // class KthLargest {
+//     int k2;
+//     priority_queue<int,vector<int> ,greater<int> >pr;
 // public:
-//     multiset<int , std::greater<>> myMultiSet;
-//     int index;
 //     KthLargest(int k, vector<int>& nums) {
-//         for (int& val : nums)
-//             myMultiSet.insert(val);
-//         int mulSetSize = myMultiSet.size();
-//         index = mulSetSize - k;// index 0 based so minus 1
-        
+//         k2=k;
+//         for(int val : nums)
+//         pr.push (val);
+//         while (pr.size() >k2) 
+//         pr.pop();
 //     }
+    
+//     int add(int val) {
+//         pr.push (val);
+//         if(pr.size() >k2)
+//         pr.pop();
 
-//      int add(int val) {
-//         myMultiSet.insert(val);
-//         index++;
-//         auto it = myMultiSet.begin();
-//         if (index >= 0 && index < myMultiSet.size()) {            
-//             std::advance(it, index);  // Move to the element at index
-//         }   
-//         return *it;
+//         return pr.top();
 //     }
 // };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
 
 class KthLargest {
 
 public:
     //priority_queue <int, vector<int>, less<int>> pr;
-    multiset<int, std::greater<>> myMultiSet;
-    int k;
+    //vector<int>result;
+    multiset<int> myMultiSet;
+    int index;
     KthLargest(int k, vector<int>& nums) {
         for (int& val : nums)
             myMultiSet.insert(val);
         int mulSetSize = myMultiSet.size();
-        this->k= k-1;
+        index = mulSetSize - k;// index 0 based so minus 1   
     }
-
      int add(int val) {
         myMultiSet.insert(val);
+        index++;
         auto it = myMultiSet.begin();
-        std::advance(it, k);  // Move to the element at index
+        if (index >= 0 && index < myMultiSet.size()) {
+            std::advance(it, index);  // Move to the element at index
+        }
+        
         return *it;
     }
+
 };
+// class KthLargest {
+
+// public:
+//     //priority_queue <int, vector<int>, less<int>> pr;
+//     multiset<int, std::greater<>> myMultiSet;
+//     int k;
+//     KthLargest(int k, vector<int>& nums) {
+//         for (int& val : nums)
+//             myMultiSet.insert(val);
+//         int mulSetSize = myMultiSet.size();
+//         this->k= k-1;
+//     }
+
+//      int add(int val) {
+//         myMultiSet.insert(val);
+//         auto it = myMultiSet.begin();
+//         std::advance(it, k);  // Move to the element at index
+//         return *it;
+//     }
+// };
