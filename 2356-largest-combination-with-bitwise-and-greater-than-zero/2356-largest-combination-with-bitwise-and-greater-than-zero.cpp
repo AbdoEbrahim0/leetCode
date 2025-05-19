d53ededfc2d71a4eb802ddea988d15b2c67b9bae
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-19 22:07:17 +0300

    Time: 32 ms (57.42%), Space: 61.4 MB (49.47%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -28,26 +28,46 @@
 //     }
 // };
 
+//[optimized] //change number of bit from 32 to 24 as max number can be 10^7
+// class Solution {
+// public:
+//     int largestCombination(vector<int>& candidates) {
+//         vector<bitset<24>> Bin;
+//         for (int& val : candidates)
+//         {
+//             Bin.emplace_back(bitset<24>(val));
+//         }
+//         vector<int>ones(24);
+//         int maxi = 0;
+//             for (int i = 0; i < 24; i++)
+//             {
+//                 for (bitset<24>& val : Bin)
+//                 {
+//                     if(val[i]&1)
+//                     ones[i]++;
+//                 }
+//                 maxi = max(ones[i], maxi);
+//             }
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        vector<bitset<24>> Bin;
+        vector<int>ones(32);
+        int maxi = 0;
         for (int& val : candidates)
         {
-            Bin.emplace_back(bitset<24>(val));
-        }
-//change number of bit from 32 to 24 as max number can be 10^7
-        vector<int>ones(24);
-        int maxi = 0;
-            for (int i = 0; i < 24; i++)
+            bitset<32> bits (val);
+            for (int i = 0; i < 32; i++)
             {
-                for (bitset<24>& val : Bin)
-                {
-                    if(val[i]&1)
-                    ones[i]++;
-                }
-                maxi = max(ones[i], maxi);
+                if(bits[i])
+                ones[i]++;
+                //maxi = max(ones[i], maxi);
             }
+        }
+        maxi = *max_element(ones.begin(), ones.end());
         return maxi;
     }
 };
