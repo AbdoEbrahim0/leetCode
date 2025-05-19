20e0620c3ba8bf9595df3d48c4acbe10637a4aa9
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-19 21:33:10 +0300

    Time: 53 ms (7.95%), Space: 72.5 MB (4.77%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,9 +1,36 @@
+// class Solution {
+// public:
+//     int largestCombination(vector<int>& candidates) {
+
+//         int res = candidates[0];
+//         vector<int> anding;
+//         vector<bitset<32>> Bin;
+//         for (int& val : candidates)
+//         {
+//             Bin.emplace_back(bitset<32>(val));
+//         }
+
+//         vector<int>ones(32);
+//         int maxi = 0;
+//             for (int i = 0; i < 32; i++)
+//             {
+//                 for (int x = 0; x < candidates.size(); x++)
+//                 {
+
+//                     if(Bin[x][i]==1)
+//                     ones[i]++;
+//                 }
+//                 maxi = max(ones[i], maxi);
+//             }
+        
+        
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-
-        int res = candidates[0];
-        vector<int> anding;
         vector<bitset<32>> Bin;
         for (int& val : candidates)
         {
@@ -14,16 +41,13 @@ public:
         int maxi = 0;
             for (int i = 0; i < 32; i++)
             {
-                for (int x = 0; x < candidates.size(); x++)
+                for (bitset<32>& val : Bin)
                 {
-
-                    if(Bin[x][i]==1)
+                    if(val[i]&1)
                     ones[i]++;
                 }
                 maxi = max(ones[i], maxi);
             }
-        
-        
         return maxi;
     }
 };
