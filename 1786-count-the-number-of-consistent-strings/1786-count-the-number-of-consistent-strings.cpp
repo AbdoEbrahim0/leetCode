e97e01e3c6b88510e43d0d1a7eca918339eb0a95
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-20 11:59:29 +0300

    Time: 46 ms (54.61%), Space: 34.1 MB (17.14%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -1,29 +1,52 @@
+//Unordered Set	times:O(n * m)	space: O(26)
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        bitset<26>visted;
-        for (char& ch : allowed)
-        {
-            visted[ch - 'a'] = 1;
-        }
-        bool valid = true;
-        int counter = 0;
-        for (const string &word : words)
-        {
-            valid = 1;
-            for (const char &ch : word)
-            {
-                if (visted[ch - 'a'] == false)
-                {
-                    valid = 0;
+        unordered_set<char> allowedChars(allowed.begin(), allowed.end());
+        int count = 0;
+        
+        for (const string& word : words) {//O(n)
+            bool isValid = true;
+            for (char ch : word) {
+                if (allowedChars.find(ch) == allowedChars.end()) { //O(m)
+                    isValid = false;
                     break;
                 }
             }
-            if (valid)
-                counter++;
-
+            if (isValid) {
+                count++;
+            }
         }
-        return counter;
+        return count;
     }
-
 };
+// //Bitset	 times: O(n * m)	space: O(1)
+// class Solution {
+// public:
+//     int countConsistentStrings(string allowed, vector<string>& words) {
+//         bitset<26>visted;
+//         for (const char& ch : allowed)
+//         {
+//             visted[ch - 'a'] = 1;
+//         }
+//         bool valid = true;
+//         int counter = 0;
+//         for (const string &word : words)
+//         {
+//             valid = 1;
+//             for (const char &ch : word)
+//             {
+//                 if (visted[ch - 'a'] == false)
+//                 {
+//                     valid = 0;
+//                     break;
+//                 }
+//             }
+//             if (valid)
+//                 counter++;
+
+//         }
+//         return counter;
+//     }
+
+// };
