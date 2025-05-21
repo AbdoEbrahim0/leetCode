6323579389b8f1b49850f5ff466b88575e943963
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-22 00:19:48 +0300

    Time: 8 ms (6.69%), Space: 86.1 MB (72.44%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,43 +1,43 @@
-// class Solution {
-// public:
-//     int longestSubarray(vector<int>& nums) {      
-//         int counter = 1;
-//         int resOfAnding = nums[0];
-//         int maxi = 0;
-//         int maxElement=*max_element(nums.begin(),nums.end());
-//         for (int i = 1; i < nums.size(); i++)
-//         {
-//             if ( (resOfAnding & nums[i])>=maxElement)
-//             {
-//                 resOfAnding = resOfAnding & nums[i];
-//                 counter++;
-//                 maxi = max(maxi, counter);
-//             }
-//             else
-//             {
-//                 resOfAnding = nums[i];
-//                 counter = 1;
-//             }
-//         }
-//         return maxi = max(maxi, counter);
-//     }
-// };
-
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
-        int maxElement = *max_element(nums.begin(), nums.end());
-        int maxLength = 0, currentLength = 0;
-        
-        for (int &num : nums) {
-            if (num == maxElement) {
-                currentLength++;
-                maxLength = max(maxLength, currentLength);
-            } else {
-                currentLength = 0;
+    int longestSubarray(vector<int>& nums) {      
+        int counter = 1;
+        int resOfAnding = nums[0];
+        int maxi = 0;
+        int maxElement=*max_element(nums.begin(),nums.end());
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if ( (resOfAnding & nums[i])>=maxElement)
+            {
+                resOfAnding = resOfAnding & nums[i];
+                counter++;
+                maxi = max(maxi, counter);
+            }
+            else
+            {
+                resOfAnding = nums[i];
+                counter = 1;
             }
         }
-        
-        return maxLength;
+        return maxi = max(maxi, counter);
     }
 };
+
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int maxElement = *max_element(nums.begin(), nums.end());
+//         int maxLength = 0, currentLength = 0;
+        
+//         for (int &num : nums) {
+//             if (num == maxElement) {
+//                 currentLength++;
+//                 maxLength = max(maxLength, currentLength);
+//             } else {
+//                 currentLength = 0;
+//             }
+//         }
+        
+//         return maxLength;
+//     }
+// };
