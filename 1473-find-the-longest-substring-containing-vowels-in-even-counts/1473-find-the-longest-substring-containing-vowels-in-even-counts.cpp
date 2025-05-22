974fd2bec49c6eab9d03038362966d6989f1361a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-22 23:28:50 +0300

    Time: 39 ms (38.14%), Space: 19.3 MB (46.74%) - LeetHub

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,87 @@
+// class Solution {
+// public:
+//     int findTheLongestSubstring(string s) {
+//         int occuranceOFVowels[26] = {};
+//         bool checkCharVowel[26] = {};
+//         for (char ch : s)
+//         {
+//             if (ch == 'a' || ch == 'o' || ch == 'i' || ch == 'e' || ch == 'u')
+//             {
+//                 occuranceOFVowels[ch - 'a']++;
+//                 checkCharVowel[ch - 'a'] = true;
+//             }
+//         }
+//         int LongestSubString = 0;
+//         int len = 0;//a=97 e=101 i=105 o=111 u=117  a=0 e =4 i=8 o =14 u=20
+//         string subString = "";
+//         for (char ch : s)
+//         {
+//             if ( (subString.size() )   ||  ( occuranceOFVowels[0] >= 2 || occuranceOFVowels[4] >= 2 || occuranceOFVowels[8] >= 2 ||
+//                 occuranceOFVowels[14] >= 2 || occuranceOFVowels[20] >= 2 ))
+//             {
+//                         if (checkCharVowel[ch - 'a'] == false)
+//                         {
+//                             //len++;
+//                             subString += ch;
+//                         }
+//                         else if (checkCharVowel[ch - 'a'])
+//                         {
+//                             //len++;
+//                             subString += ch;
+//                             occuranceOFVowels[ch - 'a']--;
+//                             if (occuranceOFVowels[ch - 'a'] == 0)
+//                             {
+//                                 LongestSubString = max(LongestSubString, ((int)subString.size() - 1)); //dont take it
+//                                 //LongestSubString = max(LongestSubString, (len-1)); //dont take it
+//                                 //len = 0;
+//                                 if(!( 
+//                                     (occuranceOFVowels[0] >= 2 || occuranceOFVowels[4] >= 2 
+//                                     || occuranceOFVowels[8] >= 2 ||occuranceOFVowels[14] >= 2
+//                                     || occuranceOFVowels[20] >= 2)))
+//                                         {subString = "";}
+//                             }
+//                         }
+//             }
+//             else
+//                 break;
+//             //                LongestSubString = max(LongestSubString, len);
+//             LongestSubString = max(LongestSubString, (int)subString.size());             
+//         }
+//         return LongestSubString;
+//     }
+// };
+
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        bool checkCharVowel[26] = {};
+        for (char ch : s)
+        {
+            if (ch == 'a' || ch == 'o' || ch == 'i' || ch == 'e' || ch == 'u')
+            {
+                checkCharVowel[ch - 'a'] = true;
+            }
+        }
+
+        int mask = 0;
+        unordered_map<int, int>mp;
+        mp[mask] = -1;
+        int ans = 0;
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (checkCharVowel[s[j] - 'a'])
+            {
+                int idx = s[j] - 'a';
+                mask ^= (1 << idx);
+            }
+            if (mp.find(mask) == mp.end())
+                mp[mask]=j;
+            else
+            {
+                ans = max(ans, j - mp[mask]);
+            }
+        }
+        return ans;
+
+    }
+};
