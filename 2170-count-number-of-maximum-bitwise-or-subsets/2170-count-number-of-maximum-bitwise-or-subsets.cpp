e5682fc677b46e367f79c6fc1669227bd287ce26
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-24 21:56:24 +0300

    Time: 1317 ms (6.98%), Space: 423.1 MB (7.85%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,5 +1,47 @@
+// //using mask
+// class Solution {
+// public:
+//     int countMaxOrSubsets(vector<int>& nums) {
+
+//         int maxOr = 0;
+//         for (int &val : nums)
+//         {
+//             maxOr |= val;
+//         }
+//         short n = nums.size();
+//         int counter = 0;
+//         for (int i = 0; i < (1 << n); i++)
+//         {
+//             int mask = i;
+//             int curOr = 0;
+//             for (int j = 0; j < n; j++)
+//             {
+//                 if (mask & 1)
+//                 {
+//                     curOr |= nums[j];
+//                 }
+//                 mask = mask >> 1; // mask= mask/2
+//             }
+            
+//             counter += (curOr == maxOr);
+//         }
+//         return counter;
+//     }
+// };
+//using recursion
+
 class Solution {
 public:
+    int solve (int idx,int curOr,vector<int> nums,int sizeOfArray,int maxOr)
+    {
+        if(idx==sizeOfArray)
+        return maxOr==curOr;
+        int first=solve(idx+1 ,curOr,nums,sizeOfArray,maxOr) ;
+        int second =solve(idx+1 ,curOr | nums[idx],nums,sizeOfArray,maxOr);
+        return first + second;
+        
+
+    }
     int countMaxOrSubsets(vector<int>& nums) {
 
         int maxOr = 0;
@@ -7,23 +49,7 @@ public:
         {
             maxOr |= val;
         }
-        short n = nums.size();
-        int counter = 0;
-        for (int i = 0; i < (1 << n); i++)
-        {
-            int mask = i;
-            int curOr = 0;
-            for (int j = 0; j < n; j++)
-            {
-                if (mask & 1)
-                {
-                    curOr |= nums[j];
-                }
-                mask = mask >> 1; // mask= mask/2
-            }
-            
-            counter += (curOr == maxOr);
-        }
-        return counter;
+        int n=nums.size();
+        return solve(0,0,nums,n,maxOr); 
     }
 };
