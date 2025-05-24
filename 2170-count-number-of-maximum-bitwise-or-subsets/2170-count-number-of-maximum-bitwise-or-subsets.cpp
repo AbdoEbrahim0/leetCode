35ce9b2206668d28b7722d4e540509da920292a4
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-24 23:49:49 +0300

    Time: 27 ms (52.37%), Space: 20.3 MB (22.14%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -30,25 +30,50 @@
 // };
 //using recursion
 
+// class Solution {
+// public:
+//     int solve (short idx,int curOr,vector<int> &nums,short sizeOfArray,int maxOr)
+//     {
+//         if(idx==sizeOfArray)
+//         return maxOr==curOr;
+//         // int first=solve(idx+1 ,curOr,nums,sizeOfArray,maxOr) ;
+//         // int second =solve(idx+1 ,curOr | nums[idx],nums,sizeOfArray,maxOr);
+//         // return first + second;
+//         return solve(idx+1 ,curOr,nums,sizeOfArray,maxOr)  + solve(idx+1 ,curOr | nums[idx],nums,sizeOfArray,maxOr);
+//     }
+//     int countMaxOrSubsets(vector<int>& nums) {
+
+//         int maxOr = 0;
+//         for (int &val : nums)
+//         {
+//             maxOr |= val;
+//         }
+//         short n=nums.size();
+//         return solve(0,0,nums,n,maxOr); 
+//     }
+// };
+//recursion +memomization
 class Solution {
 public:
-    int solve (short idx,int curOr,vector<int> &nums,short sizeOfArray,int maxOr)
+    int dp[17][1 << 17];
+
+    int solve(short idx, int curOr, vector<int>& nums, short sizeOfArray, int maxOr)
     {
-        if(idx==sizeOfArray)
-        return maxOr==curOr;
-        // int first=solve(idx+1 ,curOr,nums,sizeOfArray,maxOr) ;
-        // int second =solve(idx+1 ,curOr | nums[idx],nums,sizeOfArray,maxOr);
-        // return first + second;
-        return solve(idx+1 ,curOr,nums,sizeOfArray,maxOr)  + solve(idx+1 ,curOr | nums[idx],nums,sizeOfArray,maxOr);
+        if (idx == sizeOfArray)
+            return maxOr == curOr;
+        if (dp[idx][curOr] != -1)
+            return dp[idx][curOr];
+
+        return dp[idx][curOr] = solve(idx + 1, curOr, nums, sizeOfArray, maxOr) + solve(idx + 1, curOr | nums[idx], nums, sizeOfArray, maxOr);
     }
     int countMaxOrSubsets(vector<int>& nums) {
-
         int maxOr = 0;
-        for (int &val : nums)
+        memset(dp, -1, sizeof dp);
+        for (int& val : nums)
         {
             maxOr |= val;
         }
-        short n=nums.size();
-        return solve(0,0,nums,n,maxOr); 
+        short n = nums.size();
+        return solve(0, 0, nums, n, maxOr);
     }
 };
