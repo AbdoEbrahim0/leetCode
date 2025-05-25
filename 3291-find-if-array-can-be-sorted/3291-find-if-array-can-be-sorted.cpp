439e96d067ebbefa566588dab59c42d074b6eba6
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-25 23:58:12 +0300

    Time: 4 ms (35.41%), Space: 34.5 MB (8.69%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -32,6 +32,37 @@
 //         return true;
 //     }
 // };
+//O(n) +O(n* n log n)
+// class Solution {
+// public:
+//     bool canSortArray(vector<int>& nums) {
+//         unordered_map <int, int>mp;
+//         for (int& val : nums)//o(log2(1e8) ) =O(26.5) *O(n)
+//         {
+//             int x = val;
+//             int ct = 0;
+//             while (x != 0)
+//             {
+//                 if (x & 1) ct++;
+//                 x = x >> 1; //shift right
+//             }
+//             mp[val] = ct;
+//         }
+//         //divide into segments to sort O(n * n log n)
+//         int n = nums.size();
+//         int last = 0;
+//         for (int i = 0; i < n-1 ; i++) //o(n)
+//         {
+//                 if (mp[nums[i]] != mp[nums[i + 1]])
+//                 {
+//                     sort(nums.begin() + last, nums.begin() + i+1);// o(n log n)
+//                     last = i+1;
+//                 }
+//         }
+//         sort(nums.begin() + last, nums.end());// o(n log n)
+//         return is_sorted(nums.begin(),nums.end());
+//     }
+// };
 
 class Solution {
 public:
@@ -51,16 +82,26 @@ public:
         //divide into segments to sort O(n log n)
         int n = nums.size();
         int last = 0;
-        for (int i = 0; i < n-1 ; i++) //o(n)
+        vector<pair<int, int>> min_max;
+        int mini = nums[0] ,maxi = nums[0];
+        for (int i = 1; i < n ; i++) //o(n)
         {
-                if (mp[nums[i]] != mp[nums[i + 1]])
-                {
-                    sort(nums.begin() + last, nums.begin() + i+1);// o(n log n)
-                    last = i+1;
-                }
+            if (mp[nums[i]] != mp[nums[i - 1]])
+            {
+                
+                min_max.push_back({ mini,maxi });
+                mini = nums[i], maxi = nums[i];
+            }
+            else
+                mini = min(mini, nums[i]), maxi = max(maxi, nums[i]);
         }
-        sort(nums.begin() + last, nums.end());// o(n log n)
-
-        return is_sorted(nums.begin(),nums.end());
+        min_max.push_back({ mini,maxi });
+        for (int i = 0; i < min_max.size() -1; i++)
+        {
+            //2 4 8 | 15 30  >>min =2 max=8  |  min =15 max=30
+            if (min_max[i].second > min_max[i + 1].first) 
+                return false;
+        }
+        return true ;
     }
 };
