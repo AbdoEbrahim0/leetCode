d7ec5c822415aa6f882dd77d7ac01ab05c2c2478
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-25 23:25:57 +0300

    Time: 8 ms (20.04%), Space: 33.6 MB (12.25%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,8 +1,43 @@
+//total O(n^2) *O(n)
+// class Solution {
+// public:
+//     bool canSortArray(vector<int>& nums) {
+//         unordered_map <int, int>mp;
+//         for (int& val : nums)//o(1e8) =O(26.5) *O(n)
+//         {
+//             int x = val;
+//             int ct = 0;
+//             while (x != 0)
+//             {
+//                 if (x & 1) ct++;
+//                 x = x >> 1; //shift right
+//             }
+//             mp[val] = ct;
+//         }
+//         //bubble sort O(n^2)
+//         int n = nums.size();
+//         for (int i = 0; i < n - 1; i++)
+//         {
+//             for (int j = 0; j < n - i - 1; j++)
+//             {
+//                 if (nums[j] > nums[j + 1])
+//                 {
+//                     if (mp[nums[j]] == mp[nums[j+1]])
+//                         swap(nums[j], nums[j + 1]);
+//                     else
+//                         return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
         unordered_map <int, int>mp;
-        for (int& val : nums)
+        for (int& val : nums)//o(log2(1e8) ) =O(26.5) *O(n)
         {
             int x = val;
             int ct = 0;
@@ -13,21 +48,19 @@ public:
             }
             mp[val] = ct;
         }
-        //bubble sort
+        //divide into segments to sort O(n log n)
         int n = nums.size();
-        for (int i = 0; i < n - 1; i++)
+        int last = 0;
+        for (int i = 0; i < n-1 ; i++) //o(n)
         {
-            for (int j = 0; j < n - i - 1; j++)
-            {
-                if (nums[j] > nums[j + 1])
+                if (mp[nums[i]] != mp[nums[i + 1]])
                 {
-                    if (mp[nums[j]] == mp[nums[j+1]])
-                        swap(nums[j], nums[j + 1]);
-                    else
-                        return false;
+                    sort(nums.begin() + last, nums.begin() + i+1);// o(n log n)
+                    last = i+1;
                 }
-            }
         }
-        return true;
+        sort(nums.begin() + last, nums.end());// o(n log n)
+
+        return is_sorted(nums.begin(),nums.end());
     }
 };
