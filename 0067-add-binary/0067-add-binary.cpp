512aff3403c956f9b78370bd61ebc315b00e8f65
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-28 19:15:29 +0300

    Time: 0 ms (100%), Space: 9 MB (63.1%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,22 +1,52 @@
+// class Solution {
+// public:
+//     string addBinary(string a, string b) {
+//         string result;
+//         int i = a.size() - 1;
+//         int j = b.size() - 1;
+//         int carry = 0;
+
+//         while (i >= 0 || j >= 0 || carry) {
+//             int sum = carry;
+
+//             if (i >= 0) sum += a[i--] - '0';
+//             if (j >= 0) sum += b[j--] - '0';
+
+//             result += (sum % 2) + '0';
+//             carry = sum / 2;
+//         }
+
+//         reverse(result.begin(), result.end());
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string result;
+        
+        
+        int carry = 0;
+        if (!(a.size() > b.size()))
+            swap(a, b);
+        string result = a;
         int i = a.size() - 1;
         int j = b.size() - 1;
-        int carry = 0;
-
-        while (i >= 0 || j >= 0 || carry) {
-            int sum = carry;
-
+        int sum;
+        //    "100"              100
+        // "110010"           110010
+        //expected "110110"   110110
+        while (i >= 0 && j >= 0 || (i >= 0 && carry)  || (j >= 0 && carry) ) {
+            sum = carry;
             if (i >= 0) sum += a[i--] - '0';
             if (j >= 0) sum += b[j--] - '0';
-
-            result += (sum % 2) + '0';
+            result[1+i]= (sum % 2) + '0';
             carry = sum / 2;
         }
+        if ((a[0] - '0' & b[0] - '0' & carry) || (b[0] - '0' & carry) || (a[0] - '0' & carry))
+            result.insert(result.begin(), '1');
 
-        reverse(result.begin(), result.end());
+        //reverse(result.begin(), result.end());
         return result;
     }
 };
