aed875822bb433cb99726ba248e725419eff219a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-30 23:15:45 +0300

    Time: 3 ms (72.06%), Space: 14.9 MB (21.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,45 +1,40 @@
-// #include <unordered_map>
+
 // class Solution {
 // public:
 //     vector<int> twoSum(vector<int>& nums, int target) {
-//         unordered_map<int, int> seen; // value -> index
 
-//         for (int i = 0; i < nums.size(); ++i) {
-//             int complement = target - nums[i];
-//             if (seen.count(complement)) {
-//                 return {seen[complement], i};
-//             }
-//             seen[nums[i]] = i;
+//         unordered_map<int,int>mp;
+
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             mp[nums[i]]= i;
 //         }
-
-//         return {}; // Should never reach here if one solution is guaranteed
+        
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             auto it = mp.find((target - nums[i]));
+//             if (it != mp.end()  && i !=(mp[target - nums[i]]))
+//             {
+//                 return { i,mp[target - nums[i]] };
+//             }
+//         }
+//         return {};
 //     }
 // };
-
+//[optimized]
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //unordered_set<int>st(nums.begin(), nums.end());
-        unordered_map<int,int>mp;
-        //vector<int> res(2);
-        for (int i = 0; i < nums.size(); i++)
-        {
-            mp[nums[i]]= i;
-        }
-        
-        for (int i = 0; i < nums.size(); i++)
-        {
-            
-            auto it = mp.find((target - nums[i]));
-            if (it != mp.end()  && i !=(mp[target - nums[i]]))
-            {
-                /*res[0]=(i);
-                res[1]=(mp[target - nums[i]]);
-                
-                break;*/
-                return { i,mp[target - nums[i]] };
+        unordered_map<int, int> seen; // value -> index
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i};
             }
+            seen[nums[i]] = i;
         }
-        return {};
+
+        return {}; // Should never reach here if one solution is guaranteed
     }
 };
