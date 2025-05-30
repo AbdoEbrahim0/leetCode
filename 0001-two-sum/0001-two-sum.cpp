ff6b607a8564003f5d91b3c84cb034cc7052d761
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-30 23:05:20 +0300

    Time: 4 ms (58.36%), Space: 15.8 MB (10.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,43 @@
-#include <unordered_map>
+// #include <unordered_map>
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> seen; // value -> index
+
+//         for (int i = 0; i < nums.size(); ++i) {
+//             int complement = target - nums[i];
+//             if (seen.count(complement)) {
+//                 return {seen[complement], i};
+//             }
+//             seen[nums[i]] = i;
+//         }
+
+//         return {}; // Should never reach here if one solution is guaranteed
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen; // value -> index
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            if (seen.count(complement)) {
-                return {seen[complement], i};
-            }
-            seen[nums[i]] = i;
+        //unordered_set<int>st(nums.begin(), nums.end());
+        unordered_map<int,int>mp;
+        vector<int>res(2);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]]= i;
         }
-
-        return {}; // Should never reach here if one solution is guaranteed
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            
+            auto it = mp.find((target - nums[i]));
+            if (it != mp.end()  && i !=(mp[target - nums[i]]))
+            {
+                res[0]=(i);
+                res[1]=(mp[target - nums[i]]);
+                break;
+            }
+        }
+        return res;
     }
 };
