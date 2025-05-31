d52e1c2d200401e0b64c29a119e2dd413f06758a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-05-31 19:53:25 +0300

    Time: 0 ms (100%), Space: 11.9 MB (67.23%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,30 +1,46 @@
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         short n=strs.size();
+//         string res="";
+//         short smallestSize = strs[0].size();
+        
+//         for (string val : strs)
+//         {
+//             smallestSize = min((int)smallestSize,(int)val.size());
+//         }
+//         short val;
+//         short cnt;
+//         for (int x = 0; x <smallestSize;)
+//             {
+//              val = 0;
+//              cnt = 1;
+//             while (val < n - 1 && x < smallestSize && strs[val][x] == strs[1 + val][x])
+//             {
+//                 cnt++;
+//                 val++;
+//             }
+//             if (cnt == n)
+//                 res = res+ strs[val][x];
+//             else break; 
+//             x++;
+//             }
+//             return res;
+//     }
+// };
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        short n=strs.size();
-        string res="";
-        short smallestSize = strs[0].size();
-        
-        for (string val : strs)
-        {
-            smallestSize = min((int)smallestSize,(int)val.size());
+        if (strs.empty()) return "";
+
+        for (int i = 0; i < strs[0].size(); ++i) {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.size(); ++j) {
+                if (i >= strs[j].size() || strs[j][i] != c)
+                    return strs[0].substr(0, i);
+            }
         }
-        short val;
-        short cnt;
-        for (int x = 0; x <smallestSize;)
-            {
-             val = 0;
-             cnt = 1;
-            while (val < n - 1 && x < smallestSize && strs[val][x] == strs[1 + val][x])
-            {
-                cnt++;
-                val++;
-            }
-            if (cnt == n)
-                res = res+ strs[val][x];
-            else break; 
-            x++;
-            }
-            return res;
+        return strs[0];
     }
 };
