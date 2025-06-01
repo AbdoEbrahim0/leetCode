878b81678a06fd6f798e822726c307217fc9da0d
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-02 02:02:01 +0300

    Time: 0 ms (100%), Space: 8.6 MB (82.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,135 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         /*int nd = -1;
+//         int ndSize = needle.size();
+//         for (char& c: haystack)
+//         {
+//             if (c == needle[++nd]) { ndSize--; }
+//             if (ndSize != 0)
+//                 nd = -1;
+
+//         }*/
+//        int x= haystack.find(needle);
+//        return (x == string::npos) ? -1 : x;
+//     }
+// };
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int nd = 0;
+//         int ndSize = needle.size();
+//         for (int c=0 ;c<  haystack.size();c++)
+//         {
+//             if (nd< ndSize && haystack[c] == needle[nd]) { nd++; }
+//             else
+//             {
+//                 if (nd == ndSize)
+//                     return c - nd;
+//                 else
+//                     nd = 0;
+//             }
+
+//         }
+//        return -1;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int nd = 0;
+//         int ndSize = needle.size();
+//         int c;
+//         for (c=0 ;c<  haystack.size();c++)
+//         {
+//             if (nd< ndSize && haystack[c] == needle[nd]) { nd++; }
+//             else
+//             {
+//                 if (nd == ndSize)
+//                     return c - nd;
+//                 else
+//                     nd = 0;
+//             }
+//         }
+//         if (nd == ndSize)
+//             return c - nd;
+//        return -1;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int nd = 0;
+//         int ndSize = needle.size();
+//         int c;
+//         for (c=0 ;c<  haystack.size();c++)
+//         {
+//             if (nd< ndSize && haystack[c] == needle[nd])
+//             { nd++; }
+//             else
+//             {
+//                 if (nd == ndSize)
+//                     return c - nd;
+//                 else
+//                 {
+//                     nd = 0;
+//                      // c--;
+//                 }
+//             }
+//         }
+//         if (nd == ndSize)
+//             return c - nd;
+//        return -1;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int nd = 0;
+//         int ndSize = needle.size();
+//         int c=0;
+//         for (;c<  haystack.size();c++ )
+//         {
+            
+//             while (nd< ndSize && haystack[c] == needle[nd])
+//             {
+//                 nd++,c++;
+//             }
+            
+//             if (nd == ndSize)
+//                 return c - nd;
+//             nd = 0;
+//             if(c>=1)
+//             c=c-2;
+            
+//         }
+//         if (nd == ndSize)
+//             return c - nd;
+//        return -1;
+//     }
+// };
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        /*int nd = -1;
-        int ndSize = needle.size();
-        for (char& c: haystack)
-        {
-            if (c == needle[++nd]) { ndSize--; }
-            if (ndSize != 0)
-                nd = -1;
+        int hSize = haystack.size();
+        int nSize = needle.size();
 
-        }*/
-       int x= haystack.find(needle);
-       
-       return (x == string::npos) ? -1 : x;
-           
+        if (nSize == 0) return 0;
+        if (hSize < nSize) return -1;
+
+        for (int i = 0; i <= hSize - nSize; ++i) {
+            int j = 0;
+            while (j < nSize && haystack[i + j] == needle[j]) {
+                ++j;
+            }
+            if (j == nSize) return i; // Full match found
+        }
+
+        return -1;
     }
 };
