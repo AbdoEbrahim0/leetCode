54f9e90a5131cb01aacedd2e33f5c71087da6a6d
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-01 22:47:07 +0300

    Time: 3 ms (15.59%), Space: 23 MB (10.55%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,46 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int sizeNums = nums.size();
+//         int indcies[3 * 10000]  ;
+//         fill_n(indcies, 30000, -1);
+//         int start=0;
+//         for (int i = sizeNums - 1; i >0; i--,start++)
+//         {
+//             if (nums[i] == nums[i - 1])
+//             {
+//                 indcies[start] = i;
+//             }
+            
+//         }
+//         for (int z = 0; z < sizeNums; z++)
+//         {
+//         if(indcies[z]>=0)
+//             nums.erase(nums.begin() + indcies[z]);
+//         }
+//         return nums.size();
+//     }
+// };
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int sizeNums = nums.size();
-        int indcies[3 * 10000]  ;
-        fill_n(indcies, 30000, -1);
-        int start=0;
-        for (int i = sizeNums - 1; i >0; i--,start++)
+        int *indcies=new int[sizeNums];
+        fill_n(indcies, sizeNums, -1);
+        short start = 0;
+        for (int i = sizeNums - 1; i > 0; i--)
         {
             if (nums[i] == nums[i - 1])
             {
-                indcies[start] = i;
+                indcies[start++] = i;
             }
-            
+
         }
-        for (int z = 0; z < sizeNums; z++)
+        for (int z = 0; z <sizeNums ; z++)
         {
-        if(indcies[z]>=0)
-            nums.erase(nums.begin() + indcies[z]);
+            if (indcies[z] >= 0)
+                nums.erase(nums.begin() + indcies[z]);
         }
         return nums.size();
     }
