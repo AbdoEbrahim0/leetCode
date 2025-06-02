08f86233ebf80d7f3efc118cfec78f87840a9373
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-02 23:51:29 +0300

    Time: 0 ms (100%), Space: 11.5 MB (65.74%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,52 @@
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         int power = 0;
+        
+//         int n = digits.size() - 1;
+//         long long num = digits[n];
+//         for (int i = n-1; i >= 0; i--)
+//         {
+//             num += (digits[i] * pow(10, ++power));
+//         }
+//         num++;
+//         int s = log10(num) + 1;
+//         vector<int> res(s);
+        
+//         while (num!=0)
+//         {
+//             res[--s] = num % 10;
+//             num = num / 10;
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int power = 0;
+        
+        int n = digits.size() - 1;
+        int carry = 1;
+        for (int i = n; i >= 0; i--)
+        {
+            //{ 4, 3, 2, 1 }
+            //{9,9,9,9}
+            if (digits[i]+carry > 9)
+            {
+                digits[i] = 0;
+                
+            }
+            else
+            {
+                carry = 0;
+                digits[i]++;
+                break;
+            }
+        }
+        if (carry == 1)
+            digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
