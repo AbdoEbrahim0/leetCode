a06c6e4d8c13eee7c8064dd9ab7b8c4b006df72f
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-04 23:06:04 +0300

    Time: 0 ms (100%), Space: 21.2 MB (16.95%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,3 +1,43 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         //{ 4, 1, 2, 1, 2 };
+//         int n = nums.size();
+//         vector<int> negative ;
+//         //fill(negative.begin(), negative.end(), false);
+//         int maxE = *max_element(nums.begin(), nums.end());
+//         vector< int >arr (maxE + 1);
+//         //fill(negative.begin(), negative.end(), false);
+//         for (int i=0;i<nums.size();i++)
+//         {
+//             if (nums[i] < 0)
+//                 negative.emplace_back(nums[i]*-1);
+//             else
+//                 arr[nums[i]]++;
+//         }
+//         for (int i = 0; i < arr.size(); i++)
+//         {
+//             if (arr[i] == 1)
+//                 return i;
+//         }
+
+//         //ceck negative vals
+//         maxE = *max_element(negative.begin(), negative.end());
+//         vector< int >arr2(maxE + 1);
+//         for (int i = 0; i < negative.size(); i++)
+//         {
+//                 arr2[negative[i]]++;
+//         }
+//         for (int i = 0; i < arr2.size(); i++)
+//         {
+//             if (arr2[i] == 1)
+//                 return i*-1;
+//         }
+        
+//         return 0;
+//     }
+// };
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -5,30 +45,32 @@ public:
         int n = nums.size();
         vector<int> negative ;
         //fill(negative.begin(), negative.end(), false);
-        int maxE = *max_element(nums.begin(), nums.end());
-        vector< int >arr (maxE + 1);
+        //int maxE = *max_element(nums.begin(), nums.end());
+        //vector< int >arr (maxE + 1);
+        int arr[3 * 10000] = {};
         //fill(negative.begin(), negative.end(), false);
-        for (int i=0;i<nums.size();i++)
+        for (int i=0;i< n;i++)
         {
             if (nums[i] < 0)
                 negative.emplace_back(nums[i]*-1);
             else
                 arr[nums[i]]++;
         }
-        for (int i = 0; i < arr.size(); i++)
+        for (int i = 0; i < 3 * 10000; i++)
         {
             if (arr[i] == 1)
                 return i;
         }
 
         //ceck negative vals
-        maxE = *max_element(negative.begin(), negative.end());
-        vector< int >arr2(maxE + 1);
+        //maxE = *max_element(negative.begin(), negative.end());
+        //vector< int >arr2(maxE + 1);
+        int arr2[3 * 10000] = {};
         for (int i = 0; i < negative.size(); i++)
         {
                 arr2[negative[i]]++;
         }
-        for (int i = 0; i < arr2.size(); i++)
+        for (int i = 0; i < 3 * 10000; i++)
         {
             if (arr2[i] == 1)
                 return i*-1;
