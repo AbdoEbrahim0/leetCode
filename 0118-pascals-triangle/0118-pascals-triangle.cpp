ce098c9c469f442134407a3d8c096e161e9423e1
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-06 00:13:43 +0300

    Time: 0 ms (100%), Space: 9.7 MB (63.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,11 +1,35 @@
+
+// //naive
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> res;
+//         vector<int> rowVToADD;
+//         res.push_back({1});
+//         for (int i = 1; i < numRows; i++)
+//         {
+//             rowVToADD.emplace_back(1);
+//             for (int j = 0; j + 1 < i; j++)
+//             {
+//                 rowVToADD.emplace_back(res[i-1][j] + res[i-1][j + 1]);
+//             }
+//             rowVToADD.emplace_back(1);
+//             res.emplace_back(rowVToADD);
+//             rowVToADD.clear();
+//         }
+//         return res;
+//     }
+// };
+//[delete clear fn]
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-        vector<int> rowVToADD;
+        
         res.push_back({1});
         for (int i = 1; i < numRows; i++)
         {
+            vector<int> rowVToADD;
             rowVToADD.emplace_back(1);
             for (int j = 0; j + 1 < i; j++)
             {
@@ -13,7 +37,7 @@ public:
             }
             rowVToADD.emplace_back(1);
             res.emplace_back(rowVToADD);
-            rowVToADD.clear();
+            
         }
         return res;
     }
