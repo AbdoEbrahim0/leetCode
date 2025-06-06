0dd494c8faa093faab5b7da52a096a6b7c229188
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-06 18:35:01 +0300

    Time: 0 ms (100%), Space: 8.7 MB (17.3%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n == 1) return 1;
+//         if (n == 2) return 2;
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// };
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return climb(n, dp);
+    }
+
+private:
+    int climb(int n, vector<int>& dp) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = climb(n - 1, dp) + climb(n - 2, dp);
+    }
+};
