34fb714a6822e9456cae433cca37a54597b959e3
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-09 19:26:26 +0300

    Time: 0 ms (100%), Space: 7.8 MB (86.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -59,19 +59,37 @@ n=4
 //     }
 // };
 // comp: O(N) Tabulation (Bottom-Up DP)
+// class Solution {
+
+// public:
+
+// int climbStairs(int n) {
+
+//     if (n <= 2) return n;
+// vector<int> dp(n + 1);    
+//     dp[1] = 1;
+//     dp[2] = 2;
+//     for (int i = 3; i <= n; ++i) {
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+//     return dp[n];
+// }
+// };
+//Space-Optimized (Fibonacci-style)
+
+// comp: O(N) Tabulation (Bottom-Up DP)
 class Solution {
 
 public:
-
 int climbStairs(int n) {
-
     if (n <= 2) return n;
-vector<int> dp(n + 1);    
-    dp[1] = 1;
-    dp[2] = 2;
+    int a = 1, b = 2;
     for (int i = 3; i <= n; ++i) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        int temp = a + b;
+        a = b;
+        b = temp;
     }
-    return dp[n];
+    return b;
 }
 };
+
