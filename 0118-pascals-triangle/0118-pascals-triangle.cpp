f2fd49b632b12412fc8b543dbd77a23f70170b88
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-09 20:06:52 +0300

    Time: 0 ms (100%), Space: 9.6 MB (63.5%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -21,24 +21,43 @@
 //     }
 // };
 //[delete clear fn]
+
+//Time: O(n²), Space: O(n²)
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> res;
+        
+//         res.push_back({1});
+//         for (int i = 1; i < numRows; i++)
+//         {
+//             vector<int> rowVToADD;
+//             rowVToADD.emplace_back(1);
+//             for (int j = 0; j +1< i; j++)//as i push at index 0 {1} always
+//             {
+//                 rowVToADD.emplace_back(res[i-1][j] + res[i-1][j + 1]);
+//             }
+//             rowVToADD.emplace_back(1);
+//             res.emplace_back(rowVToADD); 
+//         }
+//         return res;
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-        
-        res.push_back({1});
-        for (int i = 1; i < numRows; i++)
-        {
-            vector<int> rowVToADD;
-            rowVToADD.emplace_back(1);
-            for (int j = 1; j < i; j++)
-            {
-                rowVToADD.emplace_back(res[i-1][j-1] + res[i-1][j-1 + 1]);
+
+        for (int i = 0; i < numRows; ++i) {
+            vector<int> row(i + 1, 1); // all elements = 1
+
+            for (int j = 1; j < i; ++j) {
+                row[j] = res[i - 1][j - 1] + res[i - 1][j]; // sum from previous row
             }
-            rowVToADD.emplace_back(1);
-            res.emplace_back(rowVToADD);
-            
+
+            res.push_back(row);
         }
+
         return res;
     }
 };
