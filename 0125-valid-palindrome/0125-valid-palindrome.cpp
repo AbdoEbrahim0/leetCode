7558d12b7fa7f52b0c01e323af774ca41bb5ff21
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-10 03:12:32 +0300

    Time: 31 ms (7.24%), Space: 10 MB (58.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -19,8 +19,41 @@ public:
             st++;
             i--;
         }
-        // if (s.empty())
-        //         return true;
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int n = s.size();
+//         int st = 0;
+//         int half = ceil(n / 2.0);
+        
+//         n = s.size();
+//         for (int i = n - 1; i > 0 && st < half; )
+//         {
+
+//             while (st < half && !isalpha(s[st]) && !isdigit(s[st]))
+//             {
+//                 st++;
+//             }
+//             while (i > 0 && !isalpha(s[i]) && !isdigit(s[i]))
+//             {
+//                 i--;
+//             }
+//             int st2=0;
+//             if (tolower(s[st]) != tolower(s[i]))
+//             {
+//              while(!isalpha(s[st2++]))
+//              s.erase(s.begin()+i);
+//              if(s.empty())
+//                 return true;
+//                 return false;
+//             }
+//             st++;
+//             i--;
+//         }
+//         return true;
+//     }
+// };
