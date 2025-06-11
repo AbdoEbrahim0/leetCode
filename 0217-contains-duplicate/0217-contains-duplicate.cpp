c4a89102d7e505eb0e3c4653186e881d801ef3bb
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-11 22:16:54 +0300

    Time: 52 ms (47.53%), Space: 90.8 MB (43.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,25 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_map<int, int>ump(nums.size());
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             ump[nums[i]] = ump[nums[i]] + 1;
+//             if (ump[nums[i]] == 2)
+//                 return 1;
+//         }
+//         return 0;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        //bool ans = false;
-        unordered_map<int, int>ump(nums.size());
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            ump[nums[i]] = ump[nums[i]] + 1;
-            if (ump[nums[i]] == 2)
-                return 1;
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) return true;
+            seen.insert(num);
         }
-        return 0;
+        return false;
     }
 };
