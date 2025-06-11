04049d26fe2a315aca33782753fd00574c2e8309
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-11 21:27:18 +0300

    Time: 0 ms (100%), Space: 9.2 MB (42.06%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -41,47 +41,74 @@
 // };
 
 //after cleaning up
+// class Solution {
+// public:
+//     vector<string> summaryRanges(vector<int>& nums) {
+//         if (nums.size() == 0)
+//             return {};
+//         vector<string> v;
+        
+//         int end= nums[0],st = nums[0];
+//         /*
+//         Input: nums = [0,1,2,4,5,7]
+//         Output: ["0->2","4->5","7"]
+
+//         Input: nums = [0,2,3,4,6,8,9]
+//         Output: ["0","2->4","6","8->9"]
+//         */
+//         int n = nums.size();
+
+//         for (int i=0 ;i< nums.size();i++)
+//         {
+
+//             while (i+1 < n && nums[i + 1] == nums[i] + 1)
+//             {
+//                 end = nums[i + 1];
+//                 i++;
+//             }
+//             if (i == n - 1)
+//                 end = nums[i];
+//             if (i + 1 < n && st == end)
+//             {
+//                 v.emplace_back(to_string(nums[i])) ;
+//             }
+//             else
+//             {
+//                 if (st < end)
+//                     v.emplace_back(to_string(st) + "->" + to_string(end));
+//                 else
+//                     v.emplace_back(to_string(st));
+//             }
+//                 if(i + 1 < n)
+//                 st = nums[i+1];
+//         }
+//         return v;
+//     }
+// };
+//after simplyify logic and deal with edge case 
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        if (nums.size() == 0)
-            return {};
-        vector<string> v;
-        
-        int end= nums[0],st = nums[0];
-        /*
-        Input: nums = [0,1,2,4,5,7]
-        Output: ["0->2","4->5","7"]
-
-        Input: nums = [0,2,3,4,6,8,9]
-        Output: ["0","2->4","6","8->9"]
-        */
+        vector<string> res;
         int n = nums.size();
+        if (n == 0) return res;
 
-        for (int i=0 ;i< nums.size();i++)
-        {
+        int start = nums[0];
 
-            while (i+1 < n && nums[i + 1] == nums[i] + 1)
-            {
-                end = nums[i + 1];
-                i++;
+        for (int i = 1; i <= n; ++i) {
+            // If we're at the end or the sequence breaks
+            if (i == n || nums[i] != nums[i - 1] + 1) {
+                if (nums[i - 1] == start) {
+                    res.emplace_back(to_string(start));
+                } else {
+                    res.emplace_back(to_string(start) + "->" + to_string(nums[i - 1]));
+                }
+                // If not at end, start a new range
+                if (i < n) start = nums[i];
             }
-            if (i == n - 1)
-                end = nums[i];
-            if (i + 1 < n && st == end)
-            {
-                v.emplace_back(to_string(nums[i])) ;
-            }
-            else
-            {
-                if (st < end)
-                    v.emplace_back(to_string(st) + "->" + to_string(end));
-                else
-                    v.emplace_back(to_string(st));
-            }
-                if(i + 1 < n)
-                st = nums[i+1];
         }
-        return v;
+
+        return res;
     }
 };
+
