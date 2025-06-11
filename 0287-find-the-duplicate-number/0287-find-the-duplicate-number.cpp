d46c4c33059b3457c86437b14df84fc6c3fa153e
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-12 02:05:33 +0300

    Time: 37 ms (24.18%), Space: 67.6 MB (23.39%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,45 +1,61 @@
-// class Solution {
-// public:
-//     int findDuplicate(vector<int>& nums) {
-//         int n = nums.size()-1;
-//         int expectedSum = n * (n + 1) / 2;
-//         int actualSum = accumulate(nums.begin(), nums.end(), 0);
-//         return actualSum - expectedSum ;
-//     }
-// };
+// // class Solution {
+// // public:
+// //     int findDuplicate(vector<int>& nums) {
+// //         int n = nums.size()-1;
+// //         int expectedSum = n * (n + 1) / 2;
+// //         int actualSum = accumulate(nums.begin(), nums.end(), 0);
+// //         return actualSum - expectedSum ;
+// //     }
+// // };
+
+// // class Solution {
+// // public:
+// //     int findDuplicate(vector<int>& nums) {
+// //         int n = nums.size()-1;
+// //         int xorAll = 0;
+// //         for (int i = 0; i <= n; ++i) {
+// //             xorAll ^= i;
+// //         }
+// //         for (int num : nums) {
+// //             xorAll ^= num;
+// //         }
+// //         return xorAll;
+// //     }
+// // };
 
 // class Solution {
 // public:
 //     int findDuplicate(vector<int>& nums) {
-//         int n = nums.size()-1;
-//         int xorAll = 0;
-//         for (int i = 0; i <= n; ++i) {
-//             xorAll ^= i;
+//         int n = nums.size() ;
+//         int arr[100001] = {};
+//         int i;
+//         for (i = 0; i < n; i++)
+//         {
+//             //arr[nums[i]] = arr[nums[i]]++; // wrong  implement      
+//             arr[nums[i]]++; //correct
+//             if (arr[nums[i]] >= 2)
+//                 break;
 //         }
-//         for (int num : nums) {
-//             xorAll ^= num;
-//         }
-//         return xorAll;
+//         return nums[i];
 //     }
 // };
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        //3,1,3,4,2
         int n = nums.size() ;
-        int arr[100001] = {};
-        int i;
-        for (i = 0; i < n; i++)
+        vector<int>copy(nums.begin(), nums.end());
+        //1 2 3 3 4
+        sort(copy.begin(), copy.end());
+        int  i;
+        for ( i = 1; i < n; i++)
         {
-            //arr[nums[i]] = arr[nums[i]]++; // wrong  implement      
-            //correct
-            arr[nums[i]]++;
-
-            if (arr[nums[i]] >= 2)
-                break;
+            if (copy[i] == copy[i - 1])
+            {
+             break;   
+            }
         }
-
-
-        return nums[i];
+return copy[i-1];
     }
 };
