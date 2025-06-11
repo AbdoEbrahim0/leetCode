21047125d0bbcf441049a96f2d16a4ad5b3430bb
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-11 21:24:38 +0300

    Time: 0 ms (100%), Space: 9.3 MB (42.06%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,43 +1,87 @@
+// class Solution {
+// public:
+//     vector<string> summaryRanges(vector<int>& nums) {
+//         if(nums.size()==0)
+//         return {};
+//         vector<string> v;
+//         string s="";
+//         int end= nums[0],st = nums[0];
+//         /*
+//         Input: nums = [0,1,2,4,5,7]
+//         Output: ["0->2","4->5","7"]
+//         */
+//         for (int i=0 ;i< nums.size();i++)
+//         {
+
+//             while (i+1 < nums.size() && nums[i + 1] == nums[i] + 1)
+//             {
+//                 end = nums[i + 1];
+//                 i++;
+//             }
+//             if (i == nums.size() - 1)//last element
+//                 end = nums[i];
+// //            if (st == end)
+//             if (i + 1 < nums.size() && st == end)
+//             {
+//                 v.push_back(to_string(nums[i])) ;
+//             }
+//             else
+//             {
+//                 if (st < end)
+//                     s = to_string(st) + "->" + to_string(end);
+//                 else
+//                     s = to_string(st);
+//                 v.push_back(s);
+//             }
+//                 if(i + 1 <nums.size())
+//                 st = nums[i+1];
+//         }
+//         return v;
+//     }
+// };
+
+//after cleaning up
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        if(nums.size()==0)
-        return {};
+        if (nums.size() == 0)
+            return {};
         vector<string> v;
-        string s="";
+        
         int end= nums[0],st = nums[0];
         /*
         Input: nums = [0,1,2,4,5,7]
         Output: ["0->2","4->5","7"]
+
+        Input: nums = [0,2,3,4,6,8,9]
+        Output: ["0","2->4","6","8->9"]
         */
+        int n = nums.size();
+
         for (int i=0 ;i< nums.size();i++)
         {
 
-            while (i+1 < nums.size() && nums[i + 1] == nums[i] + 1)
+            while (i+1 < n && nums[i + 1] == nums[i] + 1)
             {
                 end = nums[i + 1];
                 i++;
             }
-            if (i == nums.size() - 1)
+            if (i == n - 1)
                 end = nums[i];
-//            if (st == end)
-            if (i + 1 < nums.size() && st == end)
+            if (i + 1 < n && st == end)
             {
-                v.push_back(to_string(nums[i])) ;
+                v.emplace_back(to_string(nums[i])) ;
             }
             else
             {
-                
                 if (st < end)
-                    s = to_string(st) + "->" + to_string(end);
+                    v.emplace_back(to_string(st) + "->" + to_string(end));
                 else
-                    s = to_string(st);
-                v.push_back(s);
+                    v.emplace_back(to_string(st));
             }
-                if(i + 1 <nums.size())
+                if(i + 1 < n)
                 st = nums[i+1];
         }
-        
         return v;
     }
 };
