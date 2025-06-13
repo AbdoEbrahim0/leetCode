4c16855a9c8b71512925bd0cbe63a00db4eba787
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-13 11:00:32 +0300

    Time: 0 ms (100%), Space: 23.9 MB (17.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -11,24 +11,45 @@
 //     }
 // };
 // if we dont care about sorting
+//O(n)	O(1)
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {   
+//         short n = nums.size();
+//         short lastIndx = 0;
+//         short numOfZeros=0;
+//         for (short j = 0; j < n && lastIndx<(n-numOfZeros) ; j++)
+//         {
+//             if (nums[j] != 0)
+//                 swap(nums[lastIndx++], nums[j]);
+//             else
+//                 numOfZeros++;   
+//         }
+//         for (short j = lastIndx; j < n; j++)
+//         {
+//             nums[j] = 0;
+//         }
+//     }
+// };
+
+//2 pointers best for interview 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
         short n = nums.size();
-        short lastIndx = 0;
-        short numOfZeros=0;
-        for (short j = 0; j < n && lastIndx<(n-numOfZeros) ; j++)
+        short lastNonZeroIndex = 0;
+        for (short j = 0; j < n   ; j++)
         {
             if (nums[j] != 0)
-                swap(nums[lastIndx++], nums[j]);
-            else
-                numOfZeros++;   
+            {
+                nums[lastNonZeroIndex++] = nums[j];
+                //  lastNonZeroIndex ++;
+            }
         }
-        
-        for (short j = lastIndx; j < n; j++)
+        for (; lastNonZeroIndex < n   ; )
         {
-            nums[j] = 0;
+        nums[lastNonZeroIndex++] =0;
         }
     }
 };
