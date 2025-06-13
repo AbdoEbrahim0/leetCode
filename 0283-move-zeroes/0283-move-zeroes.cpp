01d8f1cb0b8c367650c5989cda070ef9570eabff
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-13 10:30:33 +0300

    Time: 4 ms (14.29%), Space: 23.9 MB (49.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,31 @@
+// n log n as remove sort implecitly 
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+        
+//         auto toZeros =remove(nums.begin(), nums.end(), 0);
+//         for (int dx= toZeros - nums.begin();dx<nums.size(); dx++)
+//         {
+//             nums[dx]= 0;
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //sort(nums.begin(), nums.end());
-        auto toZeros =remove(nums.begin(), nums.end(), 0);
-        for (int dx= toZeros - nums.begin();dx<nums.size(); dx++)
+        
+        int n = nums.size();
+        int lastIndx = 0;
+        for (int j = 0; j < n; j++)
         {
-            nums[dx]= 0;
+            if (nums[j] != 0)
+                swap(nums[lastIndx++], nums[j]);
         }
-        //return nums;
+        
+        for (int j = lastIndx; j < n; j++)
+        {
+            nums[j] = 0;
+        }
     }
 };
