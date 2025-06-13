a6f95194e9e5b1422ae27df521b511de6e8681eb
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-13 12:29:38 +0300

    Time: 7 ms (23.48%), Space: 23.8 MB (92.6%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -24,26 +24,42 @@
  */
 
  //prefix sum
- class NumArray {
-private:
-    vector<int> _nums;
-    int total = 0;
+//  class NumArray {
+// private:
+//     vector<int> _nums;
+//     int total = 0;
+// public:
+//     NumArray(vector<int>& nums) {
+//         this->_nums = nums;
+//         total = accumulate(nums.begin(), nums.end(), 0);
+//     }
+//     // [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+//     int sumRange(int left, int right) {
+//         int resLeft = 0, resRight = 0;
+//         for (int i = 0; i < left; i++)
+//         {
+//             resLeft += _nums[i];
+//         }
+//         for (int i = right+1; i < _nums.size(); i++)
+//         {
+//             resRight += _nums[i];
+//         }
+//         return total-resLeft-resRight;
+//     }
+// };
+
+ //prefix sum [optimized]
+class NumArray {
+private: vector<int> prefix;
 public:
     NumArray(vector<int>& nums) {
-        this->_nums = nums;
-        total = accumulate(nums.begin(), nums.end(), 0);
+        //{ -2, 0, 3, -5, 2, -1 }
+        prefix.resize(nums.size() + 1, 0);
+        for (int i = 0, j = 1; i < nums.size(); ++i, ++j) {
+            prefix[j] = nums[i] + prefix[i];
+        }
     }
-    // [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
     int sumRange(int left, int right) {
-        int resLeft = 0, resRight = 0;
-        for (int i = 0; i < left; i++)
-        {
-            resLeft += _nums[i];
-        }
-        for (int i = right+1; i < _nums.size(); i++)
-        {
-            resRight += _nums[i];
-        }
-        return total-resLeft-resRight;
+        return prefix[right + 1] - prefix[left];
     }
 };
