951d521d672cd19cfd82005708dd64a0a3429ffa
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-13 23:31:30 +0300

    Time: 0 ms (100%), Space: 13.7 MB (97.8%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,4 +1,34 @@
+//(n1 log n1  +  n2 log n2) space : O(1)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end()); // O(n1 log n1)
+        sort(nums2.begin(), nums2.end()); // O(n2 log n2)
+        
+        int i = 0, j = 0;
+        vector<int> res;
+        
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                ++i;
+            } else if (nums1[i] > nums2[j]) {
+                ++j;
+            } else {
+                // Avoid duplicates in result
+                if (res.empty() || res.back() != nums1[i]) {
+                    res.push_back(nums1[i]);
+                }
+                ++i;
+                ++j;
+            }
+        }
+        
+        return res;
+    }
 
+
+};
+//O(n1+n2)  space : O(N)
 // class Solution {
 // public:
 //     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -26,30 +56,32 @@
 //         return res;
 //     }
 // };
-class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        //unordered_set <int>set1;
-        int set2[1001] = {};
-        bool bSet2[1001] = {};
-        for (int i = 0; i < n2; i++)
-        {
-            set2[nums2[i]]++;
-            bSet2[nums2[i]] = true;
-        }
-        vector<int>res;
-        for (int i = 0; i < n1; i++)
-        {
-            if (set2[nums1[i]] && bSet2[nums1[i]])
-            {
-                res.emplace_back(nums1[i]);
-                bSet2[nums1[i]] = false;
-            }
+
+//O(N)  space O(1)
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         int n1 = nums1.size();
+//         int n2 = nums2.size();
+//         //unordered_set <int>set1;
+//         int set2[1001] = {};
+//         bool bSet2[1001] = {};
+//         for (int i = 0; i < n2; i++)
+//         {
+//             set2[nums2[i]]++;
+//             bSet2[nums2[i]] = true;
+//         }
+//         vector<int>res;
+//         for (int i = 0; i < n1; i++)
+//         {
+//             if (set2[nums1[i]] && bSet2[nums1[i]])
+//             {
+//                 res.emplace_back(nums1[i]);
+//                 bSet2[nums1[i]] = false;
+//             }
             
 
-        }
-        return res;
-    }
-};
+//         }
+//         return res;
+//     }
+// };
