feaae251720da56d3ae2ee2f9cda9e88d55c09d6
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-13 22:59:11 +0300

    Time: 3 ms (39.54%), Space: 14.7 MB (8.85%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,3 +1,33 @@
+
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         int n1 = nums1.size();
+//         int n2 = nums2.size();
+//         //unordered_set <int>set1;
+//         unordered_set <int>set2;
+//         for (int i = 0; i < n2; i++)//o(N2)
+//         {
+//             set2.insert(nums2[i]);
+//         }
+//         vector<int>res;
+//         for (int i = 0; i < n1 ; i++)//o(N1)
+//         {
+//             if (set2.count(nums1[i])) // avg O(1)
+//                 res.emplace_back(nums1[i]);
+//         }
+//         set2.clear();
+//         for (int z = 0; z < res.size(); z++)
+//         {
+//             set2.insert(res[z]);
+            
+//         }
+//         res = vector<int>(set2.begin(), set2.end());
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -5,14 +35,14 @@ public:
         int n2 = nums2.size();
         //unordered_set <int>set1;
         unordered_set <int>set2;
-        for (int i = 0; i < n2; i++)
+        for (int i = 0; i < n2; i++)//o(N2)
         {
             set2.insert(nums2[i]);
         }
         vector<int>res;
-        for (int i = 0; i < n1 ; i++)
+        for (int i = 0; i < n1 ; i++)//o(N1)
         {
-            if (set2.count(nums1[i]))
+            if (set2.count(nums1[i])) // avg O(1)
                 res.emplace_back(nums1[i]);
         }
         set2.clear();
