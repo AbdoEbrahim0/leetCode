63e70319bfd7571812c3befb972877cf6a3fb63a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-13 23:18:04 +0300

    Time: 0 ms (100%), Space: 14.4 MB (49.96%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -26,32 +26,30 @@
 //         return res;
 //     }
 // };
-
-
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
         //unordered_set <int>set1;
-        unordered_set <int>set2;
-        for (int i = 0; i < n2; i++)//o(N2)
+        int set2[1001] = {};
+        bool bSet2[1001] = {};
+        for (int i = 0; i < n2; i++)
         {
-            set2.insert(nums2[i]);
+            set2[nums2[i]]++;
+            bSet2[nums2[i]] = true;
         }
         vector<int>res;
-        for (int i = 0; i < n1 ; i++)//o(N1)
+        for (int i = 0; i < n1; i++)
         {
-            if (set2.count(nums1[i])) // avg O(1)
+            if (set2[nums1[i]] && bSet2[nums1[i]])
+            {
                 res.emplace_back(nums1[i]);
-        }
-        set2.clear();
-        for (int z = 0; z < res.size(); z++)
-        {
-            set2.insert(res[z]);
+                bSet2[nums1[i]] = false;
+            }
             
+
         }
-        res = vector<int>(set2.begin(), set2.end());
         return res;
     }
 };
