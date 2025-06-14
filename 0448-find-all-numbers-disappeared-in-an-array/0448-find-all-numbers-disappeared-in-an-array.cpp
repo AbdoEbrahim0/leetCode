cdba877430fe08fb3c8373948daefcfdc11251ff
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-14 12:55:01 +0300

    Time: 8 ms (45.17%), Space: 53.1 MB (73.1%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -20,7 +20,7 @@
 // };
 
 
-
+////o(n log n) space o(n)
 // visted array
 // class Solution {
 // public:
@@ -42,7 +42,7 @@
 //         return res;
 //     }
 // };
-
+//o(n) space o(1)
 // class Solution {
 // public:
 //     vector<int> findDisappearedNumbers(vector<int>& nums) {
@@ -64,27 +64,45 @@
 //     }
 // };
 
+//o(n) space o(n)
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         //bool arr[100001] = {};
+//         int n = nums.size();
+//         vector<bool> arr(n);
+        
+//         vector<int>res;
+        
 
+//         for (int j = 0; j < n; j++)
+//         {
+//             arr[nums[j]] =1;
+//         }
+//         for (int i = 1; i <= n; i++)
+//         {
+//             if (arr[i] ==false)
+//                 res.emplace_back(i);
+
+//         }
+//         return res;
+//     }
+// };
+
+//most optimized with memory care
 class Solution {
 public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-        //bool arr[100001] = {};
-        int n = nums.size();
-        vector<bool> arr(n);
-        
-        vector<int>res;
-        
-
-        for (int j = 0; j < n; j++)
-        {
-            arr[nums[j]] =1;
-        }
-        for (int i = 1; i <= n; i++)
-        {
-            if (arr[i] ==false)
-                res.emplace_back(i);
-
-        }
-        return res;
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        int index = abs(nums[i]) - 1;
+        nums[index] = -abs(nums[index]);  // Mark as visited
     }
+    vector<int> res;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] > 0)
+            res.push_back(i + 1);  // Index + 1 is missing
+    }
+    return res;
+}
+
 };
