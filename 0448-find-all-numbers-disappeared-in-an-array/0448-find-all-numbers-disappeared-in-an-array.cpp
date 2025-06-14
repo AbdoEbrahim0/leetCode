6504ec0ed8b33470d1d05a78b670d15aa2f9b815
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-14 12:29:53 +0300

    Time: 0 ms (100%), Space: 53.1 MB (73.1%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,19 +1,89 @@
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         set<int> mySet(nums.begin(), nums.end());
+        
+//         vector<int>res;
+//         //4,3,2,7,8,2,3,1
+//         //1,2,3,4,7,8
+//         int n = nums.size();
+//         auto it =mySet.begin();
+//         for (int i = 1; i <= n; i++)
+//         {
+//             if (i != *(it))
+//                 res.emplace_back(i);
+//             else
+//                 it++;
+//         }
+//         return res;
+//     }
+// };
+
+
+
+// visted array
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         set<int> mySet(nums.begin(), nums.end());
+        
+//         vector<int>res;
+//         //4,3,2,7,8,2,3,1
+//         //1,2,3,4,7,8
+//         int n = nums.size();
+//         auto it =mySet.begin();
+//         for (int i = 1; i <= n; i++)
+//         {
+//             if (i != *(it))
+//                 res.emplace_back(i);
+//             else
+//                 it++;
+//         }
+//         return res;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         set<int> mySet(nums.begin(), nums.end());
+        
+//         vector<int>res;
+//         //4,3,2,7,8,2,3,1
+//         //1,2,3,4,7,8
+//         int n = nums.size();
+//         auto it =mySet.begin();
+//         for (int i = 1; i <= n; i++)
+//         {
+//             if (i != *(it))
+//                 res.emplace_back(i);
+//             else
+//                 it++;
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        set<int> mySet(nums.begin(), nums.end());
-        
-        vector<int>res;
+        bool arr[100001] = {};
         //4,3,2,7,8,2,3,1
-        //1,2,3,4,7,8
+        // 1,2,3,4,5,6,7,8
+        // 1,2,3,4,7,8
+        vector<int>res;
         int n = nums.size();
-        auto it =mySet.begin();
+
+        for (int j = 0; j < n; j++)
+        {
+            arr[nums[j]] =1;
+        }
         for (int i = 1; i <= n; i++)
         {
-            if (i != *(it))
+            if (arr[i] ==false)
                 res.emplace_back(i);
-            else
-                it++;
+
         }
         return res;
     }
