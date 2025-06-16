dd03c81e0d1bf76d095e8895d1ad4efbaf42e3e4
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-16 12:17:08 +0300

    Time: 15 ms (17.53%), Space: 44.9 MB (43.75%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,3 +1,41 @@
+// class Solution {
+// public:
+//     int findContentChildren(vector<int>& g, vector<int>& s) {
+//         sort(g.begin(), g.end());
+//         sort(s.begin(), s.end());
+//         int gSize = g.size();
+//         int sSize = s.size();
+//                     // for (int i = 0; i < gSize; i++)
+//                     //     cout << g[i] << " ";
+//                     // cout << "\n";
+//                     // for (int i = 0; i < sSize; i++)
+//                     //     cout << s[i] << " ";
+//         int indxS = 0;
+//         int satisfied = 0;
+//         // g = [1, 2, 3], s = [1, 1] out:1
+//         //Input: g = [1,2], s = [1,2,3] out:2
+//         //Input: g = [1,2,3], s = [3] out:1
+// //g = { 2, 16, 32, 34, 34, 41, 45 };
+// //s= { 2, 3, 4, 6, 6, 7, 10, 13, 18, 21, 23, 23, 23, 27, 30, 30, 33, 36, 39, 39, 40, 43, 45 };
+//         for (int i = 0; i < gSize; i++)
+//         {
+//             while (indxS < sSize  && s[indxS] < g[i])
+//                 indxS++;
+//             if (indxS >= sSize)
+//             {
+//                 break;
+//             }
+//             if (i < gSize && g[i] <= s[indxS])
+//             {
+//                 satisfied++;
+//                 indxS++;
+//             }
+//         }
+//         return satisfied;
+//     }
+// };
+
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
@@ -5,37 +43,31 @@ public:
         sort(s.begin(), s.end());
         int gSize = g.size();
         int sSize = s.size();
-                    // for (int i = 0; i < gSize; i++)
-                    //     cout << g[i] << " ";
-                    // cout << "\n";
-                    // for (int i = 0; i < sSize; i++)
-                    //     cout << s[i] << " ";
         int indxS = 0;
         int satisfied = 0;
         // g = [1, 2, 3], s = [1, 1] out:1
         //Input: g = [1,2], s = [1,2,3] out:2
         //Input: g = [1,2,3], s = [3] out:1
-//g = { 2, 16, 32, 34, 34, 41, 45 };
-//s= { 2, 3, 4, 6, 6, 7, 10, 13, 18, 21, 23, 23, 23, 27, 30, 30, 33, 36, 39, 39, 40, 43, 45 };
-        for (int i = 0; i < gSize; i++)
-        {
-            while (indxS < sSize  && s[indxS] < g[i])
+        
+        //vector<int> g = { 10,9,8,7 };
+        //vector<int> s = { 5,6,7,8 };
+        //int maxElement = -1;
+        
+        for (int i = 0; i < gSize && indxS < sSize;)
+        {   
+            if (s[indxS] < g[i])
                 indxS++;
-            if (indxS >= sSize)
-            {
-                break;
-            }
-            if (i < gSize && g[i] <= s[indxS])
+            else if(s[indxS] >= g[i])
             {
                 satisfied++;
-                //s[indxS] = 0;
+                i++;
                 indxS++;
             }
+            
         }
         return satisfied;
     }
 };
-
 // class Solution {
 // public:
 //     int findContentChildren(vector<int>& g, vector<int>& s) {
