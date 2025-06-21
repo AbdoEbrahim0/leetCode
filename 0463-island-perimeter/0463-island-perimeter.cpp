568960ed6cbf2756d6c9f837ae7572bdee2ea3e0
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-21 12:33:58 +0300

    Time: 6 ms (43.38%), Space: 101.9 MB (97.62%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,17 +1,105 @@
-//my naive sol 
+//my naive sol  row scanning then column scanning for 1's
+// class Solution {
+// public:
+//     int islandPerimeter(vector<vector<int>>& grid) {
+//         int r = grid.size();
+//         int c= grid[0].size();
+//         int totalStrides ;
+//         //totalStrides = r * c + r * (c - 1) + c * (r - 1);
+//         totalStrides = 0;
+
+//         for (int i = 0; i < r; i++)
+//         {   
+//             for (int j = 0; j < c; j++)
+//             {
+//                 if (grid[i][j] == 1)
+//                 {
+//                     totalStrides += 4;
+//                     if (j + 1 < c && grid[i][j + 1] == 1)
+//                         totalStrides-=2;       
+//                 }
+//             }
+//         }
+
+        
+//         for (int j = 0; j < c; j++)
+//         {
+//             for (int i = 0; r > 1 && i < r; i++)
+//             {
+//                 while(i + 1 < r && grid[i][j] ==0)
+//                 i++;
+//                 while (i + 1 < r && grid[i][j] == 1)
+//                 {
+//                     //totalStrides += 4;
+//                     if (i + 1 < r && grid[i+1][j] == 1)
+//                         totalStrides-=2;
+//                 i++;
+//                 }
+
+//             }
+//         }
+//         return totalStrides;
+//     }
+// };
+
+
+//my naive sol row scanning then column scanning for 1's [optimized]
+// class Solution {
+// public:
+//     int islandPerimeter(vector<vector<int>>& grid) {
+//         int r = grid.size();
+//         int c= grid[0].size();
+//         int totalStrides ;
+//         totalStrides = 0;
+
+//         for (int i = 0; i < r; i++)
+//         {   
+//             for (int j = 0; j < c; j++)
+//             {
+//                 if (grid[i][j] == 1)
+//                 {
+//                     totalStrides += 4;
+//                     if (j + 1 < c && grid[i][j + 1] == 1)
+//                         totalStrides-=2;       
+//                 }
+//             }
+//         }
+//         for (int j = 0; j < c; j++)
+//         {
+//             for (int i = 0; r > 1 && i < r; i++)
+//             {
+//                 while(i + 1 < r && grid[i][j] ==0)
+//                 i++;
+//                 while (i + 1 < r && grid[i][j] == 1)
+//                 {
+//                     //totalStrides += 4;
+//                     if (i + 1 < r && grid[i+1][j] == 1)
+//                         totalStrides-=2;
+//                 i++;
+//                 }
+
+//             }
+//         }
+//         return totalStrides;
+//     }
+// };
+
+
+//my naive sol row scanning then column scanning for 1's [optimized^2]
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int r = grid.size();
         int c= grid[0].size();
         int totalStrides ;
-        //totalStrides = r * c + r * (c - 1) + c * (r - 1);
         totalStrides = 0;
-
+        // row scanning for 1's
         for (int i = 0; i < r; i++)
         {   
             for (int j = 0; j < c; j++)
             {
+                while(j + 1 < c && grid[i][j] ==0)
+                j++;
                 if (grid[i][j] == 1)
                 {
                     totalStrides += 4;
@@ -20,8 +108,7 @@ public:
                 }
             }
         }
-
-        
+        // column scanning for 1's
         for (int j = 0; j < c; j++)
         {
             for (int i = 0; r > 1 && i < r; i++)
@@ -35,9 +122,6 @@ public:
                         totalStrides-=2;
                 i++;
                 }
-                // if ( i+1<r && grid[i][j] == 1)
-                //         totalStrides-=2;
-                //break;
 
             }
         }
@@ -45,6 +129,7 @@ public:
     }
 };
 
+//gpt sol
 // class Solution {
 // public:
 //     int islandPerimeter(vector<vector<int>>& grid) {
