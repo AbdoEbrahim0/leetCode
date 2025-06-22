0f2365035751152a6b604cb0c05f17981bb13a35
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-23 00:42:15 +0300

    Time: 3 ms (9.28%), Space: 29.3 MB (7.14%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,35 +1,69 @@
+// O(N)+O(N)
+// class Solution {
+// public:
+//     // [1,4,3,3,2]
+//     int longestMonotonicSubarray(vector<int>& nums) {
+//         int mx = 0;
+//         int cnt =1;
+//         for (int i = 1; i < nums.size(); i++)
+//         {
+//             if (nums[i] > nums[i - 1])
+//             {
+//                 cnt++;
+//             }
+//             else
+//             {
+//                 mx = max(mx, cnt);
+//                 cnt = 1;
+//             }
+//         }
+//         mx = max(mx, cnt);
+//         cnt = 1;
+//         for (int i = 1; i < nums.size(); i++)
+//         {
+//             if (nums[i] < nums[i - 1])
+//             {
+//                 cnt++;
+//             }
+//             else
+//             {
+//                 mx = max(mx, cnt);
+//                 cnt = 1;
+//             }
+//         }
+//         return max(mx, cnt);
+//     }
+// };
+
 class Solution {
 public:
     // [1,4,3,3,2]
     int longestMonotonicSubarray(vector<int>& nums) {
-        int mx = 0;
-        int cnt =1;
+        int mx = 1;
+        
+        stack<int>stkInc;
+        stack<int>stkDec;
+        stkInc.push(nums[0]);
+
         for (int i = 1; i < nums.size(); i++)
         {
-            if (nums[i] > nums[i - 1])
+            while(stkInc.size()&& nums[i] <= nums[i - 1])
             {
-                cnt++;
+                stkInc.pop();
             }
-            else
-            {
-                mx = max(mx, cnt);
-                cnt = 1;
-            }
+            stkInc.push(nums[i]);
+            mx = max(mx , (int)stkInc.size());
         }
-        mx = max(mx, cnt);
-        cnt = 1;
+        stkDec.push(nums[0]);
         for (int i = 1; i < nums.size(); i++)
         {
-            if (nums[i] < nums[i - 1])
+            while (stkDec.size() && nums[i] >= nums[i - 1])
             {
-                cnt++;
+                stkDec.pop();
             }
-            else
-            {
-                mx = max(mx, cnt);
-                cnt = 1;
-            }
+            stkDec.push(nums[i]);
+            mx = max(mx, (int)stkDec.size());
         }
-        return max(mx, cnt);
+        return mx;
     }
 };
