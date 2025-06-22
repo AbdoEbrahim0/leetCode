0654a481076b4b9776838eea6291f6307401b8bd
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-22 20:56:19 +0300

    Time: 0 ms (100%), Space: 7.7 MB (80.11%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -6,13 +6,10 @@ public:
         
             while (empty >= numExchange)
             {
-                
                          //15/4=3    6/4=1
                 cnt += (empty / numExchange);  //15+3=18  18+(6/4)
                 empty = empty / numExchange + (empty % numExchange);//3+3=6   6/4+6%4 1+2=3    1+3
-                
-                //numBottles= exchange;
-                 // 3%4=3
+
             }
         return cnt;
     }
