47f2c89ffbe7d830157c471211d3736b039c29b6
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-22 21:23:51 +0300

    Time: 0 ms (100%), Space: 7.9 MB (46.36%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,15 +1,16 @@
+// 
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int cnt = numBottles;
-        int empty = numBottles;
         
-            while (empty >= numExchange)
+        
+            while (numBottles >= numExchange)
             {
-                         //15/4=3    6/4=1
-                cnt += (empty / numExchange);  //15+3=18  18+(6/4)
-                empty = empty / numExchange + (empty % numExchange);//3+3=6   6/4+6%4 1+2=3    1+3
-
+                int newBottles=(numBottles / numExchange);
+                cnt += newBottles;  //15+3=18  18+(6/4)
+                //empty = exchangedbottles + remain empty bottles
+                numBottles = newBottles + (numBottles % numExchange);//3+3=6   6/4+6%4 1+2=3    1+3
             }
         return cnt;
     }
