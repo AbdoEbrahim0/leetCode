74ec9c41133b8b394a744bb62335e007598cb701
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-25 23:55:36 +0300

    Time: 0 ms (100%), Space: 9.2 MB (46.43%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -66,31 +66,102 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     vector<string> findWords(vector<string>& words) {
+//         string Q = "qwertyuiop";
+//         string A = "asdfghjkl"; //capslock
+//         string Z = "zxcvbnm"; //capslock
+//         bool Qarr[26] = {};
+//         bool Aarr[26] = {};
+//         bool Zarr[26] = {};
+        
+//         for (short i = 0; i < Q.size(); i++)
+//             Qarr[Q[i] - 'a'] = 1;
+//         for (short i = 0; i < A.size(); i++)
+//             Aarr[A[i] - 'a'] = 1;        
+//         for (short i = 0; i < Z.size(); i++)
+//             Zarr[Z[i] - 'a'] = 1;
+
+//         bool flagToDelete=false;
+//         //words = ["Hello","Alaska","Dad","Peace"]
+//         for (int x=0;x<words.size();x++)
+//         {
+            
+            
+//             char myX = tolower(words[x][0]);
+//             if (Aarr[myX - 'a'])
+//             {
+//                 for (char& c : words[x])
+//                 {
+//                     if ( Aarr[tolower(c) - 'a'] == false)
+//                     {
+//                         flagToDelete = true;
+//                         break;
+//                     }
+//                 }
+//             }
+//             else if (!flagToDelete && Zarr[myX - 'a'])
+//             {
+//                 for (char& c : words[x])
+//                 {
+//                     if (Zarr[tolower(c) - 'a'] == false)
+//                     {
+//                         flagToDelete = true;
+//                         break;
+//                     }
+//                 }
+//             }
+//             //"Qwertyuiop"
+//             else if (!flagToDelete)
+//             {
+//                 for (char& c : words[x])
+//                 {
+//                     if ( Qarr[tolower(c) - 'a'] == false)
+//                     {
+//                         flagToDelete = true;
+//                         break;
+//                     }
+//                 }
+//             }
+//             if (flagToDelete)
+//             {
+//                 words.erase(words.begin() + x);
+//                 x--;
+//             }
+//             flagToDelete=false;
+//         }
+//         return words;
+//     }
+// };
+
+//more cleaning 
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
         string Q = "qwertyuiop";
         string A = "asdfghjkl"; //capslock
-        string Z = "zxcvbnm"; //capslock
+        string Z = "zxcvbnm"; //shift
         bool Qarr[26] = {};
         bool Aarr[26] = {};
         bool Zarr[26] = {};
-        
-        for (short i = 0; i < Q.size(); i++)
+        short i;
+        for (i= 0; i < 10; i++)
             Qarr[Q[i] - 'a'] = 1;
-        for (short i = 0; i < A.size(); i++)
+        i = 0;
+        for ( i = 0; i < 9; i++)
             Aarr[A[i] - 'a'] = 1;        
-        for (short i = 0; i < Z.size(); i++)
+        i = 0;
+        for (i = 0; i < 7; i++)
             Zarr[Z[i] - 'a'] = 1;
 
-        bool flagToDelete=false;
+        bool flagToDelete;
         //words = ["Hello","Alaska","Dad","Peace"]
         for (int x=0;x<words.size();x++)
         {
-            
-            
-            char myX = tolower(words[x][0]);
-            if (Aarr[myX - 'a'])
+            flagToDelete = false;
+            char whichRow = tolower(words[x][0]);
+            if (Aarr[whichRow - 'a'])
             {
                 for (char& c : words[x])
                 {
@@ -101,7 +172,7 @@ public:
                     }
                 }
             }
-            else if (!flagToDelete && Zarr[myX - 'a'])
+            else if (!flagToDelete && Zarr[whichRow - 'a'])
             {
                 for (char& c : words[x])
                 {
@@ -129,7 +200,6 @@ public:
                 words.erase(words.begin() + x);
                 x--;
             }
-            flagToDelete=false;
         }
         return words;
     }
