07aa322bf0f5b52c6a93aade3774f644f53eea1c
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-27 00:59:51 +0300

    Time: 0 ms (100%), Space: 8 MB (97.79%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,36 +1,5 @@
-// class Solution {
-// public:
-//     int isPrefixOfWord(string sentence, string searchWord) {
-//         int index = 1;
-//         short searched = 0;
-//         short wordSize = searchWord.size();
-//         short senSize = sentence.size();
-//         for (int i = 0; i < senSize; i++)
-//         {
-//             if(sentence[i] != searchWord[searched])
-//             while (i< senSize  && sentence[i] != ' ')
-//             {
-//                 i++;
-//             }
-//             // if (sentence[i] == ' ')
-//             //     index++;
 
-//             while (sentence[i] == searchWord[searched])
-//             {
-//                 searched++;
-//                 i++;
-//                 if (searched == wordSize)
-//                     return index;
-//             }
-            
-//                 searched = 0;
-//                 if (sentence[i] == ' ')
-//                 index++;
-//         }
-//         return -1;
-//     }
-// };
-
+//time : O(N) space O(1)
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
@@ -40,10 +9,6 @@ public:
         short senSize = sentence.size();
         for (int i = 0; i < senSize; i++)
         {
-            
-            //if (sentence[i] == ' ')
-                //index++;
-
             while (sentence[i] == searchWord[searched])
             {
                 searched++;
