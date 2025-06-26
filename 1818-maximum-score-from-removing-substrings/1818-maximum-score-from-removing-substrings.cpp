30fdcca8a0a579394adcc1f6026949676cdb3bf8
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-26 22:51:10 +0300

    Time: 63 ms (30.29%), Space: 35.4 MB (10.45%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -21,51 +21,93 @@
 //         return gain;
 //     }
 // };
+//gpt
+
+// class Solution {
+// public:
+//     int maximumGain(string s, int x, int y) {
+//         int total = 0;
+//         stack<char> st;
+//         string temp = "";
+
+//         // First, remove the higher scoring pair
+//         char firstA, firstB;
+//         int firstVal, secondVal;
+
+//         if (x > y) {
+//             firstA = 'a'; firstB = 'b'; firstVal = x; secondVal = y;
+//         } else {
+//             firstA = 'b'; firstB = 'a'; firstVal = y; secondVal = x;
+//         }
+
+//         // First pass: remove first pair
+//         for (char c : s) {
+//             if (!st.empty() && st.top() == firstA && c == firstB) {
+//                 st.pop();
+//                 total += firstVal;
+//             } else {
+//                 st.push(c);
+//             }
+//         }
+
+//         // Collect the remaining string from stack
+//         while (!st.empty()) {
+//             temp += st.top();
+//             st.pop();
+//         }
+//         reverse(temp.begin(), temp.end());
+
+//         // Second pass: remove second pair
+//         for (char c : temp) {
+//             if (!st.empty() && st.top() == firstB && c == firstA) {
+//                 st.pop();
+//                 total += secondVal;
+//             } else {
+//                 st.push(c);
+//             }
+//         }
+
+//         return total;
+//     }
+// };
 
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int total = 0;
-        stack<char> st;
-        string temp = "";
+        int gain = 0;
 
-        // First, remove the higher scoring pair
-        char firstA, firstB;
-        int firstVal, secondVal;
+        // Helper to remove pattern and calculate gain
+        auto removePattern = [](string& str, char a, char b, int val) -> pair<string, int> {
+            stack<char> stk;
+            int total = 0;
+            for (char c : str) {
+                if (!stk.empty() && stk.top() == a && c == b) {
+                    stk.pop();
+                    total += val;
+                } else {
+                    stk.push(c);
+                }
+            }
+            // rebuild string from stack
+            string remaining;
+            while (!stk.empty()) {
+                remaining += stk.top();
+                stk.pop();
+            }
+            reverse(remaining.begin(), remaining.end());
+            return {remaining, total};
+        };
 
         if (x > y) {
-            firstA = 'a'; firstB = 'b'; firstVal = x; secondVal = y;
+            tie(s, gain) = removePattern(s, 'a', 'b', x);
+            auto [_, g2] = removePattern(s, 'b', 'a', y);
+            gain += g2;
         } else {
-            firstA = 'b'; firstB = 'a'; firstVal = y; secondVal = x;
+            tie(s, gain) = removePattern(s, 'b', 'a', y);
+            auto [_, g2] = removePattern(s, 'a', 'b', x);
+            gain += g2;
         }
 
-        // First pass: remove first pair
-        for (char c : s) {
-            if (!st.empty() && st.top() == firstA && c == firstB) {
-                st.pop();
-                total += firstVal;
-            } else {
-                st.push(c);
-            }
-        }
-
-        // Collect the remaining string from stack
-        while (!st.empty()) {
-            temp += st.top();
-            st.pop();
-        }
-        reverse(temp.begin(), temp.end());
-
-        // Second pass: remove second pair
-        for (char c : temp) {
-            if (!st.empty() && st.top() == firstB && c == firstA) {
-                st.pop();
-                total += secondVal;
-            } else {
-                st.push(c);
-            }
-        }
-
-        return total;
+        return gain;
     }
 };
