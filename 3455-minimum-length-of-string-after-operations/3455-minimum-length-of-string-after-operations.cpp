3792108b44cf37c1d8aa72d8cb0696c84d927471
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-28 19:09:15 +0300

    Time: 21 ms (53.3%), Space: 32 MB (87.39%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -23,6 +23,29 @@
 // };
 
 //[optimized^2 in  time]
+// class Solution {
+// public:
+//     int minimumLength(string s) {
+//         //abaacbcbb
+//           //112312234
+//           //    a b c
+//           //    3%2=1 4%2=0  2/2=1 
+//         int arr[26] = {};
+//         int sSize = s.size();
+//         for (char c : s)
+//         {
+//             arr[c - 'a']++;
+//             if (arr[c - 'a'] > 2)
+//             {
+//                     sSize -= 2;
+//                     arr[c - 'a']=arr[c - 'a']-2;
+//             }
+//         }
+        
+//         return sSize;
+//     }
+// };
+////[optimized^3 in  time and mem]
 class Solution {
 public:
     int minimumLength(string s) {
@@ -30,20 +53,25 @@ public:
           //112312234
           //    a b c
           //    3%2=1 4%2=0  2/2=1 
-        int arr[26] = {};
-        int sSize = s.size();
+
+        /*lyqkwhyy
+        11111123*/
+        bitset<26>vis1,vis2;
+        
+        int remove = 0;
         for (char c : s)
         {
-            arr[c - 'a']++;
-            if (arr[c - 'a'] > 2)
+            if (!vis1[c - 'a'])
+                vis1[c - 'a'] = 1;
+            else if (!vis2[c - 'a'])
+                vis2[c - 'a'] = 1;
+            else
             {
-
-                    sSize -= 2;
-                    arr[c - 'a']=arr[c - 'a']-2;
-//                    arr[c - 'a']--;
+                remove += 2;
+                vis2[c - 'a'] = 0;
             }
         }
         
-        return sSize;
+        return s.size()-remove;
     }
 };
