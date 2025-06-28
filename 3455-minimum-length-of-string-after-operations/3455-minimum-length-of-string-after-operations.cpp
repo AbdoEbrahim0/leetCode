232f222f2b2cc843f028dbc30c79927436820e1c
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-28 18:56:26 +0300

    Time: 27 ms (49.89%), Space: 32.1 MB (69.77%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,3 +1,28 @@
+// class Solution {
+// public:
+//     int minimumLength(string s) {
+//         //abaacbcbb
+//           //112312234
+//           //    a b c
+//           //    3%2=1 4%2=0  2/2=1 
+//         int arr[26] = {};
+//         for (char c : s)
+//         {
+//             arr[c - 'a']++;
+//         }
+//         int len=0;
+//         for (int i = 0; i < 26; i++)
+//         {
+//             if (arr[i] && arr[i] % 2 == 0)
+//                 len += 2;
+//             else if (arr[i] && arr[i] % 2 == 1)
+//                 len += 1;
+//         } 
+//         return len;
+//     }
+// };
+
+//[optimized^2 in  time]
 class Solution {
 public:
     int minimumLength(string s) {
@@ -6,18 +31,19 @@ public:
           //    a b c
           //    3%2=1 4%2=0  2/2=1 
         int arr[26] = {};
+        int sSize = s.size();
         for (char c : s)
         {
             arr[c - 'a']++;
+            if (arr[c - 'a'] > 2)
+            {
+
+                    sSize -= 2;
+                    arr[c - 'a']--;
+                    arr[c - 'a']--;
+            }
         }
-        int len=0;
-        for (int i = 0; i < 26; i++)
-        {
-            if (arr[i] && arr[i] % 2 == 0)
-                len += 2;
-            else if (arr[i] && arr[i] % 2 == 1)
-                len += 1;
-        }
-        return len;
+        
+        return sSize;
     }
 };
