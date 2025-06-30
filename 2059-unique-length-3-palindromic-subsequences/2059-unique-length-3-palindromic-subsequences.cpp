21fafcfaa791c821556228d040dc14b6cb2396bf
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-06-30 06:56:19 +0300

    Time: 630 ms (11.25%), Space: 17.3 MB (21.14%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,10 +1,60 @@
+// class Solution {
+// public:
+//     int countPalindromicSubsequence(string s) {
+//         unordered_map<char, pair<int, int>>myMap;
+//         //aabca
+//         bool caontainPalindrome = false;
+//         vector< char> mapValues;
+//         for (int i=0;i<s.size();i++)
+//         {
+//             if (myMap.find(s[i]) == myMap.end())
+//             {
+//                 myMap[s[i]].first = i;
+//                 mapValues.emplace_back(s[i]);
+//             }
+//             else
+//                 myMap[s[i]].second = i;
+
+//             if (myMap[s[i]].second != 0)
+//                 caontainPalindrome = true;
+
+//             //vis[s[i] - 'a']=1;
+//         }
+        
+//         if (caontainPalindrome == false)
+//             return 0;
+//         int cnt = 0;
+//         //aabca
+//         //bbcbaba
+//         unordered_set<string> mySet;
+//         string Uniq = "";
+//         for (int x=0;x<mapValues.size();x++)
+//         {   
+//             for (int i = myMap[mapValues[x]].first +1 ; i < myMap[mapValues[x]].second; i++)//iterate on string
+//             if (i> myMap[mapValues[x]].first  && i< myMap[mapValues[x]].second)
+//             {
+//                 //cnt++;
+//                 Uniq = "";
+//                 Uniq.push_back(mapValues[x]);
+//                 Uniq.push_back(s[i]);
+//                 Uniq.push_back(mapValues[x]);
+//                 mySet.insert(Uniq);
+//             }
+//         }
+        
+//         return mySet.size();
+//     }
+// };
+
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
+        
         unordered_map<char, pair<int, int>>myMap;
         //aabca
         bool caontainPalindrome = false;
         vector< char> mapValues;
+        //O(N)
         for (int i=0;i<s.size();i++)
         {
             if (myMap.find(s[i]) == myMap.end())
@@ -17,8 +67,6 @@ public:
 
             if (myMap[s[i]].second != 0)
                 caontainPalindrome = true;
-
-            //vis[s[i] - 'a']=1;
         }
         
         if (caontainPalindrome == false)
@@ -27,19 +75,23 @@ public:
         //aabca
         //bbcbaba
         unordered_set<string> mySet;
-        string Uniq = "";
+        string Uniq = "123";
         for (int x=0;x<mapValues.size();x++)
         {   
-            for (int i = myMap[mapValues[x]].first +1 ; i < myMap[mapValues[x]].second; i++)//iterate on string
-            if (i> myMap[mapValues[x]].first  && i< myMap[mapValues[x]].second)
+
+            for (int i = myMap[mapValues[x]].first + 1; i < myMap[mapValues[x]].second; i++)//iterate on string
             {
-                //cnt++;
-                Uniq = "";
-                Uniq.push_back(mapValues[x]);
-                Uniq.push_back(s[i]);
-                Uniq.push_back(mapValues[x]);
-                mySet.insert(Uniq);
+                Uniq[0]= mapValues[x];
+                Uniq[1] = s[i];
+                Uniq[2] = mapValues[x];
+
+                if ((!mySet.count(Uniq)) && i > myMap[mapValues[x]].first && i < myMap[mapValues[x]].second)
+                {
+                    //cnt++;
+                    mySet.insert(Uniq);
+                }
             }
+            
         }
         
         return mySet.size();
