2d4d8bdc4c8c4dc5832b7ce7fa2361e5c280a1c5
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-02 03:36:50 +0300

    Time: 16 ms (78.87%), Space: 138.2 MB (59.81%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -9,24 +9,62 @@
  * };
  */
 
- class Solution {
+//  class Solution {
+// public:
+//     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+//         int dx[4] = { 0,1,0,-1 };
+//         int dy[4] = { 1,0,-1,0 };
+//         vector<vector<int>>v(m, vector<int> (n , -1));
+//         int i = 0, j = 0, dr = 0;
+//         while (head)
+//         {
+//             v[i][j] = head->val;
+//             int ii = i + dx[dr], jj = j + dy[dr];
+//             if (ii < 0 || jj < 0 || ii >= m || jj >= n || v[ii][jj] !=-1) //to avoid looping and go inner depth
+//                 dr=(dr+1 ) %4 ;
+
+//             i += dx[dr];
+//             j += dy[dr];
+//             head = head->next;
+//         }
+//         return v;
+//     }
+// };
+
+class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        int dx[4] = { 0,1,0,-1 };
-        int dy[4] = { 1,0,-1,0 };
-        vector<vector<int>>v(m, vector<int> (n , -1));
-        int i = 0, j = 0, dr = 0;
-        while (head)
-        {
-            v[i][j] = head->val;
-            int ii = i + dx[dr], jj = j + dy[dr];
-            if (ii < 0 || jj < 0 || ii >= m || jj >= n || v[ii][jj] !=-1) //to avoid looping and go inner depth
-                dr=(dr+1 ) %4 ;
-
-            i += dx[dr];
-            j += dy[dr];
-            head = head->next;
+        vector<vector<int>> ans(m,vector<int>(n,-1));
+        int top = 0, bottom = m-1, left = 0, right = n-1;
+        ListNode* temp = head;
+        while(top<=bottom&&left<=right&&temp){
+            for(int i = left;i<=right&&temp;i++){
+                ans[top][i] = temp->val;
+                temp = temp->next;
+            }
+            top++;
+            for(int i = top;i<=bottom&&temp;i++){
+                ans[i][right] = temp->val;
+                temp = temp->next;
+            }
+            right--;
+            if(left<=right){
+                for(int i = right;i>=left&&temp;i--){
+                    ans[bottom][i] = temp->val;
+                    temp = temp->next;
+                }
+                bottom--;
+            }
+            if(top<=bottom){
+                for(int i = bottom;i>=top&&temp;i--){
+                    ans[i][left] = temp->val;
+                    temp = temp->next;
+                }
+                left++;
+            }
         }
-        return v;
+        return ans;
     }
 };
+
+
