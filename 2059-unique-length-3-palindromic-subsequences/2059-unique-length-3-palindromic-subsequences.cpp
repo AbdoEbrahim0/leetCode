fbe35ca94e181c5629bec3cee8135abda8ed4607
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-02 04:54:44 +0300

    Time: 27 ms (93.15%), Space: 14.1 MB (93.71%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -98,48 +98,87 @@
 //     }
 // };
 //optimized ^3
+// class Solution {
+// public:
+//     int countPalindromicSubsequence(string s) {
+//         unordered_map<char, pair<int, int>>myMap;
+//         //aabca
+//         bool caontainPalindrome = false;
+//             char mapValuesArr[26];
+//         //O(N)
+//         for (int i=0;i<s.size();i++)
+//         {
+//             if (myMap.find(s[i]) == myMap.end())
+//                 myMap[s[i]].first = i;
+            
+//             else
+//                 myMap[s[i]].second = i;
+
+//             if (myMap[s[i]].second != 0)// if alll char appear only once so there is no palindrome
+//                 caontainPalindrome = true;
+//         }
+        
+//         if (caontainPalindrome == false)
+//             return 0;
+//         //aabca
+//         //bbcbaba
+//         unordered_set<string> mySet;
+//         string Uniq = "123";// uniqe of lenth 3
+//         for (int x=0;x<26;x++)
+//         {   
+//             for (int i = myMap[x+'a'].first + 1; i < myMap[x + 'a'].second; i++)//iterate on string
+//             {
+//                 Uniq[0]= x + 'a';
+//                 Uniq[1] = s[i];
+//                 Uniq[2] = Uniq[0];
+//                 if ((!mySet.count(Uniq)))
+//                 {
+//                     mySet.insert(Uniq);
+//                 //    break;
+//                 }
+//             }
+            
+//         }
+        
+//         return mySet.size();
+//     }
+// };
+
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_map<char, pair<int, int>>myMap;
-        //aabca
-        bool caontainPalindrome = false;
-            char mapValuesArr[26];
-        //O(N)
-        for (int i=0;i<s.size();i++)
+     // char : 1st occurence >> last Occurence
+     // a    : 0             >> 4
+     // b    : 2             >> dont find
+     // c    : 3             >> dont find
+     
+        vector<int> L(26, -1), R(26);
+        for (int i = 0; i < s.size(); i++)
         {
-            if (myMap.find(s[i]) == myMap.end())
-                myMap[s[i]].first = i;
-            
-            else
-                myMap[s[i]].second = i;
+            if(L[s[i] - 'a']==-1)
+                L[s[i] - 'a'] = i;
+            else 
+                R[s[i] - 'a'] = i;
 
-            if (myMap[s[i]].second != 0)// if alll char appear only once so there is no palindrome
-                caontainPalindrome = true;
         }
-        
-        if (caontainPalindrome == false)
-            return 0;
-        //aabca
-        //bbcbaba
-        unordered_set<string> mySet;
-        string Uniq = "123";// uniqe of lenth 3
-        for (int x=0;x<26;x++)
-        {   
-            for (int i = myMap[x+'a'].first + 1; i < myMap[x + 'a'].second; i++)//iterate on string
+        int ans = 0;
+        for (int j = 0; j < 26; j++)
+        {
+            if (R[j] == 0)//if occur once //"adc"
+                continue;
+            bitset<26>vis;
+            for (int st = L[j] + 1; st < R[j]; st++)
             {
-                Uniq[0]= x + 'a';
-                Uniq[1] = s[i];
-                Uniq[2] = Uniq[0];
-                if ((!mySet.count(Uniq)))
+                char ch = s[st];
+                //check if appear at least once and 
+                //to check if it was selected this char before it may lead to same result and we nne unique 
+                if (L[ch - 'a'] != -1 && vis[ch - 'a']==0)
                 {
-                    mySet.insert(Uniq);
-                //    break;
+                    ans++;
+                    vis[ch - 'a'] = 1;
                 }
             }
-            
         }
-        
-        return mySet.size();
+        return ans;
     }
 };
