df12e8a415ac608f0c03617d28a4757c457a51cb
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-05 23:26:45 +0300

    Time: 1463 ms (5.08%), Space: 64.3 MB (5.08%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -5,27 +5,65 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int countGoodStrings(int low, int high, int zero, int one) {
+//         const int MOD = 1e9 + 7;
+//         vector<int> dp(high + 1, 0);
+//         dp[0] = 1; // Base case: one way to make empty string
+        
+//         for (int i = 1; i <= high; ++i) {
+//             if (i >= zero) {
+//                 dp[i] = (dp[i] + dp[i - zero]) % MOD;
+//             }
+//             if (i >= one) {
+//                 dp[i] = (dp[i] + dp[i - one]) % MOD;
+//             }
+//         }
+        
+//         int result = 0;
+//         for (int i = low; i <= high; ++i) {
+//             result = (result + dp[i]) % MOD;
+//         }
+        
+//         return result;
+//     }
+// };
+
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
         const int MOD = 1e9 + 7;
-        vector<int> dp(high + 1, 0);
-        dp[0] = 1; // Base case: one way to make empty string
+        unordered_map<int, int> memo;
         
-        for (int i = 1; i <= high; ++i) {
-            if (i >= zero) {
-                dp[i] = (dp[i] + dp[i - zero]) % MOD;
-            }
-            if (i >= one) {
-                dp[i] = (dp[i] + dp[i - one]) % MOD;
-            }
-        }
+        function<int(int)> dp = [&](int len) {
+            if (len == 0) return 1;
+            if (len < 0) return 0;
+            if (memo.count(len)) return memo[len];
+            
+            int res = (dp(len - zero) + dp(len - one)) % MOD;
+            memo[len] = res;
+            return res;
+        };
         
         int result = 0;
         for (int i = low; i <= high; ++i) {
-            result = (result + dp[i]) % MOD;
+            result = (result + dp(i)) % MOD;
         }
         
         return result;
     }
 };
+// class Solution {
+// public:
+//     int countGoodStrings(int low, int high, int zero, int one) {
+
+//     }
+//     };
