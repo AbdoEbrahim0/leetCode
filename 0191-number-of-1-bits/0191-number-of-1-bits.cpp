ad35568d982785ba836b507826224aea853ce573
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-08 00:10:19 +0300

    Time: 0 ms (100%), Space: 8.2 MB (47.09%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -11,19 +11,32 @@
 //         return cnt;
 //     }
 // };
+// class Solution {
+// public:
+//     int hammingWeight(int n) {
+//         int cnt = 0;
+//         for (int i=0;i<32;i++)
+//         {
+//             //int  digit = (1 << i); //1 2 4 8 16 32 64 128
+
+//             if (n & (1 << i))  //128 =10000000   & 1=00000001   =0 
+//                             //128 =10000000   & 1=00000010   =0
+//                 cnt++;   
+//         }
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
     int hammingWeight(int n) {
         int cnt = 0;
+        bitset<32>binaryX(n);
         for (int i=0;i<32;i++)
         {
-            //int  digit = (1 << i); //1 2 4 8 16 32 64 128
-            
-            if (n & (1 << i))  //128 =10000000   & 1=00000001   =0 
-                            //128 =10000000   & 1=00000010   =0
+            if (binaryX[i] == 1)
                 cnt++;   
         }
-        
         return cnt;
     }
 };
