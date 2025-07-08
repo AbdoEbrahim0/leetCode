a6d5a9bf60cba16ca271f4456a7b0cf36631af04
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-08 11:10:09 +0300

    Time: 0 ms (100%), Space: 7.8 MB (57.71%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,20 +1,13 @@
-// class Solution {
-// public:
-//     string convertToTitle(int columnNumber) {
-//         char arr[26];
-//         for (int i = 0; i < 26; i++)
-//             arr[i] = char('a' + i);
-//         string res;
-//         for(int i = 0; i < columnNumber / 26;i++)
-//         {
-//             res += toupper(arr[i]);
-//         }
-//         res += toupper(arr[columnNumber % 26]) ;
-        
+// 1>>A
+// 28>>AB
+// 701>>ZY    
+// 52>>AZ
+  
+//2147483647 >>"FXSHRXW"
+//2147483647%26=23 arr[23-1]=W
+//82,595,524.884615384615384615384615  82,595,524%26=24 arr[24-1]=X 
+//  not valid >>> 82,595,524+1 %26 =25 arr[25-1]=Y
 
-//         return res;
-//     }
-// };
 // class Solution {
 // public:
 //     string convertToTitle(int columnNumber) {
@@ -22,42 +15,29 @@
 //         for (int i = 0; i < 26; i++)
 //             arr[i] = char('A' + i);
 //         string res;
-//         for (int i = 0; i < columnNumber / 26; i++)
+//         while ( columnNumber >26)
 //         {
-//             res += arr[i];
+//             res += arr[(--columnNumber) % 26 ];//decrement to as our array 0 based
+//                         columnNumber = columnNumber / 26;
 //         }
-//         res += arr[(columnNumber % 26 )-1];
-
-
+//         res += arr[columnNumber-1];
+//         reverse(res.begin(), res.end());
 //         return res;
 //     }
 // }; 
 
+
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        char arr[26];
-        for (int i = 0; i < 26; i++)
-            arr[i] = char('A' + i);
         string res;
-        //int lastChar = (columnNumber % 26) - 1;
-        while ( columnNumber >26)
+        while ( columnNumber >0)
         {
-            // if((columnNumber % 26) -1 <0)
-            // res+='Z';
-            // else
-            res += arr[(--columnNumber) % 26 ];
-            columnNumber = columnNumber / 26.0;
+           short reminder= (--columnNumber)% 26;
+            res +=char('A'+reminder);//decrement to as our array 0 based
+            columnNumber = columnNumber / 26;
         }
-        res += arr[columnNumber-1];
-        // to produce result at coulmn number =2147483647  out :"FXSHRXW"
-        //2147483647%26=23 arr[23-1]=W
-        //82,595,524.884615384615384615384615  82,595,524%26=24 arr[24-1]=X   not valid >>> 82,595,524+1 %26 =25 arr[25-1]=Y
         reverse(res.begin(), res.end());
         return res;
-            // 1>>A
-    // 28>>AB
-    // 701>>ZY    
-    // 52>>AZ
     }
 }; 
