a40fc8307a6a13415b3b3c9a4a79760b6e144b8b
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-08 10:51:26 +0300

    Time: 0 ms (100%), Space: 7.9 MB (22.98%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -33,39 +33,31 @@
 //     }
 // }; 
 
-// class Solution {
-// public:
-//     string convertToTitle(int columnNumber) {
-//         char arr[26];
-//         for (int i = 0; i < 26; i++)
-//             arr[i] = char('A' + i);
-//         string res;
-//         int lastChar = (columnNumber % 26) - 1;
-//         for (int i = 0; i < columnNumber / 26; i++)
-//         {
-//             res += arr[columnNumber / 26 -1];
-//             columnNumber = columnNumber / 26;
-//         }
-//         res += arr[lastChar];
-
-
-//         return res;
-//     }
-// }; 
-
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
+        char arr[26];
+        for (int i = 0; i < 26; i++)
+            arr[i] = char('A' + i);
         string res;
-
-        while (columnNumber > 0) {
-            columnNumber--;  // adjust for 1-based indexing
-            int remainder = columnNumber % 26;
-            res += (char)('A' + remainder);
-            columnNumber /= 26;
+        //int lastChar = (columnNumber % 26) - 1;
+        while ( columnNumber >26)
+        {
+            // if((columnNumber % 26) -1 <0)
+            // res+='Z';
+            // else
+            res += arr[(--columnNumber) % 26 ];
+            columnNumber = columnNumber / 26.0;
         }
-
+        res += arr[columnNumber-1];
+        // to produce result at coulmn number =2147483647  out :"FXSHRXW"
+        //2147483647%26=23 arr[23-1]=W
+        //82,595,524.884615384615384615384615  82,595,524%26=24 arr[24-1]=X   not valid >>> 82,595,524+1 %26 =25 arr[25-1]=Y
         reverse(res.begin(), res.end());
         return res;
+            // 1>>A
+    // 28>>AB
+    // 701>>ZY    
+    // 52>>AZ
     }
-};
+}; 
