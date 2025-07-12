c36691eee98066f0b0f19ddf633f1d27270d1393
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-12 10:38:20 +0300

    Time: 1 ms (33.03%), Space: 8.4 MB (22.85%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -24,6 +24,7 @@
 //         return n==1;
 //     }
 // };
+
 //optimized ^2
 // class Solution {
 // public:
@@ -48,51 +49,52 @@
 //         return true;
 //     }
 // };
+
 //optimized ^3
-class Solution {
-public:
-    bool isHappy(int n) {
-        //n=19 true  ,  n=2 = false
-        //important  :: in case n=2  >> 2 4 16 37 58 89 145 42 20    4 16 37 58
-        bitset<1000 + 1> vis; //i selecet 1000 by trial to pass all testcases all bigger number than 1000 is valid 
-        //unordered_set<int>mySet;
-        string s;
-        while (n != 1)
-        {
-            s = to_string(n);
-            n = 0;
-            for (int i = 0; i < s.size(); i++)
-                n += (s[i] - '0') * (s[i] - '0');
-            if (vis[n])
-                return 0;
-            vis[n] = true;
-        }
-        return true;
-    }
-};
-
-//optimized ^4 gpt 
 // class Solution {
-//     public:
+// public:
 //     bool isHappy(int n) {
-//         unordered_set<int> seen;
-
-//         while (n != 1 && !seen.count(n)) {
-//             seen.insert(n);
-//             n = getNext(n);
+//         //n=19 true  ,  n=2 = false
+//         //important  :: in case n=2  >> 2 4 16 37 58 89 145 42 20    4 16 37 58
+//         bitset<1000 + 1> vis; //i selecet 1000 by trial to pass all testcases all bigger number than 1000 is valid 
+//         //unordered_set<int>mySet;
+//         string s;
+//         while (n != 1)
+//         {
+//             s = to_string(n);
+//             n = 0;
+//             for (int i = 0; i < s.size(); i++)
+//                 n += (s[i] - '0') * (s[i] - '0');
+//             if (vis[n])
+//                 return 0;
+//             vis[n] = true;
 //         }
-
-//         return n == 1;
-//     }
-
-// private:
-//     int getNext(int n) {
-//         int total = 0;
-//         while (n > 0) {
-//             int digit = n % 10;
-//             total += digit * digit;
-//             n /= 10;
-//         }
-//         return total;
+//         return true;
 //     }
 // };
+
+//optimized ^4 gpt 
+class Solution {
+    public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while (n != 1 && !seen.count(n)) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+
+        return n == 1;
+    }
+
+private:
+    int getNext(int n) {
+        int total = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            total += digit * digit;
+            n /= 10;
+        }
+        return total;
+    }
+};
