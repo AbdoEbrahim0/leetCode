fcfaa0d73bdf9c6e3e291a9892560ce290e7cb40
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-12 10:33:28 +0300

    Time: 0 ms (100%), Space: 8.8 MB (6.39%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -4,7 +4,7 @@
 //         //n=19 true  ,  n=2 = false
 //         // realize that only happy number is when n=1 OR n=10 OR n=100 OR n=1000 etc  
 //         //2,147,483,648   1 000 000 000
-//         //important  :: in case 2 4 16 37 58 89 145 42 20  4 16
+           //important  :: in case n=2  >> 2 4 16 37 58 89 145 42 20    4 16 37 58 
 //         int x = 0;
 //         unordered_set<int>mySet;
 //         string s;
@@ -30,8 +30,7 @@
 //     bool isHappy(int n) {
 //         //n=19 true  ,  n=2 = false
 //         // realize that only happy number is when n=1 OR n=10 OR n=100 OR n=1000 etc  
-//         //2,147,483,648   1 000 000 000
-//         //important  :: in case 2 4 16 37 58 89 145 42 20  4 16
+//         //important  :: in case n=2  >> 2 4 16 37 58 89 145 42 20    4 16 37 58
 //         unordered_set<int>mySet;
 //         string s;
 //         while (n!=1 )
@@ -49,14 +48,13 @@
 //         return true;
 //     }
 // };
+//optimized ^3
 class Solution {
 public:
     bool isHappy(int n) {
         //n=19 true  ,  n=2 = false
-        // realize that only happy number is when n=1 OR n=10 OR n=100 OR n=1000 etc  
-        //2,147,483,648   1 000 000 000
-        //important  :: in case 2 4 16 37 58 89 145 42 20  4 16
-        bitset<1000 + 1> vis;
+        //important  :: in case n=2  >> 2 4 16 37 58 89 145 42 20    4 16 37 58
+        bitset<1000 + 1> vis; //i selecet 1000 by trial to pass all testcases all bigger number than 1000 is valid 
         //unordered_set<int>mySet;
         string s;
         while (n != 1)
@@ -65,7 +63,6 @@ public:
             n = 0;
             for (int i = 0; i < s.size(); i++)
                 n += (s[i] - '0') * (s[i] - '0');
-            
             if (vis[n])
                 return 0;
             vis[n] = true;
@@ -74,7 +71,7 @@ public:
     }
 };
 
-
+//optimized ^4 gpt 
 // class Solution {
 //     public:
 //     bool isHappy(int n) {
