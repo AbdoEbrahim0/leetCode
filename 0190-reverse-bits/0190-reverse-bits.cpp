9e31d768747d6edbc0f3d8675e26fe0847e5f46f
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-12 08:26:33 +0300

    Time: 4 ms (32.06%), Space: 7.8 MB (26.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,33 +1,3 @@
-// class Solution {
-// public:
-//     uint32_t reverseBits(uint32_t n) {
-//         uint32_t result = 0;
-//         //00000010100101000001111010011100 43261596
-//         //00111001011110000010100101000000 964176192
-//         for (int i = 31; i >= 0; i--)
-//         {
-//             if (n  & 1) 
-//                 result += (1<<i);
-//             n = (n >> 1) ;
-//         }
-//         return result;
-//     }
-// };
-
-class Solution {
-public:
-    uint32_t reverseBits(uint32_t n) {
-        uint32_t result = 0;
-        for (int i = 0; i < 32; ++i) {
-            result <<= 1;           // Make space for the next bit
-            result |= (n & 1);      // Add the last bit of n to result
-            n >>= 1;                // Move to the next bit
-        }
-        return result;
-    }
-};
-
-
 
 // class Solution {
 // public:
@@ -44,3 +14,36 @@ public:
 //         return (uint32_t)Original.to_ulong();
 //     }
 // };
+
+//optimized ^2
+// class Solution {
+// public:
+//     uint32_t reverseBits(uint32_t n) {
+//         uint32_t result = 0;
+//         //00000010100101000001111010011100 43261596
+//         //00111001011110000010100101000000 964176192
+//         for (int i = 31; i >= 0; i--)
+//         {
+//             if (n  & 1) 
+//                 result += (1<<i);
+//             n = (n >> 1) ;
+//         }
+//         return result;
+//     }
+// };
+// more clean and elgant for OPtimized ^2
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (int i = 0; i < 32; ++i) {
+            result <<= 1;           // Make space for the next bit
+            result |= (n & 1);      // Add the last bit of n to result
+            n >>= 1;                // Move to the next bit
+        }
+        return result;
+    }
+};
+
+
+
