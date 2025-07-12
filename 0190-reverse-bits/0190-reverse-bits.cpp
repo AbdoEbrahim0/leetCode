fc249041c7a4b6cf6a8ee40baed4b9773e7900e4
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-12 08:24:35 +0300

    Time: 2 ms (43.67%), Space: 7.7 MB (87.24%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -17,54 +17,30 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        //00000010100101000001111010011100 43261596
-        //00111001011110000010100101000000 964176192
-        bitset<32>Original(n);
-        
-        for (short x = 0; x < 16; x++)
-        {
-            bool temp = Original[x];
-            Original[x] = Original[31 - x];
-            Original[31 - x] = temp;
+        uint32_t result = 0;
+        for (int i = 0; i < 32; ++i) {
+            result <<= 1;           // Make space for the next bit
+            result |= (n & 1);      // Add the last bit of n to result
+            n >>= 1;                // Move to the next bit
         }
-        
-        return (uint32_t)Original.to_ulong();
+        return result;
     }
 };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // for (int i = 0; i < 32; ++i) {
-        //     result <<= 1;           // Make space for the next bit
-        //     result |= (n & 1);      // Add the last bit of n to result
-        //     n >>= 1;                // Move to the next bit
-        // }
-        
-        // return result;
+// class Solution {
+// public:
+//     uint32_t reverseBits(uint32_t n) {
+//         //00000010100101000001111010011100 43261596
+//         //00111001011110000010100101000000 964176192
+//         bitset<32>Original(n);
+//         for (short x = 0; x < 16; x++)
+//         {
+//             bool temp = Original[x];
+//             Original[x] = Original[31 - x];
+//             Original[31 - x] = temp;
+//         }
+//         return (uint32_t)Original.to_ulong();
+//     }
+// };
