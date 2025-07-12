194c5c3f5f80a8a718c61c3340e9625889a3ed25
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-12 09:19:10 +0300

    Time: 5 ms (4.14%), Space: 9.4 MB (5.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,51 @@
 class Solution {
-    public:
+public:
     bool isHappy(int n) {
-        unordered_set<int> seen;
-
-        while (n != 1 && !seen.count(n)) {
-            seen.insert(n);
-            n = getNext(n);
+        //n=19 true  ,  n=2 = false
+        // realize that only happy number is when n=1 OR n=10 OR n=100 OR n=1000 etc  
+        //2,147,483,648   1 000 000 000
+        //important  :: in case 2 4 16 37 58 89 145 42 20  4 16
+        int x = 0;
+        unordered_set<int>mySet;
+        string s;
+        while (n!=1 )
+        {
+            s = to_string(n);
+            for (int i = 0; i < s.size(); i++)
+            {
+                x += (s[i] - '0')* (s[i] - '0');
+            }
+            if (mySet.count(x))
+                return 0;
+            mySet.insert(x);
+            n = x;
+            x = 0;
         }
-
-        return n == 1;
-    }
-
-private:
-    int getNext(int n) {
-        int total = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            total += digit * digit;
-            n /= 10;
-        }
-        return total;
+        return n==1;
     }
 };
+
+// class Solution {
+//     public:
+//     bool isHappy(int n) {
+//         unordered_set<int> seen;
+
+//         while (n != 1 && !seen.count(n)) {
+//             seen.insert(n);
+//             n = getNext(n);
+//         }
+
+//         return n == 1;
+//     }
+
+// private:
+//     int getNext(int n) {
+//         int total = 0;
+//         while (n > 0) {
+//             int digit = n % 10;
+//             total += digit * digit;
+//             n /= 10;
+//         }
+//         return total;
+//     }
+// };
