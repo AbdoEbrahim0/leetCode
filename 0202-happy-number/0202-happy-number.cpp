4b41a3e032749eb38d67a730c463b760ef9f3f22
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-12 10:23:37 +0300

    Time: 2 ms (27.73%), Space: 9.1 MB (5.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -25,6 +25,30 @@
 //     }
 // };
 //optimized ^2
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//         //n=19 true  ,  n=2 = false
+//         // realize that only happy number is when n=1 OR n=10 OR n=100 OR n=1000 etc  
+//         //2,147,483,648   1 000 000 000
+//         //important  :: in case 2 4 16 37 58 89 145 42 20  4 16
+//         unordered_set<int>mySet;
+//         string s;
+//         while (n!=1 )
+//         {
+//             s = to_string(n);
+//             n = 0;
+//             for (int i = 0; i < s.size(); i++)
+//             {
+//                 n += (s[i] - '0')* (s[i] - '0');
+//             }
+//             if (mySet.count(n))
+//                 return 0;
+//             mySet.insert(n);
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isHappy(int n) {
@@ -32,23 +56,24 @@ public:
         // realize that only happy number is when n=1 OR n=10 OR n=100 OR n=1000 etc  
         //2,147,483,648   1 000 000 000
         //important  :: in case 2 4 16 37 58 89 145 42 20  4 16
-        unordered_set<int>mySet;
+        bitset<1000000 + 1> vis;
+        //unordered_set<int>mySet;
         string s;
-        while (n!=1 )
+        while (n != 1)
         {
             s = to_string(n);
             n = 0;
-            for (short i = 0; i < s.size(); i++)
-            {
-                n += (s[i] - '0')* (s[i] - '0');
-            }
-            if (mySet.count(n))
+            for (int i = 0; i < s.size(); i++)
+                n += (s[i] - '0') * (s[i] - '0');
+            
+            if (vis[n])
                 return 0;
-            mySet.insert(n);
+            vis[n] = true;
         }
         return true;
     }
 };
+
 
 // class Solution {
 //     public:
