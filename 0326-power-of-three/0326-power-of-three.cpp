11e9061ac1749cb2d128b508cba2701ea65bc58a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-15 19:38:10 +0300

    Time: 4 ms (47.28%), Space: 9.1 MB (5.21%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,4 +1,21 @@
 //O(N/3) =N
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//     if (n <= 0) return 0;
+//         bool flag = true;
+//         while (n != 1 && flag)
+//         {
+//             if ((n / 3.0) != ceil(n / 3.0))
+//                 flag = false;
+
+//             n = n / 3;
+//         }
+//         return flag;
+//     }
+// };
+
+//optimized^2 in MEM and time
 class Solution {
 public:
     bool isPowerOfThree(int n) {
@@ -12,4 +29,5 @@ return n == 1;
 
     }
 };
+
 
