1e01cf53e8e4bb14fb0b8ffff4480ba6453e6b85
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-16 21:36:20 +0300

    Time: 0 ms (100%), Space: 9.3 MB (57.33%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,48 +1,48 @@
-// class Solution {
-// public:
-//     bool isIsomorphic(string s, string t) {
-//         // if (s.size() != t.size())
-//         //     return false;
-//         unordered_map<char,char> m1;
-//         unordered_set<char> appearAgain;
-//         for (int i=0;i< s.size();i++)
-//         {
-//             if (m1.find(s[i]) != m1.end())
-//             {
-//                 if (m1[s[i]] != t[i])
-//                     return false;
-//             }
-//             else
-//             {
-//                 //s ="badc", t ="baba";
-//                 if (appearAgain.count(t[i]))
-//                     return false;
-//                 m1[s[i]] = t[i];
-//                 appearAgain.insert(t[i]);
-//             }
-//         }
-//         return true;
-//     }
-// };
-
-//optimizd ^2
-
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.length() != t.length()) return false;
-        unordered_map<char, char> mapST, mapTS;
-        for (int i = 0; i < s.length(); ++i) {
-            char c1 = s[i];
-            char c2 = t[i];
-            // Check s → t mapping
-            if (mapST.count(c1) && mapST[c1] != c2) return false;
-            // Check t → s mapping
-            if (mapTS.count(c2) && mapTS[c2] != c1) return false;
-            mapST[c1] = c2;
-            mapTS[c2] = c1;
+        // if (s.size() != t.size())
+        //     return false;
+        unordered_map<char,char> m1;
+        unordered_set<char> appearAgain;
+        for (int i=0;i< s.size();i++)
+        {
+            if (m1.count(s[i]))
+            {
+                if (m1[s[i]] != t[i])
+                    return false;
+            }
+            else
+            {
+                //s ="badc", t ="baba";
+                if (appearAgain.count(t[i]))
+                    return false;
+                m1[s[i]] = t[i];
+                appearAgain.insert(t[i]);
+            }
         }
-
         return true;
     }
 };
+
+//optimizd ^2
+
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         if (s.length() != t.length()) return false;
+//         unordered_map<char, char> mapST, mapTS;
+//         for (int i = 0; i < s.length(); ++i) {
+//             char c1 = s[i];
+//             char c2 = t[i];
+//             // Check s → t mapping
+//             if (mapST.count(c1) && mapST[c1] != c2) return false;
+//             // Check t → s mapping
+//             if (mapTS.count(c2) && mapTS[c2] != c1) return false;
+//             mapST[c1] = c2;
+//             mapTS[c2] = c1;
+//         }
+
+//         return true;
+//     }
+// };
