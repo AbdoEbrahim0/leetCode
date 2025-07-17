85d30ce6022f3e67e2308953d8594a2fce5735be
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-17 11:10:29 +0300

    Time: 0 ms (100%), Space: 10.6 MB (58.23%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -20,14 +20,40 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> res;
+//         for (int i = 0; i <= n; i++) //O(N)
+//         {
+//             bitset<32>bits(i);   
+//             res.emplace_back(bits.count());
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
+/*
+0    000  
+1    001
+res[1]=res[1>>1 =0] +(1&1) =0 + 1 =1                       
+2    010
+res[2]=res[2>>1 =1] +(2&1) =1 + (10 & 01 ))  =0 + 1=1
+3    011
+res[3]=res[3>>1 =2] +(3&1) =1 + (11 & 01 ))  =1 + 1=2
+4    100
+res[4]=res[4>>1 =2] +(4&1) =1 + (100 & 001)) =1 + 0=1
+5    101 
+res[5]=res[5>>1 =2] +(5&1) =1 + (101 & 001)) =1 + 1=2
+
+*/
     vector<int> countBits(int n) {
-        vector<int> res;
-        for (int i = 0; i <= n; i++) //O(N)
-        {
-            bitset<32>bits(i);   
-            res.emplace_back(bits.count());
+        vector<int> res(n + 1);
+        res[0] = 0; // base case
+        for (int i = 1; i <= n; ++i) {
+            res[i] = res[i >> 1] + (i & 1);
         }
         return res;
     }
