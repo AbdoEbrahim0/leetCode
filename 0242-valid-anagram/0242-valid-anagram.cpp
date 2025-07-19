4bf6cc8adf54af48a04abdeabc0ed4fd393f4a36
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-19 23:30:19 +0300

    Time: 0 ms (100%), Space: 9.7 MB (74.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,60 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size() != t.size())
+//             return false;
+//         short arrS[26] = {};
+//         short arrT[26] = {};
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             arrS[s[i] - 'a']++;
+//             arrT[t[i] - 'a']++;
+//         }
+//         for (int i = 0; i <26; i++)
+//         {
+//             if (arrS[i] != arrT[i])
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size())
             return false;
-        short arrS[26] = {};
-        short arrT[26] = {};
-        for (int i = 0; i < s.size(); i++)
-        {
-            arrS[s[i] - 'a']++;
-            arrT[t[i] - 'a']++;
+        int count[26] = {};
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-        for (int i = 0; i <26; i++)
-        {
-            if (arrS[i] != arrT[i])
-                return false;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
         }
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size() != t.size())
+//             return false;
+//         bitset<26>arrS;
+//         bitset<26>arrT;
+//         //short arrS[26] = {};
+//         //short arrT[26] = {};
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             arrS[s[i] - 'a']=~arrS[s[i] - 'a'];
+//             arrT[t[i] - 'a']= ~arrT[t[i] - 'a'];
+//         }
+//         for (int i = 0; i <26; i++) //O(1)
+//         {
+//             if (arrS[i] != arrT[i])
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
