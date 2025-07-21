c029bb9f4c3afd6df4ec75b3a4ca36fac9d7958f
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-21 23:25:55 +0300

    Time: 0 ms (100%), Space: 8.5 MB (78.46%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int addDigits(int & num) {
-        //int arr[32] = {};
-        //int i = 0;
+    int addDigits(int num) {
         int newNum = 0;
         while (num != 0)
         {
-           // arr[i] = num % 10;
             newNum += num % 10;
             num = num / 10;
-            //i++;
         }
         if (newNum / 10 == 0)
             return newNum;
