ffe9d1c9f7b6b3fb78434c8a53386294cc5a0f69
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-26 21:49:52 +0300

    Time: 3 ms (33.59%), Space: 14.6 MB (11.44%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -8,25 +8,50 @@
 //     }
 // };
 // 1 2 2 3
+// class Solution {
+// public:
+//     int thirdMax(vector<int>& nums) {
+//         set<int > mySet(nums.begin(), nums.end());
+//         int mySetSize = mySet.size();
+        
+//         if (mySetSize < 3)
+//         {
+//             auto it=mySet.end();
+//             it--;
+//             return *it;
+//         }
+//         int i = 0;
+//         nums.resize(mySet.size());
+//         for (auto it = mySet.begin(); i< mySet.size(); it++)
+//         {
+//             nums [i++] = *it;
+//         }
+//         return nums[nums.size()- 3];
+//     }
+// };
+
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         set<int > mySet(nums.begin(), nums.end());
         int mySetSize = mySet.size();
-        
+        auto it=mySet.end();
         if (mySetSize < 3)
         {
             auto it=mySet.end();
             it--;
             return *it;
         }
-            
         int i = 0;
-        nums.resize(mySet.size());
-        for (auto it = mySet.begin(); i< mySet.size(); it++)
-        {
-            nums [i++] = *it;
-        }
-        return nums[nums.size()- 3];
+        // nums.resize(mySet.size());
+        // for (auto it = mySet.begin(); i< mySet.size(); it++)
+        // {
+        //     nums [i++] = *it;
+        // }
+        //return nums[nums.size()- 3];
+        it--;
+        it--;
+        it--;
+        return *(it);
     }
 };
