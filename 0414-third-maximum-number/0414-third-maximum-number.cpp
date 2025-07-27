10544dadf08057555947192e2dfa77c29fd4a957
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-07-27 22:27:42 +0300

    Time: 0 ms (100%), Space: 14.2 MB (33.2%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -30,20 +30,44 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int thirdMax(vector<int>& nums) {
+//         set<int > mySet(nums.begin(), nums.end());
+//         int mySetSize = mySet.size();
+//         auto it=mySet.end();
+//         if (mySetSize < 3)
+//         {
+//             it--;
+//             return *it;
+//         }
+//         it--;
+//         it--;
+//         it--;
+//         return *(it);
+//     }
+// };
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        set<int > mySet(nums.begin(), nums.end());
-        int mySetSize = mySet.size();
-        auto it=mySet.end();
-        if (mySetSize < 3)
+        
+        unordered_set<int> mySet;
+
+        priority_queue<int, vector<int>, greater<int>> pr;
+        for (int val :nums)
         {
-            it--;
-            return *it;
+            //{ 3,5,8,20 };
+            
+            if ( mySet.count(val) ==0)
+            {
+                pr.push(val);
+                mySet.insert(val);
+            }
+            if (pr.size() > 3)
+                pr.pop();
         }
-        it--;
-        it--;
-        it--;
-        return *(it);
+if (mySet.size()<3)
+return *max_element(nums.begin(), nums.end());
+        return pr.top();
     }
 };
