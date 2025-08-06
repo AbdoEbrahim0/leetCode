8ec6b05e7f47e1ce29ea014f9ce0c9a5ecd1632c
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-06 22:22:28 +0300

    Time: 1 ms (57.27%), Space: 8.8 MB (31.13%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.c
+++ 0383-ransom-note/0383-ransom-note.c
@@ -1,8 +1,16 @@
+int getSize(char* ransomNote)
+{
+    int c=0;
+    while(ransomNote[c] !='\0')
+    c++;
+
+    return c; 
+}
 bool canConstruct(char* ransomNote, char* magazine) {
     int arr1[26] = {};
     int arr2[26] = {};
-    int ranSize = strlen(ransomNote);
-    int magSize = strlen(magazine);
+    int ranSize = getSize(ransomNote);
+    int magSize = getSize(magazine);
     
     for (int x = 0; x < ranSize; x++)
     {
