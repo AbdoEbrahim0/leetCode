49aa67a81e381a33fe4dad0044e06e7be6aed0fe
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-11 22:56:41 +0300

    Time: 2 ms (74.16%), Space: 7.8 MB (47.99%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -1,16 +1,33 @@
 
+//recursion
+// int fib(int n) {
+//     if(n==0)
+//     return 0;
+//     if (n <= 2)
+//         return 1;
+    
+//     else
+//     return fib(n-1) + fib(n-2);
+// }
 
 int fib(int n) {
-    if(n==0)
-    return 0;
-    if (n <= 2)
+    //(0)=0  (1)=1
+    int prev=0, cur=1,out=0;
+        if(n==1)
         return 1;
-    //else if(n<)
-    else
-    return fib(n-1) + fib(n-2);
+        if(n==0)
+        return 0;
+    for (int x = 2; x <= n; x++)
+    {
+        out = (prev + cur);
+        prev = cur;
+        cur = out;
+    }
+    return out;
 }
 /*
-3
-2,1
-1,0
+n=3
+n=2     ,n=1
+n=1,n=0 ,n=1
+
 */
