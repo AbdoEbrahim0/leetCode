347f121248a7432adf6148c6c7630db82b679328
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-14 00:06:05 +0300

    Time: 0 ms (100%), Space: 7.8 MB (69.87%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.c
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.c
@@ -1,25 +1,65 @@
 bool checkRecord(char* s) {
-    int absentCount = 0;
-    int lateStreak = 0;
-    
-    for (int i = 0; s[i] != '\0'; i++) {
-        if (s[i] == 'A') {
-            absentCount++;
-            if (absentCount >= 2) {
-                return false;
-            }
-            lateStreak = 0; // Reset late streak on 'A'
-        } 
-        else if (s[i] == 'L') {
-            lateStreak++;
-            if (lateStreak >= 3) {
-                return false;
-            }
-        } 
-        else { // 'P'
-            lateStreak = 0; // Reset late streak on 'P'
-        }
-    }
-    
-    return true;
+	int idx = 0;
+	int absent = 0, late = 0;
+	while (s[idx] != '\0')
+	{
+		if (s[idx] == 'A')
+		{
+			absent++;
+			if (absent >= 2)
+				return false;
+		}
+		else if( idx>= 2 && s[idx] == 'L' && s[idx-1] == 'L' && s[idx-2] == 'L')
+		{
+			return false;
+		}
+		idx++;
+	}
+	return true;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool checkRecord(char* s) {
+//     int absentCount = 0;
+//     int lateStreak = 0;
+    
+//     for (int i = 0; s[i] != '\0'; i++) {
+//         if (s[i] == 'A') {
+//             absentCount++;
+//             if (absentCount >= 2) {
+//                 return false;
+//             }
+//             lateStreak = 0; // Reset late streak on 'A'
+//         } 
+//         else if (s[i] == 'L') {
+//             lateStreak++;
+//             if (lateStreak >= 3) {
+//                 return false;
+//             }
+//         } 
+//         else { // 'P'
+//             lateStreak = 0; // Reset late streak on 'P'
+//         }
+//     }
+    
+//     return true;
+// }
