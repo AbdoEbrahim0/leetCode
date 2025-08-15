5adb6e524be19ab334d547895d97d5659c0e529e
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-16 00:20:29 +0300

    Time: 0 ms (100%), Space: 9.3 MB (31.87%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.c
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.c
@@ -1,25 +1,54 @@
-//gpt
+// //gpt
 
-void reverseRange(char *s, int left, int right) {
-    while (left < right) {
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        left++;
-        right--;
-    }
-}
+// void reverseRange(char *s, int left, int right) {
+//     while (left < right) {
+//         char temp = s[left];
+//         s[left] = s[right];
+//         s[right] = temp;
+//         left++;
+//         right--;
+//     }
+// }
 
-char* reverseWords(char* s) {
-    int n = strlen(s);
-    int start = 0;
+// char* reverseWords(char* s) {
+//     int n = strlen(s);
+//     int start = 0;
     
-    for (int i = 0; i <= n; i++) {
-        if (s[i] == ' ' || s[i] == '\0') {
-            reverseRange(s, start, i - 1);
-            start = i + 1;
-        }
-    }
+//     for (int i = 0; i <= n; i++) {
+//         if (s[i] == ' ' || s[i] == '\0') {
+//             reverseRange(s, start, i - 1);
+//             start = i + 1;
+//         }
+//     }
     
-    return s;
+//     return s;
+// }
+
+char* reverseWords(char* s) { 
+	int idx = 0;
+	int toSwap =0;
+	char temp;
+	int iterate = 0;
+	char cur= *(s + idx);
+	while (cur  != '\0')
+	{
+		
+		while (*(s + idx) != ' ' && cur != '\0')
+		{
+			idx++;
+			cur = *(s + idx);
+		}
+		toSwap = idx-1;
+		while (  iterate< toSwap)
+		{
+			temp = s[iterate];
+			s[iterate] = s[toSwap];
+			s[toSwap] = temp;
+			toSwap--;
+			iterate++;
+		}
+		iterate = idx + 1;
+		idx++;
+	}
+	return s;
 }
