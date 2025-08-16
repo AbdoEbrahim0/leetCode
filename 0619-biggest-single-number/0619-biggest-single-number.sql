ba2414936b36e61db37813dd4fce813cad35a46e
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-16 23:27:25 +0300

    Time: 408 ms (69.12%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,18 @@
 # Write your MySQL query statement below
+-- SELECT MAX(num) AS num
+-- FROM MyNumbers
+-- WHERE num NOT IN (
+--     SELECT num
+--     FROM MyNumbers
+--     GROUP BY num
+--     HAVING COUNT(*) > 1
+-- );
+
 SELECT MAX(num) AS num
 FROM MyNumbers
-WHERE num NOT IN (
+WHERE num IN (
     SELECT num
     FROM MyNumbers
     GROUP BY num
-    HAVING COUNT(*) > 1
+    HAVING COUNT(num) = 1
 );
