86b806bc0d48d3f8254ad5ced11b39034388e8c9
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-21 13:41:11 +0300

    Time: 0 ms (100%), Space: 8.5 MB (50.72%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,19 +1,51 @@
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if(x==1)
+//         return 1;
+//         else if (x==0)
+//         return 0;
+//         int i;
+//         for ( i=0;i<= x/2;i++)
+//         {
+//         if (i>=46341 ||i*i > x)
+//         {
+//             break;
+//         }
+
+//         }
+//         return i-1;
+//     }
+// };
+
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==1)
-        return 1;
-        else if (x==0)
-        return 0;
-        int i;
-        for ( i=0;i<= x/2;i++)
-        {
-        if (i>=46341 ||i*i > x)
-        {
-            break;
-        }
+        if (x == 1)
+            return 1;
+        else if (x == 0)
+            return 0;
 
-        }
-        return i-1;
+        int st = 0, end = x / 2;
+        long mid, cur;
+        
+            //if (st== 46341) break;
+            long poweredBy2 ;
+            while (st <= end)
+            {
+                mid = (st + end) / 2;
+                 poweredBy2 = mid * mid;
+                if (x ==  poweredBy2)
+                return mid;
+                if(poweredBy2 < x)
+                {
+                    cur = mid;
+                    st = mid + 1;
+                }
+                else
+                    end = mid - 1;
+            }
+        
+        return cur;
     }
 };
