f82dfda4232bf01b9ae0c6a9ee4e549634cf0569
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-31 23:53:52 +0300

    Time: 889 ms (86.3%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,20 @@
+/* Write your T-SQL query statement below */
+-- select query_name ,
+-- cast( sum(rating*1.0 /position) / count(*)  as decimal (10,2)) as quality,
+
+-- cast(sum(rating<3)*100.0/  count(*) as decimal(10,2)) as poor_query_percentage
+-- from Queries
+-- group by query_name;
+
+-- SELECT 
+--   query_name,
+--   CAST(SUM(rating * 1.0 / position) / COUNT(*) AS DECIMAL(10, 2)) AS quality,
+--   CAST(SUM(rating < 3) * 100.0 / COUNT(*) AS DECIMAL(10, 2)) AS poor_query_percentage
+-- FROM Queries
+-- GROUP BY query_name;
+SELECT 
+  query_name,
+  CAST(SUM(1.0 * rating / position) / COUNT(*) AS DECIMAL(10, 2)) AS quality,
+  CAST(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS DECIMAL(10, 2)) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
