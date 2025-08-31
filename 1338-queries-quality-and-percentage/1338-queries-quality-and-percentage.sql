2d24afb8789f87753bd80f15314ab4851f03bfc7
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-08-31 23:57:52 +0300

    Time: 901 ms (83.07%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,17 +1,19 @@
 /* Write your T-SQL query statement below */
+-- wrong code 1
 -- select query_name ,
 -- cast( sum(rating*1.0 /position) / count(*)  as decimal (10,2)) as quality,
-
 -- cast(sum(rating<3)*100.0/  count(*) as decimal(10,2)) as poor_query_percentage
 -- from Queries
 -- group by query_name;
 
+-- wrong code 2
 -- SELECT 
 --   query_name,
 --   CAST(SUM(rating * 1.0 / position) / COUNT(*) AS DECIMAL(10, 2)) AS quality,
 --   CAST(SUM(rating < 3) * 100.0 / COUNT(*) AS DECIMAL(10, 2)) AS poor_query_percentage
 -- FROM Queries
 -- GROUP BY query_name;
+-- corrected code
 SELECT 
   query_name,
   CAST(SUM(1.0 * rating / position) / COUNT(*) AS DECIMAL(10, 2)) AS quality,
