1183d61a6fe49eaa399059db477166a799407082
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-10 00:00:22 +0300

    Time: 548 ms (22.07%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -6,13 +6,23 @@
 --  (select e2.employee_id from Employees as e2)
 --  order by e1.employee_id asc;
 
-select e1.employee_id  from Employees e1 
-left join Employees as e2
-on 
-  e1.manager_id =e2.employee_id
-where   e1.salary <30000 and
-e1.manager_id is not null
-and e2.employee_id is null
+-- select e1.employee_id  from Employees e1 
+-- left join Employees as e2
+-- on 
+--   e1.manager_id =e2.employee_id
+-- where   e1.salary <30000 and
+-- e1.manager_id is not null
+-- and e2.employee_id is null
 
-order by e1.employee_id;
+-- order by e1.employee_id;
  
+ SELECT e1.employee_id
+FROM Employees e1
+WHERE e1.salary < 30000
+  AND e1.manager_id IS NOT NULL
+  AND NOT EXISTS (
+    SELECT 1
+    FROM Employees e2
+    WHERE e2.employee_id = e1.manager_id
+  )
+ORDER BY e1.employee_id ASC;
