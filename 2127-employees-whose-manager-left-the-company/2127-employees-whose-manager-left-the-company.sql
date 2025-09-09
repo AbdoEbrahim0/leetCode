0608e9aae0c2d668db48060c9061674d81ce56e5
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-09 23:56:48 +0300

    Time: 422 ms (49.79%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,8 +1,18 @@
 /* Write your T-SQL query statement below */
 --  and (select 1 from Employees as e2 where e1.employee_id   );
 
-select e1.employee_id from Employees e1 
-where   e1.salary <30000 and  e1.manager_id not  in 
- (select e2.employee_id from Employees as e2)
- order by e1.employee_id asc;
+-- select e1.employee_id from Employees e1 
+-- where   e1.salary <30000 and  e1.manager_id not  in 
+--  (select e2.employee_id from Employees as e2)
+--  order by e1.employee_id asc;
+
+select e1.employee_id  from Employees e1 
+left join Employees as e2
+on 
+  e1.manager_id =e2.employee_id
+where   e1.salary <30000 and
+e1.manager_id is not null
+and e2.employee_id is null
+
+order by e1.employee_id;
  
