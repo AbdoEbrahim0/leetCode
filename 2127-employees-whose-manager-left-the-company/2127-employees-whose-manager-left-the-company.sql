ed37389765cb4d732baf981b73cc16f10583c413
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-09 23:32:07 +0300

    Time: 485 ms (29.86%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -4,5 +4,5 @@
 select e1.employee_id from Employees e1 
 where   e1.salary <30000 and  e1.manager_id not  in 
  (select e2.employee_id from Employees as e2)
- order by e1.employee_id;
+ order by e1.employee_id asc;
  
