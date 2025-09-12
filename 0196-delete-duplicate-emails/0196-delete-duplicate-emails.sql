cf675b2b397ff1aa59ce1316562d82da27eba374
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-12 23:51:24 +0300

    Time: 432 ms (78.09%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+
+with duplicated as(
+select *, dense_rank() over (partition by email order by id ) as drnk 
+from Person
+)
+delete  from Person 
+where id in (
+    select id from duplicated where drnk>1
+);
+-- where drnk>1;
