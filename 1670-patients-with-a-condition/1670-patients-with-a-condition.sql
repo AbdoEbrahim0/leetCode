8390d90c5daa85847a3e5a8c3debbe553a9e4f80
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-13 23:46:47 +0300

    Time: 321 ms (58.29%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,4 +1,9 @@
 /* Write your T-SQL query statement below */
-select *
-from Patients 
-where conditions like 'DIAB1%' or conditions like '% DIAB1%';
+-- select *
+-- from Patients 
+-- where conditions like 'DIAB1%' or conditions like '% DIAB1%';
+
+SELECT * 
+FROM Patients 
+WHERE PATINDEX('DIAB1%', conditions) > 0
+ or  PATINDEX('% DIAB1%', conditions) > 0
