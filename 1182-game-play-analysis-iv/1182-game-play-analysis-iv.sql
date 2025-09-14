e131a8226ce94b80b9c155a6decc538b0e01c839
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-15 00:18:05 +0300

    Time: 1275 ms (83.03%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -16,7 +16,7 @@
 -- select count(distinct a3.player_id) from Activity as a3
 --     ) as decimal(10,2) ) as fraction; 
 
--- corrected version:
+-- still fail in some casses:
 -- select cast ( 
 --     1.0* count(distinct a1.player_id) 
 -- / (select count(distinct a3.player_id) from Activity as a3)
@@ -24,29 +24,17 @@
 --  from Activity as a1 join Activity as a2
 -- on a1.player_id =a2.player_id  
 -- and  datediff(day,a1.event_date,a2.event_date) =1   ; 
-
-
--- SELECT 
---   CAST(
---     1.0 * COUNT(DISTINCT a1.player_id) 
---     / (SELECT COUNT(DISTINCT player_id) FROM Activity)
---     AS DECIMAL(10,2)
---   ) AS fraction
--- FROM Activity a1
--- JOIN Activity a2 
---   ON a1.player_id = a2.player_id 
---  AND DATEDIFF(day, a1.event_date, a2.event_date) = 1;
-
 SELECT 
   ROUND(
-    1.0 * SUM(CASE WHEN a.event_date = DATEADD(day, 1, f.first_day) THEN 1 ELSE 0 END)
-    / COUNT(DISTINCT f.player_id),
+    1.0 * COUNT(DISTINCT a.player_id) 
+    / (SELECT COUNT(DISTINCT player_id) FROM Activity),
     2
   ) AS fraction
-FROM (
+FROM Activity a
+JOIN (
     SELECT player_id, MIN(event_date) AS first_day
     FROM Activity
     GROUP BY player_id
 ) f
-LEFT JOIN Activity a
-  ON f.player_id = a.player_id;
+  ON a.player_id = f.player_id
+ AND a.event_date = DATEADD(day, 1, f.first_day);
