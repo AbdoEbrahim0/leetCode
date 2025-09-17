e36b2f0fbcf058bf39e23c94b56e7fdeb87a8f45
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-17 20:36:16 +0300

    Time: 565 ms (30.49%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,26 +1,28 @@
 /* Write your T-SQL query statement below */
+
+-- you can escape divide by ZERO by divide by null
 -- select s.user_id,
---     case when c.user_id is null then  0 
---     else (
---     select count(*)
---     from Confirmations
---     where action ='confirmed'
---     group by Confirmations.user_id)
---     end
-    
+--     round(
+--         1.0* sum(case when c.action ='confirmed'  then  1 else 0 end)  /
+--         case when count(c.action) =0 then 1 else count(c.action) end  
+--     ,2)
 --  as confirmation_rate
 -- from Signups as s left join Confirmations as c
 -- on s.user_id=c.user_id
 -- group by s.user_id;
--- you can escape divide by ZERO by divide by null
-select s.user_id,
-    round(
-        1.0* sum(case when c.action ='confirmed'  then  1 else 0 end)  /
-        case when count(c.action) =0 then 1 else count(c.action) end  
-    ,2)
- as confirmation_rate
-from Signups as s left join Confirmations as c
-on s.user_id=c.user_id
-group by s.user_id;
  
 
+select user_id, (select case when   round 
+                    (1.0* sum (case when c.action ='confirmed'  then  1 else 0 end)
+                    / sum (case when c.action  in ('confirmed', 'timeout')   then  1 else 0 end)
+                    ,2) is null then 0 else 
+                    round 
+                    (1.0* sum (case when c.action ='confirmed'  then  1 else 0 end)
+                    / sum (case when c.action  in ('confirmed', 'timeout')   then  1 else 0 end)
+                    ,2) end
+                    from Confirmations as c
+                    where c.user_id=s.user_id
+                     
+                )
+as confirmation_rate
+from Signups as s;
