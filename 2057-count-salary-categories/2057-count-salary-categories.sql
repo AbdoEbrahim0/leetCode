2befcc21c82751729d6497196480a3034c599c34
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-21 00:01:45 +0300

    Time: 10294 ms (91.89%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,40 +1,35 @@
 /* Write your T-SQL query statement below */
--- select count(*)
--- from Accounts
--- where income <20000;
+-- SELECT 'Low Salary' AS category, SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS accounts_count
+-- FROM Accounts
+-- UNION ALL
+-- SELECT 'Average Salary', SUM(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END)
+-- FROM Accounts
+-- UNION ALL
+-- SELECT 'High Salary', SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END)
+-- FROM Accounts;
 
--- select count(*)
--- from Accounts
--- where income between 20000 and 50000;
+select 'Low Salary' as category ,sum(case when income<20000  then 1 else 0 end) as accounts_count
+from Accounts
+union all 
+select 'Average Salary' ,sum(case when income between 20000 and 50000 then 1 else 0 end) as accounts_count
+from Accounts
+union all
+select 'High Salary' ,sum(case when income>50000  then 1 else 0 end) as accounts_count
+from Accounts; 
 
--- select count(*)
--- from Accounts
--- where income >50000;
+-- SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count
+-- FROM Accounts
+-- WHERE income < 20000
 
+-- UNION ALL
 
--- insert into temp (category,accounts_count)
--- select 'Low Salary', count(*) from Accounts where income <20000;
+-- SELECT 'Average Salary', COUNT(*)
+-- FROM Accounts
+-- WHERE income BETWEEN 20000 AND 50000
 
--- insert into temp (category,accounts_count)
--- select 'Low Salary' ,count(*) from Accounts where income <20000 
+-- UNION ALL
 
--- select account_id as accounts_count
--- from Accounts
--- group by account_id;
-
-SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count
-FROM Accounts
-WHERE income < 20000
-
-UNION ALL
-
-SELECT 'Average Salary', COUNT(*)
-FROM Accounts
-WHERE income BETWEEN 20000 AND 50000
-
-UNION ALL
-
-SELECT 'High Salary', COUNT(*)
-FROM Accounts
-WHERE income > 50000;
+-- SELECT 'High Salary', COUNT(*)
+-- FROM Accounts
+-- WHERE income > 50000;
 
