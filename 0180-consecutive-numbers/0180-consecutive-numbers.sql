1842b6edc66df3986d9e91158138ee0175763a50
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-22 23:09:36 +0300

    Time: 604 ms (44.87%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -3,19 +3,7 @@
 -- from Logs
 -- ;
 
--- select l1.num
--- from Logs as l1
--- where l1.id =(select l2.id from Logs as l2 where (l1.num=l2.num) and 1=l2.id-l1.id )
--- and   l1.id =(select l3.id from Logs as l3 where (l1.num=l3.num) and 2=l3.id-l1.id )
-
-select distinct l1.num as ConsecutiveNums
+select distinct l1.num as ConsecutiveNums --ditinct to remove dublicated as i make
 from Logs as l1
 where l1.id = (select l1.id from Logs as l2 where (l1.num=l2.num) and  l2.id-l1.id=1  ) --there is no output when using select l2.id
-and   l1.id  = (select l1.id from Logs as l3 where (l1.num=l3.num) and 2=l3.id-l1.id);--there is no output when using select l3.id
-
-
-
--- select l2.id from Logs as l2 where 1=l2.id-1 ;
-
--- (select l3.id from Logs as l3 where 1=l3.id-2 )
-
+and   l1.id  = (select l1.id from Logs as l3 where (l1.num=l3.num) and 2=l3.id-l1.id);  --there is no output when using select l3.id
