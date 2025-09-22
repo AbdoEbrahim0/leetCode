41819d682c1def1b9d13b2979b056b73106de84a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-22 23:49:31 +0300

    Time: 509 ms (89.27%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,14 +1,38 @@
 /* Write your T-SQL query statement below */
--- select sum(case when num =1  then 1 else 0 end)
--- from Logs
--- ;
+-- [using subquery ]
 
 -- select distinct l1.num as ConsecutiveNums --ditinct to remove dublicated as i make
 -- from Logs as l1
 -- where l1.id = (select l1.id from Logs as l2 where (l1.num=l2.num) and  l2.id-l1.id=1  ) --there is no output when using select l2.id
 -- and   l1.id  = (select l1.id from Logs as l3 where (l1.num=l3.num) and 2=l3.id-l1.id);  --there is no output when using select l3.id
 
-select distinct l1.num as ConsecutiveNums
-from Logs as l1 join Logs as l2 on   l2.id-l1.id= 1 and (l1.num=l2.num)
-                join Logs as l3 on l3.id-l1.id =2 and (l1.num=l3.num);
+--[using join]
+
+-- select distinct l1.num as ConsecutiveNums
+-- from Logs as l1 join Logs as l2 on   l2.id-l1.id= 1 and (l1.num=l2.num)
+--                 join Logs as l3 on l3.id-l1.id =2 and (l1.num=l3.num);
  
+ -- [faild] 
+-- select l1.num
+-- from Logs as l1
+-- where sum (
+--     case when 
+--     l1.id =  (select l1.id from Logs as l2 where (l1.num=l2.num) and  l2.id-l1.id=1  )
+--     or 
+--     l1.id  = (select l1.id from Logs as l3 where (l1.num=l3.num) and l3.id-l1.id=2)
+--     then 1
+--     else 0
+--     end
+--     ) >2
+
+ 
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+  SELECT 
+    num,
+    LEAD(num, 1) OVER (ORDER BY id) AS next1,
+    LEAD(num, 2) OVER (ORDER BY id) AS next2
+  FROM Logs
+) AS sub
+WHERE num = next1 AND num = next2;
+
