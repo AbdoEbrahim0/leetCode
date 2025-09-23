7eef28174f890a6f9e85d8966d6b0fd10ca48cff
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-24 00:13:24 +0300

    Time: 340 ms (93.85%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,45 @@
 /* Write your T-SQL query statement below */
-select distinct max(salary) as  SecondHighestSalary 
-from Employee
-where salary < (select max(salary) from Employee); 
 
--- select max(salary)
+--[maxFn]
+-- select distinct max(salary) as  SecondHighestSalary 
 -- from Employee
+-- where salary < (select max(salary) from Employee); 
+
+-- [subQuery]
+-- processes queries in this order:
+-- FROM
+-- WHERE
+-- GROUP BY
+-- HAVING
+-- SELECT
+-- ORDER BY
+-- select salary as
+-- from (
+-- select id,salary,row_number() over(order by salary desc)  as rn
+-- from Employee 
+-- ) AS ranked --note it will not work if As ranked not used
+-- where rn =2 ;
+
+with RankedSalaries as (
+  select  salary, dense_rank() over (order by salary desc) as rn
+  from Employee
+)
+select 
+    case 
+    when max(rn)=1 then null
+    else MAX(CASE WHEN rn = 2 THEN salary END)
+    end
+    as  SecondHighestSalary 
+from RankedSalaries
+
+;
+
+
+
+--   select distinct salary, row_number() over (order by salary desc) as rn
+--   from Employee
+--   output:
+--   | salary | rn |
+-- | ------ | -- |
+-- | 100    | 1  |
+-- | 100    | 2  |
