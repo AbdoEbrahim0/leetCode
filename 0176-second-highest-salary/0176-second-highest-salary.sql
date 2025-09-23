4ce3094e3b6360219858f5ae86bd1ffcd9612f3e
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-24 00:31:25 +0300

    Time: 342 ms (92.85%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -19,27 +19,36 @@
 -- from Employee 
 -- ) AS ranked --note it will not work if As ranked not used
 -- where rn =2 ;
-
-with RankedSalaries as (
-  select  salary, dense_rank() over (order by salary desc) as rn
-  from Employee
-)
-select 
-    case 
-    when max(rn)=1 then null
-    else MAX(CASE WHEN rn = 2 THEN salary END)
-    end
-    as  SecondHighestSalary 
-from RankedSalaries
-
-;
-
-
-
---   select distinct salary, row_number() over (order by salary desc) as rn
---   from Employee
---   output:
---   | salary | rn |
+--[dense rank]
+--note row_number() will fail as it count duplicated
+-- | salary | rn |
 -- | ------ | -- |
 -- | 100    | 1  |
 -- | 100    | 2  |
+-- with RankedSalaries as (
+--   select  salary, dense_rank() over (order by salary desc) as rn
+--   from Employee
+-- )
+-- select 
+--     case 
+--     when max(rn)=1 then null
+--     else MAX(CASE WHEN rn = 2 THEN salary END)
+--     end
+--     as  SecondHighestSalary 
+-- from RankedSalaries
+-- ;
+
+select
+case 
+when count(*)=0 then null 
+else 
+ max(salary)    --note must use max(salary) as i used group by instead of salary
+end as  SecondHighestSalary 
+from 
+(
+select salary ,row_number() over (order by salary desc) as rn
+from Employee
+group by salary
+) as ranked
+
+where rn=2
