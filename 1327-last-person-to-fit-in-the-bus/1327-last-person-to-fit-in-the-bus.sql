cdfbc100065606b5c475ee922affcfe332657eb1
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-24 23:16:18 +0300

    Time: 1454 ms (25.48%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,14 +1,24 @@
 /* Write your T-SQL query statement below */
 -- declare @sum int
+-- with ModifiedbyTurnOrder as
+-- (
+-- select  person_name,turn,sum(weight) over (order by turn) as acc 
+-- from Queue
+-- )
+-- select top 1 person_name
+-- from ModifiedbyTurnOrder    
+-- where acc<=1000 
+-- order by turn Desc
+-- ;
+
 with ModifiedbyTurnOrder as
 (
-select  person_name,turn,sum(weight) over (order by turn) as acc 
+select  sum(weight) over (order by turn) as acc 
 from Queue
 )
-select top 1 person_name
-from ModifiedbyTurnOrder    
-where acc<=1000 
-order by turn Desc
+select person_name
+from Queue
+where turn= (select count(*) from ModifiedbyTurnOrder  where acc<=1000 )
 ;
 
 
