89c2fdd17606a0dbda8f48e34ff9b98f22172f0c
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-27 22:33:19 +0300

    Time: 508 ms (12.09%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,21 +1,27 @@
 /* Write your T-SQL query statement below */
 
-select s1.id, (case  
-    when s1.id%2=0  
-    then    (select student from Seat as s3 where s3.id=s1.id-1)
-    when s1.id%2=1 and s1.id+1 <= (select max(id) from Seat)
-    then    (select student from Seat as s2 where s2.id=s1.id+1)
-    else    s1.student
-end)
-as student
-from Seat as s1
+--[subQuerey without order by]
+
+-- select s1.id, (case  
+--     when s1.id%2=0  
+--     then    (select student from Seat as s3 where s3.id=s1.id-1)
+--     when s1.id%2=1 and s1.id+1 <= (select max(id) from Seat)
+--     then    (select student from Seat as s2 where s2.id=s1.id+1)
+--     else    s1.student
+-- end)
+-- as student
+-- from Seat as s1
  
 
--- select *
--- from temp;
+ select 
+    case 
+    when s1.id %2=1 and s1.id+1 <=(select max(s2.id) from Seat as s2)
+    then s1.id+1
 
--- delete from cte 
--- where id=(select top 1 id from cte order by id desc)
-
--- select *
--- from cte;
+    when s1.id %2=0 
+    then s1.id-1
+    else s1.id
+    end as id
+, s1.student 
+from Seat as s1
+order by id asc; -- i dont know why when ordering by s1.id not arrange them
