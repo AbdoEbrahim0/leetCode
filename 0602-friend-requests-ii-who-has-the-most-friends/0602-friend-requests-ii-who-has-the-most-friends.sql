e2bd16c9fb5ea41f53fdd63ee6e4cd92ba6d2b32
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-09-29 00:03:06 +0300

    Time: 366 ms (26.57%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,34 +1,33 @@
 /* Write your T-SQL query statement below */
 
-with cte as (
+
+--[CTE]
+-- ########
+-- with cte as (
+-- select accepter_id as id
+-- from RequestAccepted
+-- union all
+-- select  requester_id
+-- from RequestAccepted
+-- )
+
+-- select top 1 id ,count(id) as num
+-- from cte
+-- group by id
+-- order by num desc;
+
+
+--[subQuery]
+
+select top 1 id ,count(id) as num
+from 
+(
 select accepter_id as id
 from RequestAccepted
 union all
-select  requester_id
+select  requester_id as id
 from RequestAccepted
-)
 
-select top 1 id ,count(id) as num
-from cte
+) as allFirndsOnce --must make alies to table to work if not doing it you will get syntax error 
 group by id
 order by num desc;
-
-
-
-
--- select top 1 accepter_id as id,count(accepter_id)+count(requester_id)-1  as num
--- from RequestAccepted
--- group by accepter_id
--- order by  num desc;
-
-
--- select  requester_id,count( requester_id) as cntSender
--- from RequestAccepted
--- group by  requester_id
--- order by  cntSender desc;
-
-
--- select  requester_id, accepter_id ,count( requester_id) as cntSender ,count(accepter_id) as cntReciver
--- from RequestAccepted
--- group by  requester_id,accepter_id
--- order by  cntSender,cntReciver desc;
