6453a73356ffff7f4b023860cd39ee19cb863e95
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-04 00:05:23 +0300

    Time: 964 ms (55.09%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,28 +1,15 @@
 /* Write your T-SQL query statement below */
--- with cte as (
--- select product_id , new_price , change_date  dense_rank() over (change_date desc) as drnk
--- from Products
--- where change_date <= '2019-8-16'
-
--- ) as Ranked
--- select * from cte
-
-WITH RankedPrices AS (
-    SELECT 
-        product_id,
-        new_price,
-        ROW_NUMBER() OVER (
-            PARTITION BY product_id 
-            ORDER BY change_date DESC
-        ) AS rn
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-)
-SELECT 
-    p.product_id,
-    ISNULL(r.new_price, 10) AS price
-FROM (
-    SELECT DISTINCT product_id FROM Products
-) p
-LEFT JOIN RankedPrices r
-    ON p.product_id = r.product_id AND r.rn = 1;
+with cte as (
+select product_id ,
+       new_price ,
+       dense_rank() over 
+       (PARTITION BY product_id order by change_date desc) as drnk
+from Products
+where change_date <= '2019-8-16'
+) 
+select product_id , new_price as price from cte
+where drnk=1
+union 
+select product_id, new_price = 10
+from  Products 
+where product_id not in( select product_id from cte) 
