b494a97a77348a8a8003a43b954143297d8d97cf
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-14 01:08:43 +0300

    Time: 0 ms (100%), Space: 11.4 MB (33.82%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.cpp
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
         vector<int> v;
-        for (int z = 0; z < index.size(); z++)
+        for (signed char z = 0; z < index.size(); z++)
         {
             v.insert(v.begin() + index[z] ,nums[z]);
         }
