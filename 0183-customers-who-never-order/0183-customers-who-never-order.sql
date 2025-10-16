cf6ab0434effc07e7226b9329cf099fb34f2c684
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-16 18:32:57 +0300

    Time: 409 ms (40.46%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,5 +1,18 @@
 /* Write your T-SQL query statement below */
-select  c.name as Customers --,o.id
+
+-- using join Only
+-- select  c.name as Customers --,o.id
+-- from Orders as o right join Customers as c
+-- on c.id=o.customerId
+-- where o.id is NULL
+
+
+
+select c.name as Customers 
 from Orders as o right join Customers as c
 on c.id=o.customerId
-where o.id is NULL
+group by c.id, c.name
+having count(o.id) =0
+
+-- 
+--not in
