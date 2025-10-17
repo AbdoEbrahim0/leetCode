d96a4d80e0d034ae568950ea0398d03d2c3bc5dd
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-17 23:12:35 +0300

    Time: 441 ms (41.64%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,16 +1,19 @@
 /* Write your T-SQL query statement below */
--- select email as Email
--- from Person 
--- where email
+
+--[group by ]
 
 -- select email as Email
 -- from Person  
 -- group by Email
 -- having count(*)>1
 
+--[self join]
+
 -- select distinct p1.email as Email
 -- from Person p1 join Person p2 --fk chiled  pk parent
 -- on  p2.id !=p1.id  and p1.email=p2.email
+
+--[Except with group by ]
 
 -- SELECT email AS Email
 -- FROM Person
@@ -19,6 +22,8 @@
 -- FROM Person
 -- GROUP BY email
 -- HAVING COUNT(*) = 1;
+
+--[subQuery with group by ]
 
 -- SELECT DISTINCT email AS Email
 -- FROM Person
@@ -29,11 +34,20 @@
 --     HAVING COUNT(*) > 1
 -- );
 
-with cte as (
-select email, row_number()   over(partition by email order by id ) as rn
- from Person
+--[CTE with Window fn]
 
-)
-select distinct email as Email
-from CTE
-where rn>1
+-- with cte as (
+-- select email, row_number()   over(partition by email order by id ) as rn
+--  from Person
+-- )
+-- select distinct email as Email
+-- from CTE
+-- where rn>1
+
+--[SubQuery with Window fn]
+SELECT DISTINCT email AS Email
+FROM (
+    SELECT email, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+    FROM Person
+) AS ranked
+WHERE rn > 1;
