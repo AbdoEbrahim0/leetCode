60bd2fa3690e9c68a1eaaaf6a3e0953eadb624f0
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-17 22:18:25 +0300

    Time: 618 ms (11.33%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -8,6 +8,6 @@
 -- group by Email
 -- having count(*)>1
 
-select distinct p2.email as Email
+select distinct p1.email as Email
 from Person p1 join Person p2 --fk chiled  pk parent
 on  p2.id !=p1.id  and p1.email=p2.email
