e2c8fe2f58d168f3005c108b760df9a359c1288a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-18 22:47:50 +0300

    Time: 422 ms (86.62%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
-select e.name Employee
-from Employee as e  join Employee as m
-on m.id =e.id and e.Salary >(select m.Salary from Employee as m where  m.id=e.managerId  )
+select e.name as Employee
+from Employee as e
+where e.managerId is not null  and e.Salary >(select m.Salary from Employee as m where  m.id=e.managerId  )
 
 -- select e.name as Employee
 -- from Employee as e
