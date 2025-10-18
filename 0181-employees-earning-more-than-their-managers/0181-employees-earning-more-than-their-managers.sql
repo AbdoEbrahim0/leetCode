73a45c94abf83c53b3e7f5ee055e51dd231af76f
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-18 23:23:23 +0300

    Time: 432 ms (80.82%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -10,15 +10,32 @@
 -- from Employee as e
 -- where e.managerId is not null  and e.Salary >(select m.Salary from Employee as m where  m.id=e.managerId  )
 
-select m.name Employee
-from Employee e join Employee m
-on e.id=m.managerId and m.salary>e.salary
-/*
-| Employee |
-| -------- |
-| Sam      |
-| Max      |
-*/
+--[self join with note solving missunderstanding]
 -- select e.name Employee
 -- from Employee e join Employee m
--- on m.id=e.managerId and e.salary>m.salary
+-- on e.id=m.managerId and e.salary>m.salary
+
+/*
+Translation : finds managers who earn more than their employees.
+“Find employees (e) whose ID matches the managerId of someone else (m).”
+You're treating e as the manager, and m as the employee.
+
+| Employee |
+| -------- |
+| Max      |
+*/
+--[self join with note solving missunderstanding]
+
+select e.name Employee
+from  Employee m join Employee e 
+on m.id=e.managerId and e.salary>m.salary
+
+/*
+Translation: employees who earn more than their managers.
+“Find employees (e) whose manager’s ID matches m.id.”
+You're treating e as the employee, and m as the manager.
+
+| Employee |
+| -------- |
+| Joe     |
+*/
