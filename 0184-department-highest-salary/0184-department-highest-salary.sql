2606de261006a0c2efd8756540f3a515bdb00cab
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-19 23:59:07 +0300

    Time: 446 ms (51.75%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,23 @@
+/* Write your T-SQL query statement below */
+-- select d.name as Department ,e.name,e.salary
+-- from Department as d join Employee as e
+-- on d.id=e.departmentId
+-- group by d.name,e.name,e.salary
+-- having e.salary =(select max(salary) from Employee where departmentId =d.id)
+
+-- with cte as(
+    
+-- select departmentId ,max(salary) as MaximumSal
+-- from Employee
+-- group by departmentId
+-- )
+
+
+-- select  d.name as Department , e.name as Employee ,e.salary as Salary
+-- from Department as d ,Employee as e
+-- where d.id=cte.departmentId and e.salary =cte.MaximumSal
+
+select d.name as Department ,e.name as Employee,e.salary as Salary 
+from Department as d join Employee as e
+on d.id=e.departmentId
+where e.salary =( select max(salary) from Employee  where  departmentId=d.id )
