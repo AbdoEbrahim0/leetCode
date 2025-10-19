6f6ec95e36de5e14ccb8c255aa84763f5e168980
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-20 00:27:45 +0300

    Time: 602 ms (14.87%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -17,7 +17,13 @@
 -- from Department as d ,Employee as e
 -- where d.id=cte.departmentId and e.salary =cte.MaximumSal
 
+--[join & SubQuerey]
+-- select d.name as Department ,e.name as Employee,e.salary as Salary 
+-- from Department as d join Employee as e
+-- on d.id=e.departmentId
+-- where e.salary =( select max(salary) from Employee  where  departmentId=d.id )
+
 select d.name as Department ,e.name as Employee,e.salary as Salary 
 from Department as d join Employee as e
 on d.id=e.departmentId
-where e.salary =( select max(salary) from Employee  where  departmentId=d.id )
+where e.salary =( select max(salary) from Employee  group by  departmentId having departmentId=d.id)
