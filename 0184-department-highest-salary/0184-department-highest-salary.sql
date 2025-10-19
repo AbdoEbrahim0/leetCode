d9f9465319abe45011da993be6402e648680f68a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-20 01:03:33 +0300

    Time: 438 ms (56.52%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,29 +1,30 @@
 /* Write your T-SQL query statement below */
--- select d.name as Department ,e.name,e.salary
--- from Department as d join Employee as e
--- on d.id=e.departmentId
--- group by d.name,e.name,e.salary
--- having e.salary =(select max(salary) from Employee where departmentId =d.id)
 
--- with cte as(
-    
--- select departmentId ,max(salary) as MaximumSal
--- from Employee
--- group by departmentId
--- )
+--[join & SubQuerey and where]
 
-
--- select  d.name as Department , e.name as Employee ,e.salary as Salary
--- from Department as d ,Employee as e
--- where d.id=cte.departmentId and e.salary =cte.MaximumSal
-
---[join & SubQuerey]
 -- select d.name as Department ,e.name as Employee,e.salary as Salary 
 -- from Department as d join Employee as e
 -- on d.id=e.departmentId
 -- where e.salary =( select max(salary) from Employee  where  departmentId=d.id )
 
+--[join & SubQuerey and group by having]
+
+-- select d.name as Department ,e.name as Employee,e.salary as Salary 
+-- from Department as d join Employee as e
+-- on d.id=e.departmentId
+-- where e.salary =( select max(salary) from Employee  group by  departmentId having departmentId=d.id)
+
+
+with cte as(
+    
+select departmentId ,max(salary) as MaximumSal
+from Employee
+group by departmentId
+)
 select d.name as Department ,e.name as Employee,e.salary as Salary 
-from Department as d join Employee as e
-on d.id=e.departmentId
-where e.salary =( select max(salary) from Employee  group by  departmentId having departmentId=d.id)
+from  Employee as e
+ join Department as d
+on e.departmentId=d.id
+ join cte 
+ on e.salary =cte.MaximumSal and cte.departmentId= e.departmentId
+
