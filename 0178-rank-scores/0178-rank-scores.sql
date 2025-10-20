2239a89812757cbc4bad7ab42fcfc768a20c98ee
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-20 23:21:01 +0300

    Time: 357 ms (68.92%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,9 +1,11 @@
 /* Write your T-SQL query statement below */
-with cte as (
-select score, dense_rank() over (order by score Desc) as rank
-from Scores
+-- with cte as (
 
-)
+
+-- )
  
- select *
- from cte
+--  select *
+--  from cte
+
+ select score, dense_rank() over (order by score Desc) as rank
+from Scores
