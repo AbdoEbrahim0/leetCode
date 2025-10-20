e407b46ca8b84c964c10b835dec21a39871f0e0e
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-21 00:52:17 +0300

    Time: 586 ms (10.25%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,11 +1,44 @@
 /* Write your T-SQL query statement below */
--- with cte as (
 
+--[dense rank window fn  ]
+--  select score, dense_rank() over (order by score Desc) as rank
+-- from Scores
 
--- )
- 
---  select *
---  from cte
+--[from GPT i asked for alternative but nice to know]
+/*
+--How many  scores >= current score
+SELECT 
+    s1.score,
+    (SELECT COUNT( s2.score) 
+     FROM Scores s2 
+     WHERE s2.score >= s1.score) AS rank
+FROM Scores s1
+ORDER BY s1.score DESC;
+| score | rank |
+| ----- | ---- |
+| 4     | 2    |
+| 4     | 2    |
+| 3.85  | 3    |
+| 3.65  | 5    |
+| 3.65  | 5    |
+| 3.5   | 6    |
+*/
+--How many distinctive scores >= current score
+SELECT 
+    s1.score,
+    (SELECT COUNT(distinct  s2.score) 
+     FROM Scores s2 
+     WHERE s2.score >= s1.score) AS rank
+FROM Scores s1
+ORDER BY s1.score DESC;
 
- select score, dense_rank() over (order by score Desc) as rank
-from Scores
+/*
+| score | rank |
+| ----- | ---- |
+| 4     | 1    |
+| 4     | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.5   | 4    |
+*/
