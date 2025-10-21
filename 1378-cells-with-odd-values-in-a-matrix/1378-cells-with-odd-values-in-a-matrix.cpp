e517d392828b1e1b5be9871341005742d2914ba8
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-22 00:15:27 +0300

    Time: 2 ms (23.86%), Space: 11.9 MB (24.51%) - LeetHub

---
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+        vector<vector<int>> matrix;
+        for (int i = 0; i < m; i++)
+        {
+            vector<int>v(n);
+            
+                matrix.push_back (v);
+        }
+
+        for (vector<int> rowCol : indices)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix [rowCol[0]] [j]++; //increment row 
+            }
+            for (int i = 0; i < m; i++)
+            {
+                matrix[i] [rowCol[1]]++; //increment col
+            }
+        }
+        int oddSum = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j]  &1) oddSum++;
+        }
+        return oddSum;
+    }
+};
