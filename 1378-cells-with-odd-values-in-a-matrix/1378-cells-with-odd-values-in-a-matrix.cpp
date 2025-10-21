463dc2a24a64afa2605e5363e3c4533da327b8fa
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-22 00:33:02 +0300

    Time: 0 ms (100%), Space: 11.2 MB (84.16%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cpp
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cpp
@@ -1,27 +1,47 @@
+// class Solution {
+// public:
+//     int oddCells(int m, int n, vector<vector<int>>& indices) {
+//         int oddSum = 0;
+//         vector<vector<int>> matrix;
+//         for (int i = 0; i < m; i++)
+//         {
+//             vector<int>v(n);
+//             matrix.push_back (v);
+//         }
+//         for (vector<int> &rowCol : indices)
+//         {
+//             for (int j = 0; j < n; j++)
+//                 matrix [rowCol[0]] [j]++; //increment row 
+//             for (int i = 0; i < m; i++)
+//                 matrix[i] [rowCol[1]]++; //increment col
+//         }
+        
+//         for (int i = 0; i < m; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//                 if (matrix[i][j]  &1) oddSum++;
+//         }
+//         return oddSum;
+//     }
+// };
+
+
 class Solution {
 public:
     int oddCells(int m, int n, vector<vector<int>>& indices) {
-        int oddSum = 0;
-        vector<vector<int>> matrix;
-        for (int i = 0; i < m; i++)
-        {
-            vector<int>v(n);
-            matrix.push_back (v);
+        vector<int> row(m, 0), col(n, 0);
+
+        for (const auto& idx : indices) {
+            row[idx[0]]++;
+            col[idx[1]]++;
         }
 
-        for (vector<int> &rowCol : indices)
-        {
-            for (int j = 0; j < n; j++)
-                matrix [rowCol[0]] [j]++; //increment row 
-            for (int i = 0; i < m; i++)
-                matrix[i] [rowCol[1]]++; //increment col
-        }
-        
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-                if (matrix[i][j]  &1) oddSum++;
-        }
+        int oddSum = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if ((row[i] + col[j]) % 2 == 1)
+                    oddSum++;
+
         return oddSum;
     }
 };
