155352951f889b5651c5e3a6bfb7bfdb1b8095ad
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-22 22:43:50 +0300

    Time: 0 ms (100%), Space: 13.4 MB (42.21%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -1,19 +1,33 @@
+//[optimized using LOG10]
+// class Solution {
+// public:
+//     int findNumbers(vector<int>& nums) {
+//         int EvenNumbersOfDigits=0;
+//         for(int &val :nums)
+//         {
+//             if(int(log10(val) +1) %2==0)
+//             EvenNumbersOfDigits++;
+//             //cout<< log10(val)+1<< "|" <<val << "#\n";
+//         }
+//         return EvenNumbersOfDigits;
+//     }
+// };
+
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int EvenNumbersOfDigits=0;
         for(int &val :nums)
         {
-            
-            if(int(log10(val) +1) %2==0)
-            EvenNumbersOfDigits++;
-            //cout<< log10(val)+1<< "|" <<val << "#\n";
+        int digits=0;
+        while (val !=0)
+        {
+        digits++;
+        val/=10;
+        }
+        if(digits%2==0)
+        EvenNumbersOfDigits++;
         }
         return EvenNumbersOfDigits;
     }
 };
-
-// 5
-// 101      101
-// 001 &    001 |
-// 001      101
