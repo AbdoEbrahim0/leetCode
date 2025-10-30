68446677b27274219a737dc77d3779f2a30c492a
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2025-10-30 23:46:32 +0300

    Time: 44 ms (96.85%), Space: 55.9 MB (60.29%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -3,14 +3,11 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    
-    
     return function(x) {
     let val=x;
         for(let i=functions.length-1 ;i>=0;i--)
-        {
                 val=functions[i](val);
-        }
+        
         return val;
     }
 };
