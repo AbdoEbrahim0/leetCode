188ae1f409671e054c64c22cdcaf9da94bd33c47
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2026-01-28 16:53:03 +0200

    Time: 464 ms (71.82%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,8 @@
 /* Write your T-SQL query statement below */
-select  name  from Customer where referee_id is NULL OR  referee_id !=2;
+-- select  name  from Customer where referee_id is NULL OR  referee_id !=2;
+
+--.Net
+
+select name
+from Customer
+where referee_id is null or referee_id !=2;
