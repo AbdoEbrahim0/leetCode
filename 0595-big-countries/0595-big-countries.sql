85650b141460e850bd52155e83759b65c8a1d2bc
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2026-01-28 17:05:37 +0200

    Time: 591 ms (41.89%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,8 @@
 /* Write your T-SQL query statement below */
-select name , population ,area  from World where area >= 3000000 OR population>=  25000000;
+-- select name , population ,area  from World where area >= 3000000 OR population>=  25000000;
+
+--.net
+
+select name ,population, area     
+from World
+where population >=25000000 or  area  >=3000000;   
