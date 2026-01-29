324825da44db516d7e1948ebaeab04be01b100b0
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2026-01-29 19:30:24 +0200

    Time: 351 ms (34.37%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,8 +1,5 @@
 /* Write your T-SQL query statement below */
--- select p.product_id ,avg(p.price *u.units)as average_price from Prices as p join UnitsSold as u on 
--- u.product_id = p.product_id
--- where 
--- u.purchase_date < p.end_date and  u.purchase_date > start_date;
+
 -- SELECT 
 --   p.product_id,
 --   ISNULL(ROUND(SUM(CAST(p.price * u.units AS FLOAT)) / NULLIF(SUM(u.units), 0), 2), 0) AS average_price
@@ -46,12 +43,3 @@ and  (u.purchase_date between p.start_date and p.end_date )
 group by p.product_id 
 
 
--- WITH CTE as(
--- select p.product_id, round(1.0*sum(p.price*u.units) / sum(u.units),2) as average_price
--- from Prices p join UnitsSold u
--- on p.product_id=u.product_id
--- where u.purchase_date between p.start_date and p.end_date 
--- --group by p.product_id 
--- )
-
--- select * from CTE;
