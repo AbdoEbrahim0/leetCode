b0fab52e3ec5a322cda9cbdc13855029939d7aa1
AbdoEbrahim0 <116218046+AbdoEbrahim0@users.noreply.github.com>
2026-01-29 20:21:22 +0200

    Time: 1100 ms (74.07%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,14 @@
+/* Write your T-SQL query statement below */
+-- select project_id  ,
+-- cast(sum(e.experience_years) *1.0 /count(p.project_id)  as decimal(10,2)) as average_years
+-- from Project as p join Employee as e
+-- on p.employee_id =e.employee_id
+-- where p.employee_id =e.employee_id
+-- group by p.project_id  ;
+
+
+
+select p.project_id , round(avg(e.experience_years*1.0),2)    as average_years
+from Project p join Employee e
+on e.employee_id=p.employee_id
+group by p.project_id
